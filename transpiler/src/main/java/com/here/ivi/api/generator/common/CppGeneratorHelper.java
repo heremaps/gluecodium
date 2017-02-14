bb218535bf86e87e70c366776ae7db4a11f1bc08
Francisco Cortes <francisco.cortes@here.com>
2017-02-14 17:35:49 +0100

    MINOR: Adding CopyCtor, AssignmentOp, IsValid and ImplAccessor
    Change-Id: I97a8db6061caf9bca181846e27e5acb1d258867b

--- transpiler/src/main/java/com/here/ivi/api/generator/common/CppGeneratorHelper.java
+++ transpiler/src/main/java/com/here/ivi/api/generator/common/CppGeneratorHelper.java
@@ -3,7 +3,10 @@ package com.here.ivi.api.generator.common;
 
 import com.here.ivi.api.generator.common.templates.GeneratorNoticeTemplate;
 import com.here.ivi.api.model.FrancaModel;
+import com.here.ivi.api.model.cppmodel.CppElements;
 import com.here.ivi.api.model.cppmodel.CppMethod;
+import com.here.ivi.api.model.cppmodel.CppParameter;
+import com.here.ivi.api.model.cppmodel.CppType;
 
 import java.io.IOException;
 
@@ -18,7 +21,7 @@ public class CppGeneratorHelper{
             inputFile = "Could not resolve";
             System.err.println("Could not resolve input file ");
         }
-                                // TODO "TypeCollection or Interface " +
+
         String inputDefinition =  element.getName() + ':' + element.getVersion();
         return GeneratorNoticeTemplate.generate(suite, inputDefinition, inputFile, outputTarget);
     }
@@ -36,5 +39,29 @@ public class CppGeneratorHelper{
         ctor.name = className;
         ctor.returnType = "";
         return ctor;
+    }
+
+    public static CppMethod generateCopyCtor(String className){
+        CppMethod ctor = new CppMethod();
+        ctor.name = className;
+        ctor.returnType = "";
+        ctor.inParameters.add(generateClassParam(className));
+        return ctor;
+    }
+
+    public static CppMethod generateAssignOp(String className){
+        CppMethod ctor = new CppMethod();
+        ctor.name = "operator=";
+        ctor.returnType = className + "&"; //TODO ugly ref here...
+        ctor.inParameters.add(generateClassParam(className));
+        return ctor;
+    }
+
+    private static CppParameter generateClassParam(String className) {
+        CppParameter other = new CppParameter();
+        other.name = "other";
+        other.type = new CppType(null, className, CppElements.TypeInfo.Complex);
+        other.mode = CppParameter.Mode.Input;
+        return other;
     }
 }
