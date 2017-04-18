f9759686399edc34d72cb518fbe2f00e4e27b492
Henrik Hinrichs <henrik.hinrichs@here.com>
2017-04-18 11:25:30 +0200

    MINOR: Disable xtext info messages
    Change-Id: I10b26c31644156fd0c648102647b62ea8d7c3e4c

--- transpiler/src/main/java/com/here/ivi/api/logger/TranspilerLogger.java
+++ transpiler/src/main/java/com/here/ivi/api/logger/TranspilerLogger.java
@@ -1,5 +1,9 @@
 package com.here.ivi.api.logger;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.xtext.linking.impl.ImportedNamesAdapter;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.LogManager;
@@ -10,8 +14,9 @@ public class TranspilerLogger {
         try {
             InputStream stream = TranspilerLogger.class.getClassLoader().getResourceAsStream(propertiesFile);
             LogManager.getLogManager().readConfiguration(stream);
-        }
-        catch(IOException e) {
+            // Disable xtext info messages
+            Logger.getLogger(ImportedNamesAdapter.class).setLevel(Level.WARN);
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
