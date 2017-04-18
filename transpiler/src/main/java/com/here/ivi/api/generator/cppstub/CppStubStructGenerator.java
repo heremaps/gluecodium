b4762a0173b0e1cb82afb0066a2f3f8fe0a87c8e
Henrik Hinrichs <henrik.hinrichs@here.com>
2017-04-18 18:45:33 +0200

    MINOR: Some cleanup for structs with methods
    - Moved logic of recognizing & collecting structs into Helper
    - Removed pair generic in favor of real type
    - Some logging and generic fixes
    
    Change-Id: Ic905011f89eae89525ce2077571291d5da32a54c

--- transpiler/src/main/java/com/here/ivi/api/generator/cppstub/CppStubStructGenerator.java
+++ transpiler/src/main/java/com/here/ivi/api/generator/cppstub/CppStubStructGenerator.java
@@ -9,6 +9,7 @@ import com.here.ivi.api.generator.cppstub.templates.ApiStructHeader;
 import com.here.ivi.api.generator.cppstub.templates.StructCtor;
 import com.here.ivi.api.model.DefaultValuesHelper;
 import com.here.ivi.api.model.FrancaModel;
+import com.here.ivi.api.model.StructMethodHelper;
 import com.here.ivi.api.model.cppmodel.*;
 import navigation.CppStubSpec;
 import org.franca.core.franca.*;
@@ -19,45 +20,9 @@ import java.util.logging.Logger;
 
 public class CppStubStructGenerator {
 
-    private static final String StructName = "type";
-
     static Logger logger = Logger.getLogger(CppStubStructGenerator.class.getName());
 
     private final CppNameRules nameRules;
-
-    public static List<AbstractMap.SimpleEntry< FrancaModel.TypeCollection<CppStubSpec.TypeCollectionPropertyAccessor>,
-                                                FrancaModel.Interface<CppStubSpec.InterfacePropertyAccessor> > >
-    collectMethodContainers( final FrancaModel<CppStubSpec.InterfacePropertyAccessor,
-                                               CppStubSpec.TypeCollectionPropertyAccessor> model){
-
-        List<AbstractMap.SimpleEntry<
-                FrancaModel.TypeCollection<CppStubSpec.TypeCollectionPropertyAccessor>,
-                FrancaModel.Interface<CppStubSpec.InterfacePropertyAccessor> > > result = new LinkedList<>();
-
-        //for each type collection: search its methods (if any)
-        for (FrancaModel.TypeCollection<CppStubSpec.TypeCollectionPropertyAccessor> tc : model.typeCollections) {
-
-            if (tc.accessor.getIsStructDefinition(tc.fTypeCollection)) {
-                //find real interface
-                FInterface fi = tc.accessor.getBelongingMethodContainer(tc.fTypeCollection);
-                Optional<FrancaModel.Interface<CppStubSpec.InterfacePropertyAccessor>> fiOptional =
-                        fi != null ?
-                           model.interfaces.stream()
-                               .filter(i -> fi.equals(i.fInterface))
-                               .findFirst() :
-                           Optional.empty();
-                //should never happen
-                if (fi != null && !fiOptional.isPresent()) {
-                    logger.severe("Error: interface could not be found: " + fi);
-                }
-                FrancaModel.Interface<CppStubSpec.InterfacePropertyAccessor> fullInterface =
-                        fiOptional.orElse(null);
-
-                result.add(new AbstractMap.SimpleEntry<>(tc, fullInterface));
-            }
-        }
-        return result;
-    }
 
     public CppStubStructGenerator(CppNameRules rules) {
         this.nameRules = rules;
@@ -66,11 +31,10 @@ public class CppStubStructGenerator {
     public GeneratedFile generateFiles(final GeneratorSuite<?,?> suite,
                                        final FrancaModel<? extends CppStubSpec.InterfacePropertyAccessor,
                                                          ? extends CppStubSpec.TypeCollectionPropertyAccessor> model,
-                                       final FrancaModel.Interface<CppStubSpec.InterfacePropertyAccessor> methods,
-                                       final FrancaModel.TypeCollection<CppStubSpec.TypeCollectionPropertyAccessor> tc) {
+                                       final FrancaModel.Interface<? extends CppStubSpec.InterfacePropertyAccessor> methods,
+                                       final FrancaModel.TypeCollection<? extends CppStubSpec.TypeCollectionPropertyAccessor> tc) {
 
-
-        CppClass newClass = generateClass(methods,tc,model);
+        CppClass newClass = generateClass(methods, tc, model);
 
         LegacyComments comments = LegacyFrancaCommentParser.createCommentParser(tc.fTypeCollection);
 
@@ -88,38 +52,30 @@ public class CppStubStructGenerator {
     }
 
     public CppField generateCppField(CppModelAccessor<CppStubSpec.TypeCollectionPropertyAccessor> rootType,
-                                            FField ffield, FFieldInitializer initializer) {
+                                     FField ffield, FFieldInitializer initializer) {
 
         FTypeRef typeRef = ffield.getType();
         CppField field = new CppField();
-        field.name = ffield.getName();
+        field.name = nameRules.fieldName(ffield.getName());
         field.type = CppTypeMapper.map(rootType, typeRef);
         field.initializer = CppValueMapper.map(field.type, initializer.getValue());
         return field;
     }
 
-    public CppClass generateClass(final FrancaModel.Interface<CppStubSpec.InterfacePropertyAccessor> api,
-                                  final FrancaModel.TypeCollection<CppStubSpec.TypeCollectionPropertyAccessor> tc,
+    public CppClass generateClass(final FrancaModel.Interface<? extends CppStubSpec.InterfacePropertyAccessor> api,
+                                  final FrancaModel.TypeCollection<? extends CppStubSpec.TypeCollectionPropertyAccessor> tc,
                                   final FrancaModel<? extends CppStubSpec.InterfacePropertyAccessor,
                                                     ? extends CppStubSpec.TypeCollectionPropertyAccessor> model) {
 
         CppModelAccessor<CppStubSpec.TypeCollectionPropertyAccessor> rootType =
                 new CppModelAccessor<>(tc.fTypeCollection, tc.model.fModel, tc.accessor,  nameRules, model);
 
-        CppClass newClass = new CppClass(tc.getName());
+        CppClass newClass = new CppClass(nameRules.structName(tc.getName()));
 
         // find member struct ///////////////////////////
 
         // search for a struct inside the type collection of name StructName
-        FStructType memberStruct = null;
-        for(FType type : tc.fTypeCollection.getTypes())
-        {
-            if (type.getName().equals(StructName) &&
-                    type instanceof FStructType) {
-                memberStruct = (FStructType) type;
-                break;
-            }
-        }
+        FStructType memberStruct = StructMethodHelper.findStructType(tc);
 
         if (memberStruct == null) {
             logger.severe("Failed to find type struct! ");
@@ -128,12 +84,12 @@ public class CppStubStructGenerator {
 
         // default values //////////////////////////
 
-        //search for constants from type collection
+        // search for constants from type collection
         FCompoundInitializer defaultInitializer = null;
         for (FConstantDef constantDef : tc.fTypeCollection.getConstants()) {
             // only structs of the same type as belonging interface with correct name will be checked
             if (DefaultValuesHelper.isStructDefaultValueConstant(constantDef) &&
-                    constantDef.getType().getDerived().getName().equals(StructName)) {
+                    StructMethodHelper.isBelongingStruct(constantDef)) {
                 // is valid as constantDef was parsed as a struct ...
                 defaultInitializer = (FCompoundInitializer) constantDef.getRhs();
                 break;
@@ -146,17 +102,17 @@ public class CppStubStructGenerator {
         }
 
         // generate fields /////////////////////////////////
-        Iterator<FField> memberIterator           = memberStruct.getElements().iterator();
+        Iterator<FField> memberIterator = memberStruct.getElements().iterator();
         Iterator<FFieldInitializer> valueIterator = defaultInitializer.getElements().iterator();
-        while(memberIterator.hasNext() && valueIterator.hasNext()) {
-            CppField field = generateCppField(rootType,memberIterator.next(),valueIterator.next() );
+        while (memberIterator.hasNext() && valueIterator.hasNext()) {
+            CppField field = generateCppField(rootType, memberIterator.next(), valueIterator.next());
             newClass.fields.add(field);
         }
 
-        //methods ////////////////////////////
+        // methods ////////////////////////////
         //////////////////////////////////////
 
-        //default constructor is added via xtend template ...
+        // default constructor is added via xtend template ...
 
         if (api != null) {
             CppModelAccessor<CppStubSpec.InterfacePropertyAccessor> rootModelIf =
@@ -166,7 +122,7 @@ public class CppStubStructGenerator {
             StructCtor templateCtor = new StructCtor();
             api.fInterface.getMethods()
                     .stream()
-                    .filter(method -> method.getName().equals("init"))
+                    .filter(StructMethodHelper::isStructInitializer)
                     .forEach(method -> {
                                 CppMethod nonDefaultCtor = new CppMethod();
                                 nonDefaultCtor.mbt = templateCtor;
@@ -186,4 +142,5 @@ public class CppStubStructGenerator {
 
         return newClass;
     }
+
 }
