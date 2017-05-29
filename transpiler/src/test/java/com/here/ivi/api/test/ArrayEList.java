a2e02273a0069bc063744a1439b50e737f623c6c
Henrik Hinrichs <henrik.hinrichs@here.com>
2017-05-29 13:03:42 +0200

    APIGEN-78: Added regression test
    - Extracted struct field generation
    - Added annotation for issue #
    - Added unit test for order independent
      struct initialization
    
    Change-Id: Ic1e33fb8b0ab8c6288c0d232ac41163f9ad5cc40

---
+++ transpiler/src/test/java/com/here/ivi/api/test/ArrayEList.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.test;
+
+import java.util.ArrayList;
+import org.eclipse.emf.common.util.EList;
+
+public class ArrayEList<E> extends ArrayList<E> implements EList<E> {
+
+  public ArrayEList() {
+    super();
+  }
+
+  @Override
+  public void move(int newPosition, E object) {
+    // intentionally left unimplemented
+  }
+
+  @Override
+  public E move(int newPosition, int oldPosition) {
+    // intentionally left unimplemented
+    return null;
+  }
+}
---
+++ transpiler/src/test/java/com/here/ivi/api/test/Issue.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.test;
+
+/** This annotation allows adding an issue number to tests */
+public @interface Issue {
+  /** @return The issue number */
+  String value();
+}
