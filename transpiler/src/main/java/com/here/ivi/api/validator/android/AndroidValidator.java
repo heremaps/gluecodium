f89f3aa14fa309430b5f1dd14bf70a2d6cd061e5
Manuel Klahn <manuel.klahn@here.com>
2017-05-30 11:08:49 +0200

    MINOR Add initial AndroidValidator implementation
    Behaves similar to CarloValidator in that it does nothing. But it will
    be used in AndroidGeneratorSuite later on.
    
    Change-Id: If1dfe36f6245b2f1e45623188f328ac8b0bf5850

---
+++ transpiler/src/main/java/com/here/ivi/api/validator/android/AndroidValidator.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.validator.android;
+
+import com.here.ivi.api.model.FrancaModel;
+import com.here.ivi.api.validator.common.ValidatorInterface;
+import navigation.LegacySpec.InterfacePropertyAccessor;
+import navigation.LegacySpec.TypeCollectionPropertyAccessor;
+
+public final class AndroidValidator
+    implements ValidatorInterface<InterfacePropertyAccessor, TypeCollectionPropertyAccessor> {
+
+  @Override
+  public boolean validate(
+      FrancaModel<InterfacePropertyAccessor, TypeCollectionPropertyAccessor> model) {
+    return true;
+  }
+}
