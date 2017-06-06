1b168b7279e9c25c89f012f0bb826376dd87d245
Rafal Parzych <rafal.parzych@here.com>
2017-06-06 14:37:35 +0200

    APIGEN-102 Add unit tests for com.here.ivi.api.output package
    - for FileOutput class compares with actual temporary files,
    - for ConsoleOutput redirects System.out with SystemOutRule
    
    Change-Id: Iecf2a8c54d00361ff98b2891477842de00ed2f58

---
+++ transpiler/src/test/java/com/here/ivi/api/output/ConsoleOutputTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.output;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.rules.ExpectedException;
+
+public class ConsoleOutputTest {
+
+  @Rule public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+  @Rule public final ExpectedException exception = ExpectedException.none();
+
+  private static final String EXPECTED =
+      String.format(
+          "Generated %1s:\n\n"
+              + "        %2s\n\n"
+              + "Generated %3s:\n\n\n"
+              + "Generated %4s:\n\n"
+              + "        %5s\n\n"
+              + "Done.\n",
+          TestFiles.PATH1,
+          TestFiles.CONTENT1,
+          TestFiles.PATH2,
+          TestFiles.PATH3,
+          TestFiles.CONTENT3);
+
+  @Test
+  public void verifyExpectedConsoleOutputFormatting() throws IOException {
+    assertFalse(TestFiles.FILES.isEmpty());
+
+    ConsoleOutput consoleOutput = new ConsoleOutput();
+    consoleOutput.output(TestFiles.FILES);
+
+    assertEquals(EXPECTED, systemOutRule.getLog());
+  }
+}
---
+++ transpiler/src/test/java/com/here/ivi/api/output/FileOutputTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.output;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.here.ivi.api.generator.common.GeneratedFile;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+
+public class FileOutputTest {
+  @Rule public TemporaryFolder testFolder = new TemporaryFolder();
+
+  @Rule public final ExpectedException exception = ExpectedException.none();;
+
+  @Test
+  public void testFileOutput() throws IOException {
+    assertFalse(TestFiles.FILES.isEmpty());
+
+    FileOutput fileOutput = new FileOutput(testFolder.getRoot());
+    fileOutput.output(TestFiles.FILES);
+
+    for (GeneratedFile file : TestFiles.FILES) {
+      Path path = Paths.get(testFolder.getRoot().getPath(), file.targetFile.getPath());
+      List<String> lines = Files.readAllLines(path, Charset.defaultCharset());
+      if (file.content.length() > 0) {
+        assertEquals(1, lines.size());
+        assertEquals(file.content, lines.get(0));
+      } else {
+        assertTrue(lines.isEmpty());
+      }
+    }
+  }
+}
