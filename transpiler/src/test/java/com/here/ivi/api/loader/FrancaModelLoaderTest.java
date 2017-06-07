f78283a5609809dc08a57876d91ccae59e27f82d
Georg Bremer <georg.bremer@here.com>
2017-06-07 08:50:26 +0000

    APIGEN-82: Test for flaky code generation
    The FrancaModelLoader mapped the fdepl to fidl files based on the
    ambiguous imports in the fdepl file. Added an integration test
    specifically for this scenario.
    
    Adding more elaborate tests with multiple specs would require to inject
    custom ResourceSet class into franca because it resolves these relative
    to its own classpath.
    
    Change-Id: Ib9efd9f43c5be66b341397048535e2b4d9fce8b1

---
+++ transpiler/src/test/java/com/here/ivi/api/loader/FrancaModelLoaderTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.loader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.here.ivi.api.loader.legacy.LegacySpecAccessorFactory;
+import com.here.ivi.api.model.FrancaModel;
+import com.here.ivi.api.model.Interface;
+import com.here.ivi.api.model.ModelHelper;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import navigation.CppStubSpec;
+import org.eclipse.emf.common.util.EList;
+import org.franca.core.franca.FMethod;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FrancaModelLoaderTest {
+
+  /**
+   * The test reproduces bug APIGEN-82. Each fidl got a single fdepl file assigned based on the
+   * includes in the fdepl. When two fdepl files included the same fidl, the second one read would
+   * replace the first one in the mapping. The test reads two fdepls, the first defines a method
+   * const, the second one just includes the fidl. If the mapping would get overridden the method
+   * would lose the constness information.
+   *
+   * @throws URISyntaxException
+   */
+  @Test
+  public void multipleDeploymentsIncludingSameFidl() throws URISyntaxException {
+    LegacySpecAccessorFactory accessorFactory = new LegacySpecAccessorFactory();
+
+    FrancaModelLoader<?, ?> loader = new FrancaModelLoader<>(accessorFactory);
+    ModelHelper.getFdeplInjector().injectMembers(loader);
+
+    URL simpleFidl =
+        ClassLoader.getSystemClassLoader().getResource("francamodelloadertest/Simple.fidl");
+    URL simpleFdepl =
+        ClassLoader.getSystemClassLoader()
+            .getResource("francamodelloadertest/cppstub/Simple.fdepl");
+    // Additional deployment rule also includes Simple.fidl and would replace the information from Simple.fdepl
+    URL additionalFdepl =
+        ClassLoader.getSystemClassLoader()
+            .getResource("francamodelloadertest/cppstub/DependentOnSimple.fdepl");
+
+    Collection<File> currentFiles =
+        Arrays.asList(
+            new File(simpleFdepl.toURI()),
+            new File(simpleFidl.toURI()),
+            new File(additionalFdepl.toURI()));
+    FrancaModel<?, ?> model = loader.load(accessorFactory.getSpecPath(), currentFiles);
+
+    List<? extends Interface<?>> interfaces = model.getInterfaces();
+    assertEquals(1, interfaces.size());
+    Interface<?> iface = interfaces.get(0);
+
+    EList<FMethod> methods = iface.getFrancaInterface().getMethods();
+    assertEquals(1, methods.size());
+    FMethod constMethod = methods.get(0);
+
+    CppStubSpec.InterfacePropertyAccessor propertyAccessor = iface.getPropertyAccessor();
+    assertTrue(propertyAccessor.getConst(constMethod));
+  }
+}
