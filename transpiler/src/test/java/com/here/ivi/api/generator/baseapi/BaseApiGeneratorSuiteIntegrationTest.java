2150f81fe4e6620e8323854494232198e34fe51b
Dario Limongi <dario.limongi@here.com>
2017-06-13 09:28:49 +0200

    APIGEN-95: Add unit tests for BaseApiGeneratorSuite
    Unit tests for public methods of BaseApiGeneratorSuite have been
    created
    
    Due to APIGEN-229, generateFiles() with a valid model has been only
    integration tested
    
    Change-Id: Ie9bd4aa54870e08e3a796d0acdfc98a781f06f7d

---
+++ transpiler/src/test/java/com/here/ivi/api/generator/baseapi/BaseApiGeneratorSuiteIntegrationTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.baseapi;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import com.here.ivi.api.OptionReader;
+import com.here.ivi.api.Transpiler;
+import com.here.ivi.api.generator.common.GeneratedFile;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BaseApiGeneratorSuiteIntegrationTest {
+  private final String francaFilesPath = "src/test/resources/baseapi_generator_suite/fidl";
+  private BaseApiGeneratorSuite baseApiGeneratorSuite;
+
+  private OptionReader.TranspilerOptions generateTranspilerOptions() {
+    OptionReader.TranspilerOptions options = null;
+    OptionReader optionReader = new OptionReader();
+    try {
+      options = optionReader.read(new String[] {francaFilesPath});
+    } catch (Exception exception) {
+      fail("Error while generating Transpiler options: " + exception.getMessage());
+    }
+    return options;
+  }
+
+  @Before
+  public void setUp() {
+    Transpiler transpiler = new Transpiler(generateTranspilerOptions());
+    baseApiGeneratorSuite = new BaseApiGeneratorSuite(transpiler);
+  }
+
+  @Test
+  public void generateFilesIntegration() {
+    // TODO: APIGEN-229 This test should spy stubGenerator::generate and
+    // typeCollectionGenerator::generate to check with verify() that they are called accordingly
+    baseApiGeneratorSuite.buildModel(francaFilesPath);
+
+    List<GeneratedFile> generatedFiles = baseApiGeneratorSuite.generateFiles();
+
+    assertNotNull(generatedFiles);
+    assertEquals("Expected cpp/internal files and test generated file", 5, generatedFiles.size());
+
+    assertFalse(generatedFiles.get(0).content.toString().isEmpty());
+    assertEquals("stub/test/TestInterfaceStub.h", generatedFiles.get(0).targetFile.toString());
+
+    for (GeneratedFile generatedFile : generatedFiles) {
+      assertFalse(generatedFile.content.toString().isEmpty());
+      assertFalse(generatedFile.targetFile.getName().isEmpty());
+    }
+  }
+}
