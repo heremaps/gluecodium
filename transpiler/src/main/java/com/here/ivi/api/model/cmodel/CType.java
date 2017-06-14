73d9900b81bd3813f2cef0c764cb530d7856f441
Georg Bremer <georg.bremer@here.com>
2017-06-14 16:01:27 +0000

    APIGEN-240: Add CModel
    Change-Id: I84b49b8433d9ec6e22770afcb8f2245cc1c16589

---
+++ transpiler/src/main/java/com/here/ivi/api/model/cmodel/CType.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.model.cmodel;
+
+/** Base class for all C types */
+public class CType {
+  public static final CType VOID = new CType("void");
+
+  public final String name;
+  public Boolean isConst = false;
+
+  public CType(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return isConst ? "const " + name : name;
+  }
+}
