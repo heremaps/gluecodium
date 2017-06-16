bebbdd1d39952a6ad5e65028da7be3ef4bbec599
Daniel Kamkha <daniel.kamkha@here.com>
2017-06-16 10:54:39 +0200

    APIGEN-154: Extract method-mapping from StubMapper
    Extracted method-mapping functionality from StubMapper class into a
    standalone dedicated class StubMethodMapper. The reasoning behind the
    change is that StubMapper class is too large, which impacts both
    testability and maintainability.
    
    Added a new unit test for StubMapper that verifies StubMethodMapper
    usage. Added unit tests for the StubMethodMapper class. For both sets of
    tests the coverage is not full, but is limited to HelloWorld-Base scope.
    
    Change-Id: I4409862622e5ace54c7407dee75001b64a60ad1e

---
+++ transpiler/src/main/java/com/here/ivi/api/generator/baseapi/StubMethodMapper.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.baseapi;
+
+import com.here.ivi.api.generator.common.NameHelper;
+import com.here.ivi.api.generator.common.cpp.CppNameRules;
+import com.here.ivi.api.generator.common.cpp.CppTypeMapper;
+import com.here.ivi.api.model.Includes;
+import com.here.ivi.api.model.cppmodel.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import navigation.BaseApiSpec;
+import org.franca.core.franca.FArgument;
+import org.franca.core.franca.FMethod;
+
+public class StubMethodMapper {
+
+  private static final Includes.SystemInclude EXPECTED_INCLUDE =
+      new Includes.SystemInclude("cpp/internal/expected.h");
+
+  private final CppNameRules nameRules;
+
+  public StubMethodMapper(CppNameRules nameRules) {
+    this.nameRules = nameRules;
+  }
+
+  public void mapMethodElements(
+      CppClass stubClass,
+      FMethod method,
+      CppModelAccessor<? extends BaseApiSpec.InterfacePropertyAccessor> rootModel) {
+
+    String uniqueMethodName =
+        nameRules.getMethodName(method.getName())
+            + NameHelper.toUpperCamelCase(method.getSelector());
+
+    CppType errorType;
+    String errorComment = "";
+    if (method.getErrorEnum() != null) {
+      errorType = CppTypeMapper.mapEnum(rootModel, method.getErrorEnum());
+      errorComment = StubCommentParser.FORMATTER.readCleanedErrorComment(method);
+    } else {
+      errorType = CppType.VOID;
+    }
+
+    String returnComment;
+    CppType returnType;
+
+    if (method.getOutArgs().isEmpty()) {
+      returnType = errorType;
+      returnComment = errorComment;
+    } else {
+      List<CppType> returnTypes = new ArrayList<>();
+      // documentation for the result type
+      String typeComment = "Result type for @ref " + stubClass.name + "::" + uniqueMethodName;
+
+      if (!errorType.equals(CppType.VOID)) {
+        returnTypes.add(errorType);
+        if (!errorComment.isEmpty()) {
+          // add error template arg documentation
+          typeComment += StubCommentParser.FORMATTER.formatTag("@arg Error", errorComment);
+        }
+      }
+
+      if (method.getOutArgs().size() > 1) { // create struct for multiple out arguments
+        CppStruct struct = new CppStruct();
+        struct.name = NameHelper.toUpperCamelCase(uniqueMethodName) + "Result";
+        struct.fields =
+            method
+                .getOutArgs()
+                .stream()
+                .map(
+                    argument -> {
+                      CppType type = mapCppType(rootModel, argument, method);
+                      CppField field =
+                          new CppField(type, NameHelper.toLowerCamelCase(argument.getName()));
+                      // document struct field with argument comment
+                      field.comment = StubCommentParser.FORMATTER.readCleanedComment(argument);
+                      return field;
+                    })
+                .collect(Collectors.toList());
+
+        // document return type, struct and append value information to type documentation
+        struct.comment = "Result struct for @ref " + stubClass.name + "::" + uniqueMethodName + ".";
+        typeComment += "\n* @arg Value The value struct instance";
+        returnComment =
+            errorType.equals(CppType.VOID)
+                ? "The result type, containing a struct of values."
+                : "The result type, containing an error and a struct of values.";
+
+        // register with model
+        stubClass.structs.add(struct);
+        returnTypes.add(new CppType(struct.name));
+      } else { // take first & only argument
+        FArgument argument = method.getOutArgs().get(0);
+        CppType type = mapCppType(rootModel, argument, method);
+
+        // document return type and append value information to type documentation
+
+        returnComment =
+            errorType.equals(CppType.VOID)
+                ? "The result type, containing " + type.name + " value."
+                : "The result type, containing either an error or the " + type.name + " value.";
+        if (!errorComment.isEmpty()) {
+          typeComment += StubCommentParser.FORMATTER.formatWithTag("@arg Value", argument);
+        }
+
+        // register with model
+        returnTypes.add(type);
+      }
+
+      if (returnTypes.size() > 1) {
+        // always wrap multiple out values (error + outArgs) in their own type
+        List<String> names = returnTypes.stream().map(t -> t.name).collect(Collectors.toList());
+        Set<Includes.Include> includes =
+            returnTypes.stream().flatMap(t -> t.includes.stream()).collect(Collectors.toSet());
+        includes.add(EXPECTED_INCLUDE);
+
+        returnType =
+            new CppType(
+                rootModel.getDefiner(),
+                "here::internal::Expected< " + String.join(", ", names) + " >",
+                CppElements.TypeInfo.Complex,
+                includes);
+
+        // create & add using for this type with correct documentation
+        String usingTypeName = NameHelper.toUpperCamelCase(uniqueMethodName) + "Expected";
+        CppUsing using = new CppUsing(usingTypeName, returnType);
+        using.comment = typeComment;
+        stubClass.usings.add(using);
+        returnType = new CppType(usingTypeName);
+      } else {
+        returnType = returnTypes.get(0);
+      }
+    }
+
+    // create & add method, add return value documentation as this is not done in buildStubMethod
+    CppMethod cppMethod = buildStubMethod(method, returnType, rootModel);
+    if (!returnComment.isEmpty()) {
+      cppMethod.comment += StubCommentParser.FORMATTER.formatTag("@return", returnComment);
+    }
+    stubClass.methods.add(cppMethod);
+  }
+
+  private CppType mapCppType(
+      CppModelAccessor<? extends BaseApiSpec.InterfacePropertyAccessor> rootModel,
+      FArgument argument,
+      FMethod method) {
+
+    CppType type = CppTypeMapper.map(rootModel, argument);
+
+    if (type.info != CppElements.TypeInfo.InterfaceInstance) {
+      return type;
+    }
+
+    if (rootModel.getAccessor().getCreates(method) == null) {
+      return CppTypeMapper.wrapSharedPtr(type, nameRules);
+    } else {
+      return CppTypeMapper.wrapUniquePtr(type, nameRules);
+    }
+  }
+
+  private CppMethod buildStubMethod(
+      FMethod method,
+      CppType returnType,
+      CppModelAccessor<? extends BaseApiSpec.InterfacePropertyAccessor> rootModel) {
+
+    CppMethod.Builder builder =
+        new CppMethod.Builder(method.getName() + NameHelper.toUpperCamelCase(method.getSelector()))
+            .returnType(returnType);
+
+    if (rootModel.getAccessor().getStatic(method)) {
+      builder.specifier(CppMethod.Specifier.STATIC);
+    } else {
+      if (rootModel.getAccessor().getConst(method)) {
+        // const needs to be before "= 0" pure virtual specifier
+        builder.qualifier(CppMethod.Qualifier.CONST);
+      }
+      builder.specifier(CppMethod.Specifier.VIRTUAL);
+      builder.qualifier(CppMethod.Qualifier.PURE_VIRTUAL);
+    }
+
+    for (FArgument inArg : method.getInArgs()) {
+      CppParameter param = new CppParameter();
+      param.name = inArg.getName();
+      param.mode = CppParameter.Mode.Input;
+
+      param.type = CppTypeMapper.map(rootModel, inArg.getType());
+      if (param.type.info == CppElements.TypeInfo.InterfaceInstance) {
+        param.type = CppTypeMapper.wrapSharedPtr(param.type, nameRules);
+      }
+
+      builder.inParameter(param);
+    }
+
+    builder.comment(StubCommentParser.parse(method).getMainBodyText());
+
+    return builder.build();
+  }
+}
---
+++ transpiler/src/test/java/com/here/ivi/api/generator/baseapi/StubMethodMapperTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.baseapi;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import com.here.ivi.api.generator.common.cpp.CppNameRules;
+import com.here.ivi.api.model.Interface;
+import com.here.ivi.api.model.cppmodel.*;
+import com.here.ivi.api.test.ArrayEList;
+import navigation.BaseApiSpec;
+import org.eclipse.emf.common.util.EList;
+import org.franca.core.franca.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Answers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(JUnit4.class)
+public class StubMethodMapperTest {
+
+  private static final String methodName = "shoot foot";
+  private static final String argumentName = "which foot";
+  private static final String stdString = "std::string";
+
+  @SuppressWarnings("unused")
+  @Mock
+  private CppNameRules nameRules;
+
+  @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+  private FMethod francaMethod;
+
+  @SuppressWarnings("unused")
+  @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+  private Interface<BaseApiSpec.InterfacePropertyAccessor> anInterface;
+
+  @InjectMocks private StubMethodMapper mapper;
+
+  @InjectMocks private CppModelAccessor<BaseApiSpec.InterfacePropertyAccessor> modelAccessor;
+
+  private CppClass cppClass = new CppClass("");
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+
+    when(francaMethod.getErrorEnum()).thenReturn(null);
+    when(francaMethod.getOutArgs()).thenReturn(new ArrayEList<>());
+    when(francaMethod.getName()).thenReturn(methodName);
+  }
+
+  private CppMethod getFirstMethod() {
+    assertNotNull(cppClass.methods);
+    assertEquals(1, cppClass.methods.size());
+
+    CppMethod cppMethod = cppClass.methods.iterator().next();
+    assertNotNull(cppMethod);
+
+    return cppMethod;
+  }
+
+  private EList<FArgument> createFrancaArgumentsArray() {
+    FTypeCollection parentTypeCollection = mock(FTypeCollection.class);
+    when(parentTypeCollection.eContainer()).thenReturn(mock(FModel.class));
+
+    FTypeRef typeRef = mock(FTypeRef.class);
+    when(typeRef.getPredefined()).thenReturn(FBasicTypeId.STRING);
+    when(typeRef.eContainer()).thenReturn(parentTypeCollection);
+
+    FArgument argument = mock(FArgument.class);
+    when(argument.getName()).thenReturn(argumentName);
+    when(argument.getType()).thenReturn(typeRef);
+
+    ArrayEList<FArgument> arguments = new ArrayEList<>();
+    arguments.add(argument);
+
+    return arguments;
+  }
+
+  @Test
+  public void mapMethodElementsNoArguments() {
+    mapper.mapMethodElements(cppClass, francaMethod, modelAccessor);
+
+    CppMethod cppMethod = getFirstMethod();
+    assertEquals(methodName, cppMethod.name);
+    assertTrue(cppMethod.getInParameters().isEmpty());
+    assertTrue(cppMethod.getOutParameters().isEmpty());
+    assertEquals(CppType.VOID, cppMethod.getReturnType());
+  }
+
+  @Test
+  public void mapMethodElementsStaticMethod() {
+    when(anInterface.getPropertyAccessor().getStatic(francaMethod)).thenReturn(true);
+
+    mapper.mapMethodElements(cppClass, francaMethod, modelAccessor);
+
+    CppMethod cppMethod = getFirstMethod();
+    assertTrue(cppMethod.getSpecifiers().contains(CppMethod.Specifier.STATIC));
+  }
+
+  @Test
+  public void mapMethodElementsOneInputArgument() {
+    EList<FArgument> francaArguments = createFrancaArgumentsArray();
+    when(francaMethod.getInArgs()).thenReturn(francaArguments);
+
+    mapper.mapMethodElements(cppClass, francaMethod, modelAccessor);
+
+    CppMethod cppMethod = getFirstMethod();
+    assertEquals(1, cppMethod.getInParameters().size());
+
+    CppParameter cppParameter = cppMethod.getInParameters().get(0);
+    assertEquals(argumentName, cppParameter.name);
+    assertNotNull(cppParameter.type);
+    assertEquals(stdString, cppParameter.type.name);
+  }
+
+  @Test
+  public void mapMethodElementsOneOutputArgument() {
+    EList<FArgument> francaArguments = createFrancaArgumentsArray();
+    when(francaMethod.getOutArgs()).thenReturn(francaArguments);
+
+    mapper.mapMethodElements(cppClass, francaMethod, modelAccessor);
+
+    CppMethod cppMethod = getFirstMethod();
+    assertTrue(cppMethod.getOutParameters().isEmpty());
+
+    assertNotNull(cppMethod.getReturnType());
+    assertEquals(stdString, cppMethod.getReturnType().name);
+  }
+}
