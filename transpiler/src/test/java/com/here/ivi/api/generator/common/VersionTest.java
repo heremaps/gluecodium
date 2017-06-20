b23e3b576641dc896634874b2863cc3ef33bc56c
Daniel Kamkha <daniel.kamkha@here.com>
2017-06-20 10:14:15 +0200

    MINOR: Refactor Version class and add more tests for it
    Moved string-to-Version parsing logic from Transpiler.parseVersion()
    method into a dedicated factory method Version.createFromString().
    
    Renamed the existing factory method Version.create() to
    Version.createFromFrancaVersion().
    
    Updated Version.toString() method to use String.format() instead of
    string concatenation.
    
    Updated Version.createFromString() to use a regexp instead of doing a
    series of string spits and comparisons.
    
    Added new unit tests for Version.createFromFrancaVersion() and
    Version.createFromString() factory methods.
    
    Change-Id: Id40d19da0d1e27ac5493f3a3d9a90c7c9a2400a4

--- transpiler/src/test/java/com/here/ivi/api/generator/common/VersionTest.java
+++ transpiler/src/test/java/com/here/ivi/api/generator/common/VersionTest.java
@@ -12,19 +12,84 @@
 package com.here.ivi.api.generator.common;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.franca.core.franca.FVersion;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class VersionTest {
+
+  private static final int majorVersion = 7;
+  private static final int minorVersion = 21;
+  private static final int patchVersion = 302;
+  private static final String versionSuffix = "abc";
+  private static final String versionWithoutSuffix = "7.21.302";
+  private static final String versionWithSuffix = "7.21.302-abc";
+  private static final String invalidVersionString = "not a version";
+
   @Test
-  public void toStringWithEmptySuffix() throws Exception {
-    final Version version = new Version(1, 2, 3, "");
-    assertEquals("1.2.3", version.toString());
+  public void toStringWithoutSuffix() {
+    final Version version = new Version(majorVersion, minorVersion, patchVersion, "");
+
+    String versionString = version.toString();
+
+    assertEquals(versionWithoutSuffix, versionString);
   }
 
   @Test
-  public void toStringWithNonEmptySuffix() throws Exception {
-    final Version version = new Version(1, 2, 3, "abc");
-    assertEquals("1.2.3-abc", version.toString());
+  public void toStringWithSuffix() {
+    final Version version = new Version(majorVersion, minorVersion, patchVersion, versionSuffix);
+
+    String versionString = version.toString();
+
+    assertEquals(versionWithSuffix, versionString);
+  }
+
+  @Test
+  public void createFromFrancaVersion() {
+    FVersion francaVersion = mock(FVersion.class);
+    when(francaVersion.getMajor()).thenReturn(majorVersion);
+    when(francaVersion.getMinor()).thenReturn(minorVersion);
+
+    Version version = Version.createFromFrancaVersion(francaVersion);
+
+    assertEquals(majorVersion, version.major);
+    assertEquals(minorVersion, version.minor);
+    assertEquals(0, version.patch);
+    assertEquals("", version.suffix);
+  }
+
+  @Test
+  public void createFromStringWithoutSuffix() {
+    Version version = Version.createFromString(versionWithoutSuffix);
+
+    assertEquals(majorVersion, version.major);
+    assertEquals(minorVersion, version.minor);
+    assertEquals(patchVersion, version.patch);
+    assertEquals("", version.suffix);
+  }
+
+  @Test
+  public void createFromStringWithSuffix() {
+    Version version = Version.createFromString(versionWithSuffix);
+
+    assertEquals(majorVersion, version.major);
+    assertEquals(minorVersion, version.minor);
+    assertEquals(patchVersion, version.patch);
+    assertEquals(versionSuffix, version.suffix);
+  }
+
+  @Test
+  public void createFromStringWithInvalidString() {
+    Version version = Version.createFromString(invalidVersionString);
+
+    assertEquals(0, version.major);
+    assertEquals(0, version.minor);
+    assertEquals(0, version.patch);
+    assertEquals("", version.suffix);
   }
 }
