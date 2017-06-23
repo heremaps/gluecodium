63f42d37c5ee68b0923981f5ed1fd15cdff11e8a
Alexander Lehmann <alexander.lehmann@here.com>
2017-06-23 15:14:15 +0200

    APIGEN-108 Swift roundtrip
    Also added test target: make test will run the swift test.
    
    Change-Id: I7b590465d506e5497ff228634265371983fb2db6

---
+++ apps/ios/resources/Tests/helloTests/HelloWorldTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import hello
+
+class HelloWorldTests: XCTestCase {
+    func testHelloWorldMethod() {
+        XCTAssertEqual(HelloWorld.helloWorldMethod(inputString: "Dario"), "Hello Dario")
+    }
+
+    static var allTests = [
+        ("testHelloWorldMethod", testHelloWorldMethod),
+    ]
+}
---
+++ apps/ios/resources/empty.c
@@ -0,0 +1,1 @@
+// This file is left intentionaly blank
