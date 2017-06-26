af505721e62431145bd4ef4f784c2d5c2bc44411
Sascha Peilicke <sascha.peilicke@here.com>
2017-06-26 15:37:35 +0200

    Add additional Apigen CMake modules
    Details about where the transpiler puts files for each target language
    should be hidden from the CMake user. For that we introduce the
    following modules:
    
     - apigen_transpile: Calls the transpiler (at configure-time)
       and sets various properties to the CMake target (for later use)
     - apigen_target_include_directories: Adds additional include
       directories needed to compile the CMake target for a target language
     - apigen_target_sources: Adds additional source files for the same
       reason
    
    All modules are prefixed with apigen_ as the transpiler is only one part
    of a larger chain. Also provide a convenience module that includes all
    others:
    
      include(Apigen)
      ...
    
    The CMake module documentation format is reStructuredText, see:
    https://cmake.org/cmake/help/latest/manual/cmake-developer.7.html#module-documentation
    
    Resolves: APIGEN-245
    Change-Id: I76716e70aa48658b92ffd1952c1bec8ba2697333

---
+++ cmake/modules/Apigen.cmake
@@ -0,0 +1,28 @@
+# Copyright (c) 2017 HERE Europe B.V.
+#
+# All rights reserved.  This software, including documentation, is protected
+# by copyright controlled by HERE.  All rights are reserved.  Copying,
+# including reproducing, storing, adapting or translating, any or all of
+# this material requires the prior written consent of HERE.  This
+# material also contains confidential information which may not be
+# disclosed to others without the prior written consent of HERE.
+
+if(DEFINED includeguard_Apigen)
+  return()
+endif()
+set(includeguard_Apigen ON)
+
+cmake_minimum_required(VERSION 3.5)
+
+#.rst:
+# The main APIGEN module
+# ----------------------
+#
+# Includes all other APIGEN modules for convenience
+
+include(${CMAKE_CURRENT_LIST_DIR}/ApigenTargetIncludeDirectories.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/ApigenTargetSources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/ApigenTranspile.cmake)
+#TODO: include(${CMAKE_CURRENT_LIST_DIR}/ApigenJavaCompile.cmake)
+#TODO: include(${CMAKE_CURRENT_LIST_DIR}/ApigenJavaJar.cmake)
+#TODO: include(${CMAKE_CURRENT_LIST_DIR}/ApigenAndroidArchive.cmake)
