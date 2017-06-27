f3e3dae78607f19c26319077bad938e1940c479a
Sascha Peilicke <sascha.peilicke@here.com>
2017-06-27 17:39:34 +0200

    Further CMake modules for Android Archive generation
    We already have generic CMake modules that set include directories and
    additional sources for targets based on which generator is used (and
    hence indirectly which platform is the target).
    
    Add additional CMake modules to compile Java sources into *.class files,
    collecting *.class files into a *.jar file. Lastly, the module
    ApigenAndroidArchive collects outputs from many other CMake modules and
    collects them into a single Android Archive.
    
    In other words, we have a set of CMake modules that are target-platform
    agnostic and a couple which only make sense to run for a given target
    (say, "Android").
    
    Change-Id: I77887a9c57b6f4a5c064c841576c4e96e8677046

---
+++ cmake/modules/ApigenJavaCompile.cmake
@@ -0,0 +1,58 @@
+# Copyright (c) 2017 HERE Europe B.V.
+#
+# All rights reserved.  This software, including documentation, is protected
+# by copyright controlled by HERE.  All rights are reserved.  Copying,
+# including reproducing, storing, adapting or translating, any or all of
+# this material requires the prior written consent of HERE.  This
+# material also contains confidential information which may not be
+# disclosed to others without the prior written consent of HERE.
+
+if(DEFINED includeguard_ApigenJavaCompile)
+  return()
+endif()
+set(includeguard_ApigenJavaCompile ON)
+
+cmake_minimum_required(VERSION 3.5)
+
+#.rst:
+# ApigenJavaCompile
+# -------------------
+#
+# This module compiles Java code into *.class files for a target for which
+# apigen_transpile($target ... android) was run.
+#
+# .. command:: apigen_java_compile
+#
+# The general form of the command is::
+#
+#     apigen_java_compile(target)
+#
+
+find_package(Java REQUIRED)
+
+function(apigen_java_compile target)
+
+    get_target_property(GENERATOR ${target} APIGEN_TRANSPILER_GENERATOR)
+    get_target_property(OUTPUT_DIR ${target} APIGEN_TRANSPILER_GENERATOR_OUTPUT_DIR)
+
+    if(${GENERATOR} MATCHES android)
+
+        # Transpiler invocations for different generators need different output directories
+        # as the transpiler currently wipes the directory upon start.
+        set(APIGEN_JAVA_COMPILE_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/apigen/${GENERATOR}-java-compile)
+        # TODO: Use real package name!
+        set(APIGEN_TRANSPILER_JAVA_SOURCE_DIR ${OUTPUT_DIR}/com/here/android)
+
+        # Attach properties to target for re-use in other modules
+        set_target_properties(${target} PROPERTIES
+            APIGEN_JAVA_COMPILE_OUTPUT_DIR ${APIGEN_JAVA_COMPILE_OUTPUT_DIR})
+
+        file(GLOB JAVA_SOURCES ${APIGEN_TRANSPILER_JAVA_SOURCE_DIR}/*.java)
+        add_custom_command(TARGET ${target} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${APIGEN_JAVA_COMPILE_OUTPUT_DIR}
+            COMMAND find ${TRANSPILER_ANDROID_DIR} -name *.java | xargs "${Java_JAVAC_EXECUTABLE}" ${CMAKE_JAVA_COMPILE_FLAGS} -d ${APIGEN_JAVA_COMPILE_OUTPUT_DIR}
+            COMMENT "Compiling generated Java sources into class files...")
+
+    endif()
+
+endfunction(apigen_java_compile)
