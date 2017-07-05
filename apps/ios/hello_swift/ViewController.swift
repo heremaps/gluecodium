515550501e9712c377f580c4d673eda7a14e00dd
Sascha Peilicke <sascha@peilicke.de>
2017-07-05 17:36:28 +0200

    Add iOS Swift app
    Minimal skeleton with stub tests.
    
    Change-Id: Ie3d1e9b384d8b746d8403065f4291d8df4707c66
    Resolves: APIGEN-301

---
+++ apps/ios/hello_swift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  hello_swift
+//
+//  Copyright © 2017 HERE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
