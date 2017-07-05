0c22b87cc25fc3fa319908d986789456996d2a16
Daniel Kamkha <daniel.kamkha@here.com>
2017-07-05 09:16:00 +0200

    APIGEN-261: Use StubModelBuilder for method input parameters
    Updated StubModelBuilder to process method input parameters through the
    TreeWalker instead of directly.
    
    To facilitate this, split StubMethodMapper.mapMethodElements() into
    smaller parts:
    * mapMethodReturnType() for mapping return type and its comment
    * mapArgumentType() for mapping types of input and output arguments
    * buildCppMethod() for creating the resulting CppMethod instance and
    filling its fields (also moved this one to StubModelBuilder)
    
    Added/updated unit tests accordingly.
    
    Change-Id: Ic7d8a5a130967c1b31ee242e2778ad8c444e9e09

--- transpiler/src/main/java/com/here/ivi/api/generator/baseapi/StubModelBuilder.java
+++ transpiler/src/main/java/com/here/ivi/api/generator/baseapi/StubModelBuilder.java
@@ -13,13 +13,16 @@ package com.here.ivi.api.generator.baseapi;
 
 import com.here.ivi.api.generator.common.AbstractModelBuilder;
 import com.here.ivi.api.generator.common.ModelBuilderContextStack;
+import com.here.ivi.api.generator.common.NameHelper;
 import com.here.ivi.api.generator.common.cpp.CppNameRules;
 import com.here.ivi.api.model.cppmodel.CppClass;
 import com.here.ivi.api.model.cppmodel.CppElement;
 import com.here.ivi.api.model.cppmodel.CppMethod;
+import com.here.ivi.api.model.cppmodel.CppParameter;
+import com.here.ivi.api.model.cppmodel.CppType;
 import com.here.ivi.api.model.franca.FrancaElement;
-import java.util.List;
 import navigation.BaseApiSpec;
+import org.franca.core.franca.FArgument;
 import org.franca.core.franca.FInterface;
 import org.franca.core.franca.FMethod;
 
@@ -62,18 +65,58 @@ public class StubModelBuilder extends AbstractModelBuilder<CppElement> {
   @Override
   public void finishBuilding(FMethod francaMethod) {
 
-    // TODO: APIGEN-261 process method arguments through the Builder as well
-    StubMethodMapper.mapMethodElements(francaMethod, rootModel).forEach(this::storeResult);
+    StubMethodMapper.ReturnTypeData returnTypeData =
+        StubMethodMapper.mapMethodReturnType(francaMethod, rootModel);
+    CppMethod cppMethod = buildCppMethod(francaMethod, returnTypeData.type, returnTypeData.comment);
+
+    storeResult(cppMethod);
     closeContext();
   }
 
-  /*
-   * This method returns the namespace members for the contained FrancaElement.
-   *
-   * As cpp namespace is not inheriting from cpp element and cpp class has no
-   * namespace member, namespaces are not handled via tree walking but processed separately.
-   */
-  public List<String> getNamespaceMembers() {
-    return rootModel.getPackage();
+  @Override
+  public void finishBuildingInputArgument(FArgument francaArgument) {
+
+    CppParameter cppParameter = new CppParameter();
+    cppParameter.name = francaArgument.getName();
+    cppParameter.mode = CppParameter.Mode.Input;
+    cppParameter.type = StubMethodMapper.mapArgumentType(francaArgument, null, rootModel);
+
+    storeResult(cppParameter);
+    closeContext();
+  }
+
+  private CppMethod buildCppMethod(
+      FMethod francaMethod, CppType returnType, String returnTypeComment) {
+
+    String methodName =
+        CppNameRules.getMethodName(francaMethod.getName())
+            + NameHelper.toUpperCamelCase(francaMethod.getSelector());
+
+    CppMethod.Builder builder = new CppMethod.Builder(methodName).returnType(returnType);
+
+    if (rootModel.getPropertyAccessor().getStatic(francaMethod)) {
+      builder.specifier(CppMethod.Specifier.STATIC);
+    } else {
+      if (rootModel.getPropertyAccessor().getConst(francaMethod)) {
+        // const needs to be before "= 0" pure virtual specifier
+        builder.qualifier(CppMethod.Qualifier.CONST);
+      }
+      builder.specifier(CppMethod.Specifier.VIRTUAL);
+      builder.qualifier(CppMethod.Qualifier.PURE_VIRTUAL);
+    }
+
+    String methodComment = StubCommentParser.parse(francaMethod).getMainBodyText();
+    if (!returnTypeComment.isEmpty()) {
+      methodComment += StubCommentParser.FORMATTER.formatTag("@return", returnTypeComment);
+    }
+    builder.comment(methodComment);
+
+    for (CppElement cppElement : getCurrentContext().previousResults) {
+      if (cppElement instanceof CppParameter) {
+        builder.inParameter((CppParameter) cppElement);
+      }
+    }
+
+    return builder.build();
   }
 }
