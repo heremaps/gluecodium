c236d8c23d4059a850432293d986dabfbdc822ea
Dario Limongi <dario.limongi@here.com>
2017-07-06 14:28:24 +0200

    MINOR: Fix signed int types in JavaTypeMapper
    Java signed types have been fixed to return proper values:
    https://docs.oracle.com/javase/specs/jls/se7/html/jls-4.html#jls-4.2.1
    
    byte   8 bits
    short 16 bits
    int   32 bits
    long  64 bits
    
    Added unit tests for map() function.
    
    As temporary fix for unsigned ints, the biggest int value is returned so
    that more values can be stored
    
    Change-Id: I2b9688d77977c45bc5c13056c3c1e259b435ffbf

---
+++ transpiler/src/test/java/com/here/ivi/api/generator/common/java/JavaTypeMapperTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.common.java;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.here.ivi.api.model.javamodel.JavaType;
+import java.util.Arrays;
+import java.util.Collection;
+import org.franca.core.franca.FBasicTypeId;
+import org.franca.core.franca.FTypeRef;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(Parameterized.class)
+public class JavaTypeMapperTest {
+
+  private final FBasicTypeId francaBasicType;
+  private final String expectedJavaTypeName;
+
+  public JavaTypeMapperTest(final FBasicTypeId francaBasicType, final String expectedJavaTypeName) {
+    this.francaBasicType = francaBasicType;
+    this.expectedJavaTypeName = expectedJavaTypeName;
+  }
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> testData() {
+    return Arrays.asList(
+        new Object[][] {
+          {FBasicTypeId.BOOLEAN, "boolean"},
+          {FBasicTypeId.FLOAT, "float"},
+          {FBasicTypeId.DOUBLE, "double"},
+          {FBasicTypeId.INT8, "byte"},
+          {FBasicTypeId.INT16, "short"},
+          {FBasicTypeId.INT32, "int"},
+          {FBasicTypeId.INT64, "long"},
+          {FBasicTypeId.UINT8, "long"},
+          {FBasicTypeId.UINT16, "long"},
+          {FBasicTypeId.UINT32, "long"},
+          {FBasicTypeId.UINT64, "long"},
+          {FBasicTypeId.STRING, "String"},
+          {FBasicTypeId.BYTE_BUFFER, "byte[]"},
+          {FBasicTypeId.UNDEFINED, "void"},
+        });
+  }
+
+  @Mock private FTypeRef francaTypeRef;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void mapFrancaBasicTypeToJavaType() {
+    when(francaTypeRef.getPredefined()).thenReturn(francaBasicType);
+
+    JavaType returnedJavaType = JavaTypeMapper.map(francaTypeRef);
+
+    assertEquals(expectedJavaTypeName, returnedJavaType.getName());
+  }
+}
