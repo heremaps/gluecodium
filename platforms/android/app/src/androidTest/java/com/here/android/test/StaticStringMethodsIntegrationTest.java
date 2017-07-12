0daa25a6ece45c7d998c3287cb2f4828738cad34
Sascha Peilicke <sascha.peilicke@here.com>
2017-07-12 14:32:22 +0200

    Update Android integration tests
    Move them into the app/src/androidTest/java folder for proper IDEA
    integration. Add a couple more test-cases and file bugs for those that
    fail at the moment.
    
    Change-Id: I5c484c76c318697ba2477a46fa696f35a9c2fa9a
    Relates-To: APIGEN-274

---
+++ platforms/android/app/src/androidTest/java/com/here/android/test/StaticStringMethodsIntegrationTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+package com.here.android.test;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@Ignore // TODO(APIGEN-344,APIGEN-345): Currently segfaults
+public final class StaticStringMethodsIntegrationTest {
+    @Test
+    public void returnInputString() {
+        // Arrange
+        String inputString = "Foo";
+
+        // Act
+        String returnedString = StaticStringMethodsTest.returnInputString(inputString);
+
+        // Assert
+        assertEquals(inputString, returnedString);
+    }
+
+    @Test
+    public void concatenateStrings() {
+        // Arrange
+        String inputString1 = "Hello";
+        String inputString2 = "World";
+
+        // Act
+        String returnedString = StaticStringMethodsTest.concatenateStrings(inputString1, inputString2);
+
+        // Assert
+        assertEquals(inputString1 + inputString2, returnedString);
+    }
+
+    @Test
+    public void returnHelloString() {
+        // Arrange, act
+        String returnedString = StaticStringMethodsTest.returnHelloString();
+
+        // Assert
+        assertEquals("hello", returnedString);
+    }
+
+
+    @Test
+    public void returnEmpty() {
+        // Arrange, act
+        String returnedString = StaticStringMethodsTest.returnEmpty();
+
+        // Assert
+        assertTrue(returnedString.isEmpty());
+    }
+}
+
