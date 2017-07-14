6be13d17714afb27c4bd9b640d2d50f45bc88c00
Rafal Parzych <rafal.parzych@here.com>
2017-07-14 11:55:16 +0200

    APIGEN-254: Update iOS App for HelloWorld-BuiltinTypes
    Added Tab Bar Controller with two tabs
    - one for Base version with same functionality as previously
    - one for Builtin Types with calculator of loan repayment calling
     Stub for method 'methodWithFloatAndInteger'
    
    Change-Id: I9c96c50fec3ffa57a81b6d11c8d606907f18ccb6

--- platforms/ios/hello_swift/AppDelegate.swift
+++ platforms/ios/hello_swift/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let barButtonFont = UIFont.systemFont(ofSize: 15)
+        UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName: barButtonFont], for: UIControlState.normal)
         return true
     }
 
