49714d918f1a54e04d1beed5be82e9e0e20a08d6
Manuel Klahn <manuel.klahn@here.com>
2017-07-18 11:33:15 +0200

    APIGEN-316 Add functional java tests for byte arrays
    Change-Id: I59b0ad166093a5bebbdec11c0627c14731380e21

---
+++ libhello/src/test/StaticByteArrayMethodsTestStub.cpp
@@ -0,0 +1,40 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include <stdint.h>
+
+#include "stub/test/StaticByteArrayMethodsTestStub.h"
+
+namespace test {
+
+std::vector< uint8_t >
+StaticByteArrayMethodsTestStub::returnInputByteBuffer( const std::vector< uint8_t >& inputBuffer )
+{
+    return inputBuffer;
+}
+
+std::vector< uint8_t >
+StaticByteArrayMethodsTestStub::returnReverseByteBuffer( const std::vector< uint8_t >& inputBuffer )
+{
+    return {inputBuffer.rbegin(), inputBuffer.rend()};
+}
+
+std::vector< uint8_t >
+StaticByteArrayMethodsTestStub::concatenateByteBuffers( const std::vector< uint8_t >& input1,
+                                                        const std::vector< uint8_t >& input2 )
+{
+    std::vector< uint8_t > output( input1 );
+    output.insert( output.end(), input2.begin(), input2.end() );
+    return output;
+}
+
+}
---
+++ platforms/android/app/src/androidTest/java/com/here/android/test/StaticByteArrayMethodsIntegrationTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.android.test;
+
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public final class StaticByteArrayMethodsIntegrationTest {
+
+    private static final int LARGE_BYTE_ARRAY_SIZE = 100000;
+
+    @Test
+    public void reverseSmallByteArray() {
+        byte[] input = "InputBytes".getBytes();
+
+        byte[] output = StaticByteArrayMethodsTest.returnReverseByteBuffer(input);
+
+        assertEquals("setyBtupnI", new String(output));
+    }
+
+    @Test
+    public void emptyByteArray() {
+        byte[] input = {};
+
+        byte[] output = StaticByteArrayMethodsTest.returnInputByteBuffer(input);
+
+        assertTrue(Arrays.equals(input, output));
+    }
+
+    @Test
+    public void concatenateByteArrays() {
+        byte[] input1 = "byte".getBytes();
+        byte[] input2 = "array".getBytes();
+
+        byte[] output = StaticByteArrayMethodsTest.concatenateByteBuffers(input1, input2);
+
+        assertEquals("bytearray", new String(output));
+    }
+
+
+    @Test
+    public void largeByteArray() {
+        byte[] input = new byte[LARGE_BYTE_ARRAY_SIZE];
+        new Random().nextBytes(input);
+
+        byte[] output = StaticByteArrayMethodsTest.returnInputByteBuffer(input);
+
+        assertTrue(Arrays.equals(input, output));
+    }
+}
