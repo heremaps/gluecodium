77699cb5500469589a873fd7d90a5679b00f534e
Daniel Kamkha <daniel.kamkha@here.com>
2017-07-21 16:38:56 +0200

    APIGEN-372: Add "prefix" Trimou helper
    Added a "prefix" custom helper for the Trimou templates. The helper
    prefixes each line of a multi-line value with the given prefix string.
    
    Updated CppDocComment template to use the new helper.
    
    Added a new CppDocComment template test for a multiline comment.
    
    Change-Id: Ia638a5336f02540c6001e104d34d8079f4ab78e6

--- transpiler/src/main/java/com/here/ivi/api/generator/common/TemplateEngine.java
+++ transpiler/src/main/java/com/here/ivi/api/generator/common/TemplateEngine.java
@@ -11,19 +11,45 @@
 
 package com.here.ivi.api.generator.common;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.trimou.engine.MustacheEngine;
 import org.trimou.engine.MustacheEngineBuilder;
 import org.trimou.engine.locator.ClassPathTemplateLocator;
+import org.trimou.handlebars.BasicHelper;
+import org.trimou.handlebars.Options;
 
 public final class TemplateEngine {
 
   private static final MustacheEngine ENGINE;
+
+  /**
+   * Prefix each line of a multi-line value with a prefix.<br>
+   * Usage: {{prefix value "prefix"}}<br>
+   * Example: {{prefix comment "// "}}
+   */
+  private static class PrefixHelper extends BasicHelper {
+
+    @Override
+    public void execute(Options options) {
+      List<Object> parameters = options.getParameters();
+      if (parameters.isEmpty()) {
+        return;
+      }
+
+      final String prefix = (parameters.size() > 1) ? parameters.get(1).toString() : "";
+      List<String> strings = Arrays.asList(parameters.get(0).toString().split("\n"));
+      options.append(strings.stream().map(s -> prefix + s).collect(Collectors.joining("\n")));
+    }
+  }
 
   static {
     ENGINE =
         MustacheEngineBuilder.newBuilder()
             .addTemplateLocator(new ClassPathTemplateLocator(1, "templates", "mustache"))
             .setProperty("org.trimou.engine.config.skipValueEscaping", true)
+            .registerHelper("prefix", new PrefixHelper())
             .build();
   }
 
