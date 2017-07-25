eb79b0771674211a1611ef70cd5bd26cea019696
elkasimi <abdessamad.elkasimi@here.com>
2017-07-25 15:26:41 +0200

    APIGEN-365: Conversion from C++ to JNI Templates
    Added conversion templates for C++ to JNI conversions.
    Added tests for templates.
    
    Change-Id: Ie799612c3ca5aaa36dc309c3465c5bd5351c10d8

---
+++ transpiler/src/test/java/com/here/ivi/api/generator/common/jni/templates/CppToJniStructConversionBodyTest.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.common.jni.templates;
+
+import static org.junit.Assert.assertEquals;
+
+import com.here.ivi.api.generator.common.TemplateEngine;
+import com.here.ivi.api.generator.common.jni.JniField;
+import com.here.ivi.api.generator.common.jni.JniModel;
+import com.here.ivi.api.generator.common.jni.JniStruct;
+import com.here.ivi.api.model.cppmodel.CppCustomType;
+import com.here.ivi.api.model.cppmodel.CppField;
+import com.here.ivi.api.model.cppmodel.CppPrimitiveType;
+import com.here.ivi.api.model.cppmodel.CppStruct;
+import com.here.ivi.api.model.javamodel.JavaClass;
+import com.here.ivi.api.model.javamodel.JavaCustomType;
+import com.here.ivi.api.model.javamodel.JavaField;
+import com.here.ivi.api.model.javamodel.JavaPrimitiveType;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CppToJniStructConversionBodyTest {
+  private static final String OUTER_CLASS_NAME = "Outer";
+  private static final String INNER_CLASS_NAME = "Inner";
+  private static final List<String> PACKAGES = Arrays.asList("a", "b", "c");
+
+  private JniStruct jniStruct;
+
+  @Before
+  public void setUp() {
+    JniModel jniModel = new JniModel();
+    jniModel.javaPackages = PACKAGES;
+    jniModel.cppNameSpaces = PACKAGES;
+    jniModel.cppClassName = OUTER_CLASS_NAME;
+    jniModel.javaClassName = OUTER_CLASS_NAME;
+
+    jniStruct =
+        new JniStruct(
+            jniModel,
+            new JavaClass(INNER_CLASS_NAME),
+            new CppStruct(INNER_CLASS_NAME),
+            new LinkedList<>());
+  }
+
+  private static JniField createIntField() {
+    JavaField javaField =
+        new JavaField(new JavaPrimitiveType(JavaPrimitiveType.Type.INT), "intfield");
+    CppField cppField = new CppField(new CppPrimitiveType(CppPrimitiveType.Type.INT8), "cppInt");
+    return new JniField(javaField, cppField);
+  }
+
+  private static JniField createCustom() {
+    JavaField javaField = new JavaField(new JavaCustomType("JavaStructType"), "nestedStruct");
+    CppField cppField = new CppField(new CppCustomType("CppStructType"), "nestedCplusCplus");
+    return new JniField(javaField, cppField);
+  }
+
+  @Test
+  public void generateInt() {
+    jniStruct.fields.add(createIntField());
+    String expected =
+        "{\n"
+            + "  auto javaClass = env->FindClass("
+            + "\"L"
+            + String.join("/", PACKAGES)
+            + "/"
+            + OUTER_CLASS_NAME
+            + "$"
+            + INNER_CLASS_NAME
+            + "\");\n"
+            + "  auto result = here::internal::create_object(env, javaClass);\n"
+            + "  auto jcppInt = ninput.cppInt;\n"
+            + "  here::set_int_field(env, javaClass, result, \"intfield\", jcppInt);\n"
+            + "  return result;\n"
+            + "}";
+
+    String generated = TemplateEngine.render("jni/CppToJniStructConversionBody", jniStruct);
+
+    assertEquals(expected, generated);
+  }
+
+  @Test
+  public void generateCustom() {
+    jniStruct.fields.add(createCustom());
+    String innerSignature =
+        "L" + String.join("/", PACKAGES) + "/" + OUTER_CLASS_NAME + "$" + INNER_CLASS_NAME;
+    String expected =
+        "{\n"
+            + "  auto javaClass = env->FindClass("
+            + "\""
+            + innerSignature
+            + "\");\n"
+            + "  auto result = here::internal::create_object(env, javaClass);\n"
+            + "  auto jnestedCplusCplus = convert_CppStructType_to_jobject(env, ninput.nestedCplusCplus);\n"
+            + "  here::internal::set_object_field(env, javaClass, result, \"nestedStruct\",\n"
+            + "  \""
+            + innerSignature
+            + ";\", jnestedCplusCplus);\n"
+            + "  return result;\n"
+            + "}";
+
+    String generated = TemplateEngine.render("jni/CppToJniStructConversionBody", jniStruct);
+
+    assertEquals(expected, generated);
+  }
+
+  @Test
+  public void generateMultipleFields() {
+    jniStruct.fields.add(createIntField());
+    jniStruct.fields.add(createCustom());
+    String innerSignature =
+        "L" + String.join("/", PACKAGES) + "/" + OUTER_CLASS_NAME + "$" + INNER_CLASS_NAME;
+    String expected =
+        "{\n"
+            + "  auto javaClass = env->FindClass("
+            + "\""
+            + innerSignature
+            + "\");\n"
+            + "  auto result = here::internal::create_object(env, javaClass);\n"
+            + "  auto jcppInt = ninput.cppInt;\n"
+            + "  auto jnestedCplusCplus = convert_CppStructType_to_jobject(env, ninput.nestedCplusCplus);\n"
+            + "  here::set_int_field(env, javaClass, result, \"intfield\", jcppInt);\n"
+            + "  here::internal::set_object_field(env, javaClass, result, \"nestedStruct\",\n"
+            + "  \""
+            + innerSignature
+            + ";\", jnestedCplusCplus);\n"
+            + "  return result;\n"
+            + "}";
+    String generated = TemplateEngine.render("jni/CppToJniStructConversionBody", jniStruct);
+
+    assertEquals(expected, generated);
+  }
+}
---
+++ transpiler/src/test/java/com/here/ivi/api/generator/common/jni/templates/CppToJniStructConversionSignatureTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.common.jni.templates;
+
+import static org.junit.Assert.assertEquals;
+
+import com.here.ivi.api.generator.common.TemplateEngine;
+import com.here.ivi.api.generator.common.jni.JniModel;
+import com.here.ivi.api.generator.common.jni.JniStruct;
+import com.here.ivi.api.model.cppmodel.CppStruct;
+import com.here.ivi.api.model.javamodel.JavaClass;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class CppToJniStructConversionSignatureTest {
+  private static final String OUTER_CLASS_NAME = "Outer";
+  private static final String INNER_CLASS_NAME = "Inner";
+  private static final List<String> PACKAGES = Arrays.asList("a", "b", "c");
+
+  private JniStruct jniStruct;
+
+  @Before
+  public void setUp() {
+    JniModel jniModel = new JniModel();
+    jniModel.javaPackages = PACKAGES;
+    jniModel.cppNameSpaces = PACKAGES;
+    jniModel.cppClassName = OUTER_CLASS_NAME;
+    jniModel.javaClassName = OUTER_CLASS_NAME;
+
+    jniStruct =
+        new JniStruct(
+            jniModel, new JavaClass("Inner"), new CppStruct("Inner"), Collections.emptyList());
+  }
+
+  @Test
+  public void generate() {
+    // Act
+    String generated = TemplateEngine.render("jni/CppToJniStructConversionSignature", jniStruct);
+
+    String expected =
+        "jobject\n"
+            + "convert_"
+            + INNER_CLASS_NAME
+            + "_to_jobject(JNIEnv* env, const "
+            + String.join("::", PACKAGES)
+            + "::"
+            + OUTER_CLASS_NAME
+            + "::"
+            + INNER_CLASS_NAME
+            + "& ninput)";
+
+    // Assert
+    assertEquals(expected, generated);
+  }
+}
