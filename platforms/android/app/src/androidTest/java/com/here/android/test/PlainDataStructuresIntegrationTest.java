a38a79fe2fd4114a687020c692200dea0a00be21
Manuel Klahn <manuel.klahn@here.com>
2017-08-03 17:13:27 +0200

    APIGEN-266 Java integration tests for plain data structures
    Change-Id: Ic95405001637feddfc68b85b5c291b1c3f40dcd6

---
+++ platforms/android/app/src/androidTest/java/com/here/android/test/PlainDataStructuresIntegrationTest.java
@@ -0,0 +1,119 @@
+package com.here.android.test;
+
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class PlainDataStructuresIntegrationTest {
+
+    @Test
+    public void returnSimpleDataStructure() {
+        PlainDataStructuresTest.Point point = PlainDataStructuresTest.createPoint(1.0, 2.0);
+
+        assertNotNull(point);
+        assertEquals(1.0, point.x);
+        assertEquals(2.0, point.y);
+    }
+
+    @Test
+    public void manipulateSimpleDataStructure() {
+        PlainDataStructuresTest.Point point = new PlainDataStructuresTest.Point();
+        point.x = 1.0;
+        point.y = 2.0;
+
+        PlainDataStructuresTest.Point result = PlainDataStructuresTest.swapPointCoordinates(point);
+
+        assertNotNull(point);
+        assertEquals(2.0, result.x);
+        assertEquals(1.0, result.y);
+    }
+
+    @Test
+    public void createNestedDataStructureWithMultipleParams() {
+        PlainDataStructuresTest.Point point1 = PlainDataStructuresTest.createPoint(1.0, 2.0);
+        PlainDataStructuresTest.Point point2 = new PlainDataStructuresTest.Point();
+        point2.x = 3.0;
+        point2.y = 4.0;
+
+        PlainDataStructuresTest.Line line = PlainDataStructuresTest.createLine(point1, point2);
+
+        assertNotNull(line);
+        assertEquals(1.0, line.a.x);
+        assertEquals(2.0, line.a.y);
+        assertEquals(3.0, line.b.x);
+        assertEquals(4.0, line.b.y);
+    }
+
+    @Test
+    public void manifoldNestedDataStructure() {
+        PlainDataStructuresTest.Point point1 = PlainDataStructuresTest.createPoint(1.0, 2.0);
+        PlainDataStructuresTest.Point point2 = PlainDataStructuresTest.createPoint(3.0, 4.0);
+        PlainDataStructuresTest.Line line = PlainDataStructuresTest.createLine(point1, point2);
+        PlainDataStructuresTest.Color color = new PlainDataStructuresTest.Color();
+        color.red = 10;
+        color.green = 20;
+        color.blue = 30;
+
+        PlainDataStructuresTest.ColoredLine coloredLine =
+                PlainDataStructuresTest.createColoredLine(line, color);
+
+        assertNotNull(coloredLine);
+        assertEquals(1.0, coloredLine.line.a.x);
+        assertEquals(2.0, coloredLine.line.a.y);
+        assertEquals(3.0, coloredLine.line.b.x);
+        assertEquals(4.0, coloredLine.line.b.y);
+        assertEquals(10, coloredLine.color.red);
+        assertEquals(20, coloredLine.color.green);
+        assertEquals(30, coloredLine.color.blue);
+    }
+
+    @Test
+    public void modifyAllBuiltInAndCustomTypesDataStructure() {
+        PlainDataStructuresTest.AllTypesStruct allTypesStruct = new
+                PlainDataStructuresTest.AllTypesStruct();
+        allTypesStruct.booleanField = true;
+        allTypesStruct.bytesField = "hello".getBytes();
+        allTypesStruct.doubleField = 1.0;
+        allTypesStruct.floatField = 2.0f;
+        allTypesStruct.int8Field = 3;
+        allTypesStruct.int16Field = 4;
+        allTypesStruct.int32Field = 5;
+        allTypesStruct.int64Field = 6;
+        allTypesStruct.uint8Field = 7;
+        allTypesStruct.uint16Field = 8;
+        allTypesStruct.uint32Field = 9;
+        allTypesStruct.uint64Field = 10;
+        allTypesStruct.stringField = "test string";
+        allTypesStruct.pointField = PlainDataStructuresTest.createPoint(11.0, 12.0);
+
+        PlainDataStructuresTest.AllTypesStruct result =
+                PlainDataStructuresTest.modifyAllTypesStruct(allTypesStruct);
+
+        assertNotNull(result);
+        assertEquals(false, result.booleanField);
+        assertTrue(Arrays.equals("olleh".getBytes(), result.bytesField));
+        assertEquals(allTypesStruct.doubleField + 1.0, result.doubleField);
+        assertEquals(allTypesStruct.floatField + 1.0f, result.floatField);
+        assertEquals(allTypesStruct.int8Field + 1, result.int8Field);
+        assertEquals(allTypesStruct.int16Field + 1, result.int16Field);
+        assertEquals(allTypesStruct.int32Field + 1, result.int32Field);
+        assertEquals(allTypesStruct.int64Field + 1, result.int64Field);
+        assertEquals(allTypesStruct.uint8Field + 1, result.uint8Field);
+        assertEquals(allTypesStruct.uint16Field + 1, result.uint16Field);
+        assertEquals(allTypesStruct.uint32Field + 1, result.uint32Field);
+        assertEquals(allTypesStruct.uint64Field + 1, result.uint64Field);
+        assertEquals("Hello test string", result.stringField);
+        assertEquals(12.0, result.pointField.x);
+        assertEquals(11.0, result.pointField.y);
+    }
+}
