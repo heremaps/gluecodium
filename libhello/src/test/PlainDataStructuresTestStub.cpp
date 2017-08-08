c65b15ccc9b9e21a123b33e5d61fb7414794ad81
elkasimi <abdessamad.elkasimi@here.com>
2017-08-08 11:25:29 +0200

    APIGEN-412: Integration tests for type collections
    Change-Id: Ie687229cceb2009c715d86dd17dfa624c478e9e0

--- libhello/src/test/PlainDataStructuresTestStub.cpp
+++ libhello/src/test/PlainDataStructuresTestStub.cpp
@@ -14,10 +14,8 @@
 
 namespace test
 {
-
 PlainDataStructuresTestStub::Point
-PlainDataStructuresTestStub::createPoint(
-    const double x, const double y )
+PlainDataStructuresTestStub::createPoint( const double x, const double y )
 {
     PlainDataStructuresTestStub::Point output;
     output.x = x;
@@ -25,11 +23,29 @@ PlainDataStructuresTestStub::createPoint(
     return output;
 }
 
+typecollectiontest::Point
+PlainDataStructuresTestStub::createTypeCollectionTestPoint( const double x, const double y )
+{
+    typecollectiontest::Point output;
+    output.x = x;
+    output.y = y;
+    return output;
+}
+
 PlainDataStructuresTestStub::Point
-PlainDataStructuresTestStub::swapPointCoordinates(
-    const PlainDataStructuresTestStub::Point& point )
+PlainDataStructuresTestStub::swapPointCoordinates( const PlainDataStructuresTestStub::Point& point )
 {
     PlainDataStructuresTestStub::Point result;
+    result.x = point.y;
+    result.y = point.x;
+    return result;
+}
+
+typecollectiontest::Point
+PlainDataStructuresTestStub::swapTypeCollectionTestPointCoordinates(
+    const typecollectiontest::Point& point )
+{
+    typecollectiontest::Point result;
     result.x = point.y;
     result.y = point.x;
     return result;
@@ -45,9 +61,19 @@ PlainDataStructuresTestStub::createLine( const PlainDataStructuresTestStub::Poin
     return line;
 }
 
+typecollectiontest::Line
+PlainDataStructuresTestStub::createTypeCollectionTestLine( const typecollectiontest::Point& pointA,
+                                                           const typecollectiontest::Point& pointB )
+{
+    typecollectiontest::Line line;
+    line.a = pointA;
+    line.b = pointB;
+    return line;
+}
+
 PlainDataStructuresTestStub::ColoredLine
 PlainDataStructuresTestStub::createColoredLine( const PlainDataStructuresTestStub::Line& line,
-                                         const PlainDataStructuresTestStub::Color& color )
+                                                const PlainDataStructuresTestStub::Color& color )
 {
     PlainDataStructuresTestStub::ColoredLine coloredLine;
     coloredLine.line = line;
@@ -68,9 +94,19 @@ PlainDataStructuresTestStub::returnAllTypesStruct(
     return input;
 }
 
+typecollectiontest::ColoredLine
+PlainDataStructuresTestStub::createTypeCollectionTestColoredLine(
+    const typecollectiontest::Line& line, const typecollectiontest::Color& color )
+{
+    typecollectiontest::ColoredLine coloredLine;
+    coloredLine.line = line;
+    coloredLine.color = color;
+    return coloredLine;
+}
+
 PlainDataStructuresTestStub::AllTypesStruct
 PlainDataStructuresTestStub::modifyAllTypesStruct(
-                                         const PlainDataStructuresTestStub::AllTypesStruct& input )
+    const PlainDataStructuresTestStub::AllTypesStruct& input )
 {
     AllTypesStruct output;
     output.int8Field = input.int8Field + 1;
@@ -81,13 +117,35 @@ PlainDataStructuresTestStub::modifyAllTypesStruct(
     output.uint32Field = input.uint32Field + 1;
     output.int64Field = input.int64Field + 1;
     output.uint64Field = input.uint64Field + 1;
-    output.floatField =  input.floatField + 1.0f;
+    output.floatField = input.floatField + 1.0f;
     output.doubleField = input.doubleField + 1.0;
     output.stringField = "Hello " + input.stringField;
     output.booleanField = !input.booleanField;
-    output.bytesField = { input.bytesField.rbegin(), input.bytesField.rend() };
+    output.bytesField = {input.bytesField.rbegin( ), input.bytesField.rend( )};
     output.pointField = PlainDataStructuresTestStub::swapPointCoordinates( input.pointField );
     return output;
 }
 
+typecollectiontest::AllTypesStruct
+PlainDataStructuresTestStub::modifyTypeCollectionTestAllTypesStruct(
+    const typecollectiontest::AllTypesStruct& input )
+{
+    typecollectiontest::AllTypesStruct output;
+    output.int8Field = input.int8Field + 1;
+    output.uint8Field = input.uint8Field + 1;
+    output.int16Field = input.int16Field + 1;
+    output.uint16Field = input.uint16Field + 1;
+    output.int32Field = input.int32Field + 1;
+    output.uint32Field = input.uint32Field + 1;
+    output.int64Field = input.int64Field + 1;
+    output.uint64Field = input.uint64Field + 1;
+    output.floatField = input.floatField + 1.0f;
+    output.doubleField = input.doubleField + 1.0;
+    output.stringField = "Hello " + input.stringField;
+    output.booleanField = !input.booleanField;
+    output.bytesField = {input.bytesField.rbegin( ), input.bytesField.rend( )};
+    output.pointField
+        = PlainDataStructuresTestStub::swapTypeCollectionTestPointCoordinates( input.pointField );
+    return output;
+}
 }
