ea69430192ff78f70413d556b3e0bb84d4d60063
Sascha Ebel <sascha.ebel@here.com>
2017-08-18 15:19:55 +0200

    APIGEN-291: converts JavaPackage template from xtend to mustache
    Change-Id: I5d92dfb043ca449f56f05923a2b45f38a4d5705e

---
+++ transpiler/src/test/java/com/here/ivi/api/generator/common/java/templates/JavaPackageTemplateTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.common.java.templates;
+
+import static org.junit.Assert.assertEquals;
+
+import com.here.ivi.api.generator.common.TemplateEngine;
+import com.here.ivi.api.model.javamodel.JavaPackage;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class JavaPackageTemplateTest {
+
+  @Test
+  public void packageGeneration() {
+    JavaPackage javaPackage = new JavaPackage(Arrays.asList("com", "here", "SomeImport"));
+
+    String expected = "package com.here.SomeImport;\n";
+
+    String generated = TemplateEngine.render("java/Package", javaPackage);
+
+    assertEquals(expected, generated);
+  }
+}
