f06959a511e73ade29616d48d0997dbea8dd9c43
elkasimi <abdessamad.elkasimi@here.com>
2017-08-18 12:04:50 +0200

    APIGEN-549: Fix TypeCollection Header name
    Fixed type collection name header from:
    stub/my/own/package/typecollectionname.h
    to:
    stub/my/own/package/TypeCollectionName.h
    
    Added regression tests.
    
    Simplified some test function calls with static imports.
    
    Change-Id: I42761129ff82a27484e0abceebf11335022f292b

--- transpiler/src/test/java/com/here/ivi/api/generator/common/cpp/CppNameRulesTest.java
+++ transpiler/src/test/java/com/here/ivi/api/generator/common/cpp/CppNameRulesTest.java
@@ -13,10 +13,15 @@ package com.here.ivi.api.generator.common.cpp;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
 import com.here.ivi.api.model.franca.DefinedBy;
 import com.here.ivi.api.model.franca.FrancaElement;
+import com.here.ivi.api.model.franca.Interface;
+import com.here.ivi.api.model.franca.ModelInfo;
+import com.here.ivi.api.model.franca.TypeCollection;
 import java.util.Arrays;
 import java.util.List;
 import org.franca.core.franca.FInterface;
@@ -45,6 +50,9 @@ public class CppNameRulesTest {
   @Mock private FTypeCollection fTypeCollection;
   @Mock private FInterface fInterface;
   @Mock private FType fType;
+  @Mock private ModelInfo modelInfo;
+  @Mock private Interface anInterface;
+  @Mock private TypeCollection typeCollection;
 
   @Before
   public void setUp() {
@@ -75,13 +83,13 @@ public class CppNameRulesTest {
     assertEquals(Arrays.asList("my", "fancy", "package", "IfaceStub"), qualifier);
 
     //verify
-    PowerMockito.verifyStatic();
+    verifyStatic();
     DefinedBy.createFromFModelElement(fType);
-    Mockito.verify(mockFrancaModel).getFrancaTypeCollection();
-    Mockito.verify(mockFrancaModel, Mockito.times(2)).getModelInfo();
-    Mockito.verify(mockFrancaModel.getModelInfo()).getFModel();
-    Mockito.verify(fInterface).getName();
-    Mockito.verify(fModel).getName();
+    verify(mockFrancaModel).getFrancaTypeCollection();
+    verify(mockFrancaModel, Mockito.times(2)).getModelInfo();
+    verify(mockFrancaModel.getModelInfo()).getFModel();
+    verify(fInterface).getName();
+    verify(fModel).getName();
   }
 
   @Test
@@ -103,12 +111,48 @@ public class CppNameRulesTest {
     assertEquals(Arrays.asList("my", "fancy", "package", "tcollection"), qualifier);
 
     //verify
-    PowerMockito.verifyStatic();
+    verifyStatic();
     DefinedBy.createFromFModelElement(fType);
-    Mockito.verify(mockFrancaModel).getFrancaTypeCollection();
-    Mockito.verify(mockFrancaModel, Mockito.times(2)).getModelInfo();
-    Mockito.verify(mockFrancaModel.getModelInfo()).getFModel();
-    Mockito.verify(fTypeCollection).getName();
-    Mockito.verify(fModel).getName();
+    verify(mockFrancaModel).getFrancaTypeCollection();
+    verify(mockFrancaModel, Mockito.times(2)).getModelInfo();
+    verify(mockFrancaModel.getModelInfo()).getFModel();
+    verify(fTypeCollection).getName();
+    verify(fModel).getName();
+  }
+
+  @Test
+  public void getHeaderNameForInterface() {
+    List<String> packageNames = Arrays.asList("my", "fancy", "package");
+    when(anInterface.getModelInfo()).thenReturn(modelInfo);
+    when(modelInfo.getPackageNames()).thenReturn(packageNames);
+    when(anInterface.getFrancaInterface()).thenReturn(fInterface);
+    when(fInterface.getName()).thenReturn("FancyName");
+
+    String headerPath = CppNameRules.getHeaderPath(anInterface);
+
+    assertEquals("stub/my/fancy/package/FancyNameStub.h", headerPath);
+
+    verify(anInterface).getModelInfo();
+    verify(modelInfo).getPackageNames();
+    verify(anInterface).getFrancaInterface();
+    verify(fInterface).getName();
+  }
+
+  @Test
+  public void getHeaderNameForTypeCollection() {
+    List<String> packageNames = Arrays.asList("my", "fancy", "package");
+    when(typeCollection.getModelInfo()).thenReturn(modelInfo);
+    when(modelInfo.getPackageNames()).thenReturn(packageNames);
+    when(typeCollection.getFrancaTypeCollection()).thenReturn(fTypeCollection);
+    when(fTypeCollection.getName()).thenReturn("FancyTypeCollectionName");
+
+    String headerPath = CppNameRules.getHeaderPath(typeCollection);
+
+    assertEquals("stub/my/fancy/package/FancyTypeCollectionName.h", headerPath);
+
+    verify(typeCollection).getModelInfo();
+    verify(modelInfo).getPackageNames();
+    verify(typeCollection).getFrancaTypeCollection();
+    verify(fTypeCollection).getName();
   }
 }
