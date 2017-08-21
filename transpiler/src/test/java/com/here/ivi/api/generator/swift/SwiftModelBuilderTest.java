c45ecf08de4e3e31c7d23cda3672d4451e322d62
Rafal Parzych <rafal.parzych@here.com>
2017-08-21 15:12:29 +0200

    APIGEN-537: Make SwiftGenerator to use FrancaTreeWalker
    - added separate model classes for in and out parameters
    - added SwiftModelBuilder
    - added unit tests
    
    Support for PlainDataStructures will be added in subsequent commits.
    
    Change-Id: I1762ff4679c2a49629b89cda4b1664523171ce43

---
+++ transpiler/src/test/java/com/here/ivi/api/generator/swift/SwiftModelBuilderTest.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.swift;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import com.here.ivi.api.common.CollectionsHelper;
+import com.here.ivi.api.generator.baseapi.StubCommentParser;
+import com.here.ivi.api.generator.common.AbstractFrancaCommentParser;
+import com.here.ivi.api.model.franca.Interface;
+import com.here.ivi.api.model.franca.ModelInfo;
+import com.here.ivi.api.model.swift.SwiftClass;
+import com.here.ivi.api.model.swift.SwiftInParameter;
+import com.here.ivi.api.model.swift.SwiftMethod;
+import com.here.ivi.api.model.swift.SwiftModelElement;
+import com.here.ivi.api.model.swift.SwiftParameter;
+import com.here.ivi.api.model.swift.SwiftType;
+import com.here.ivi.api.test.MockContextStack;
+import java.util.List;
+import navigation.BaseApiSpec.InterfacePropertyAccessor;
+import org.franca.core.franca.FArgument;
+import org.franca.core.franca.FInterface;
+import org.franca.core.franca.FMethod;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({SwiftTypeMapper.class, StubCommentParser.class})
+public class SwiftModelBuilderTest {
+  private static final String PARAM_NAME = "someParamName";
+  private static final String FUNCTION_NAME = "someFunctionName";
+  private static final String COMMENT = "some comment on model element";
+  private static final List<String> PACKAGES = asList("PKG1", "PKG2");
+
+  private MockContextStack<SwiftModelElement> contextStack = new MockContextStack<>();
+  @Mock private AbstractFrancaCommentParser.Comments comments;
+  @Mock private SwiftNameRules nameRules;
+  @Mock private Interface<InterfacePropertyAccessor> anInterface;
+  @Mock private InterfacePropertyAccessor propertyAccessor;
+  @Mock private ModelInfo modelInfo;
+  @Mock private FMethod francaMethod;
+  @Mock private FArgument francaArgument;
+  @Mock private FInterface francaInterface;
+  @Mock private SwiftType swiftType;
+  private SwiftModelBuilder modelBuilder;
+
+  @Before
+  public void setUp() {
+    mockStatic(SwiftTypeMapper.class, StubCommentParser.class);
+    when(SwiftTypeMapper.mapType(any())).thenReturn(swiftType);
+    when(StubCommentParser.parse(any(FMethod.class))).thenReturn(comments);
+    when(StubCommentParser.parse(any(FInterface.class))).thenReturn(comments);
+    when(comments.getMainBodyText()).thenReturn(COMMENT);
+
+    when(anInterface.getModelInfo()).thenReturn(modelInfo);
+    when(modelInfo.getPackageNames()).thenReturn(PACKAGES);
+    when(anInterface.getPropertyAccessor()).thenReturn(propertyAccessor);
+    when(propertyAccessor.getStatic(any())).thenReturn(true);
+    when(francaArgument.getName()).thenReturn(PARAM_NAME);
+    when(nameRules.getParameterName(any())).thenReturn(PARAM_NAME);
+    when(nameRules.getMethodName(any())).thenReturn(FUNCTION_NAME);
+
+    modelBuilder = new SwiftModelBuilder(anInterface, nameRules, contextStack);
+  }
+
+  @Test
+  public void finishBuildingInputArgumentReturnsCreatedParams() {
+    modelBuilder.finishBuildingInputArgument(francaArgument);
+
+    List<SwiftParameter> params = getResults(SwiftParameter.class);
+    assertEquals(1, params.size());
+    assertEquals(PARAM_NAME, params.get(0).variableName);
+    assertSame(swiftType, params.get(0).type);
+  }
+
+  @Test
+  public void finishBuildingOutputArgumentReturnsCreatedParam() {
+    modelBuilder.finishBuildingOutputArgument(francaArgument);
+
+    List<SwiftParameter> params = getResults(SwiftParameter.class);
+    assertEquals(1, params.size());
+    assertEquals(PARAM_NAME, params.get(0).variableName);
+    assertSame(swiftType, params.get(0).type);
+  }
+
+  @Test
+  public void finishBuildingMethodCreatesStaticMethod() {
+    when(propertyAccessor.getStatic(any())).thenReturn(true);
+
+    modelBuilder.finishBuilding(francaMethod);
+
+    List<SwiftMethod> methods = getResults(SwiftMethod.class);
+    assertEquals(1, methods.size());
+    assertEquals("Method is marked as static", true, methods.get(0).isStatic);
+  }
+
+  @Test
+  public void finishBuildingMethodCreatesNonStaticMethod() {
+    when(propertyAccessor.getStatic(any())).thenReturn(false);
+
+    modelBuilder.finishBuilding(francaMethod);
+
+    List<SwiftMethod> methods = getResults(SwiftMethod.class);
+    assertEquals(1, methods.size());
+    assertEquals("Method is marked as non-static", false, methods.get(0).isStatic);
+  }
+
+  @Test
+  public void finishBuildingCreatesMethodWithoutParams() {
+    modelBuilder.finishBuilding(francaMethod);
+
+    List<SwiftMethod> methods = getResults(SwiftMethod.class);
+    assertEquals(1, methods.size());
+    SwiftMethod function = methods.get(0);
+    assertEquals(SwiftType.VOID, function.returnType);
+    assertEquals(0, function.parameters.size());
+    assertEquals(FUNCTION_NAME, function.name);
+  }
+
+  @Test
+  public void finishBuildingCreatesMethodWithParam() {
+    SwiftInParameter param = new SwiftInParameter(PARAM_NAME, new SwiftType("someType"));
+    contextStack.injectResult(param);
+
+    modelBuilder.finishBuilding(francaMethod);
+
+    List<SwiftMethod> methods = getResults(SwiftMethod.class);
+    assertEquals(1, methods.size());
+    SwiftMethod method = methods.get(0);
+    assertEquals(1, method.parameters.size());
+    assertSame(param, method.parameters.get(0));
+  }
+
+  @Test
+  public void finishBuildingCreatesInterface() {
+    SwiftMethod method = new SwiftMethod(FUNCTION_NAME);
+    contextStack.injectResult(method);
+
+    modelBuilder.finishBuilding(francaInterface);
+
+    List<SwiftClass> classes = getResults(SwiftClass.class);
+    assertEquals(1, classes.size());
+    SwiftClass clazz = classes.get(0);
+    assertEquals(1, clazz.methods.size());
+    assertSame(method, clazz.methods.get(0));
+    assertEquals(1, clazz.imports.size());
+    assertEquals(String.join("_", PACKAGES), clazz.nameSpace);
+  }
+
+  private <T extends SwiftModelElement> List<T> getResults(Class<T> clazz) {
+    return CollectionsHelper.getAllOfType(contextStack.getParentContext().previousResults, clazz);
+  }
+}
