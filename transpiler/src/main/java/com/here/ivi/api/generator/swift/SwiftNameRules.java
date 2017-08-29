243817292d7a0245c257c542e626308a8008a577
Alexander Lehmann <alexander.lehmann@here.com>
2017-08-29 11:56:24 +0200

    MINOR Refactor Xtend classes to Java
    SwiftNameRules and SwiftGeneratorIntegrationTest have to be
    modified for APIGEN-267. To prevent adding to a deprecated
    Xtend file and to enable testing, the files get converted to
    Java.
    
    I prettified low hanging fruit. Any additional changes should be done
    independently.
    
    Change-Id: I8b14bfffbf86b2cc005d3d219e9938d2a90c8f71

---
+++ transpiler/src/main/java/com/here/ivi/api/generator/swift/SwiftNameRules.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.generator.swift;
+
+import com.here.ivi.api.generator.common.NameHelper;
+import com.here.ivi.api.model.franca.FrancaElement;
+import com.here.ivi.api.model.franca.Interface;
+import java.io.File;
+import org.franca.core.franca.FArgument;
+import org.franca.core.franca.FMethod;
+import org.franca.core.franca.FTypeCollection;
+
+public class SwiftNameRules {
+  public String getImplementationFileName(final FrancaElement<?> francaElement) {
+    return "swift"
+        + File.separator
+        + String.join(File.separator, francaElement.getModelInfo().getPackageNames())
+        + File.separator
+        + getFileName(francaElement)
+        + ".swift";
+  }
+
+  private String getFileName(final FrancaElement<?> francaElement) {
+    String switchResult = null;
+    if (francaElement instanceof Interface) {
+      switchResult =
+          SwiftNameRules.computeClassName(((Interface<?>) francaElement).getFrancaInterface());
+    } else {
+      switchResult = this.getTypeCollectionName(francaElement.getFrancaTypeCollection());
+    }
+    return switchResult;
+  }
+
+  public String getMethodName(final FMethod method) {
+    return method.getName();
+  }
+
+  public String getParameterName(final FArgument argument) {
+    return argument.getName();
+  }
+
+  public String getClassName(final FTypeCollection base) {
+    return SwiftNameRules.computeClassName(base);
+  }
+
+  public String getTypeCollectionName(final FTypeCollection base) {
+    return NameHelper.toUpperCamelCase(base.getName());
+  }
+
+  public static String computeClassName(final FTypeCollection base) {
+    return NameHelper.toUpperCamelCase(base.getName());
+  }
+}
