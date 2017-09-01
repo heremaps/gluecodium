48f79ca997be461590c7ccc19fc1f82c23b33393
Alexander Lehmann <alexander.lehmann@here.com>
2017-09-01 17:52:54 +0200

    APIGEN-269 Update iOS App for HelloWorld-PlainDataStructures
    Also add HelloWorldWorldPlainDataStructures to be generated for
    iOS
    
    Fixed white-space issues on BaseBaseViewController.
    
    Change-Id: I19e724a25c5dfdd196eb53369be0f56a512804f9

--- platforms/ios/hello_swift/BaseViewController.swift
+++ platforms/ios/hello_swift/BaseViewController.swift
@@ -11,23 +11,21 @@ import hello
 class BaseViewController: UIViewController {
     @IBOutlet var textInput: UITextField!
     @IBOutlet var textLabel: UILabel!
-    
+
     @IBAction func didPressButton(_ sender: Any) {
         textInput.resignFirstResponder()
         if let text = textInput.text {
             textLabel.text = HelloWorld.helloWorldMethod(inputString: text)
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
-
