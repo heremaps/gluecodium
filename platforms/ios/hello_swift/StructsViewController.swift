a38f4b2df53b66e1ea840ddf37431e0e87ab3a1d
Alexander Lehmann <alexander.lehmann@here.com>
2017-09-13 18:59:09 +0200

    APIGEN-269 Update iOS App for Helloworld Plain Datastructures
    Beautifications
    
    Change-Id: I769479f2d0d88c29822e268add39db25175ec1c5

--- platforms/ios/hello_swift/StructsViewController.swift
+++ platforms/ios/hello_swift/StructsViewController.swift
@@ -10,41 +10,38 @@ import hello
 
 class StructsViewController: UIViewController {
 
-    @IBOutlet var lastSyncTextField: UITextField!
     @IBOutlet var numberOfTimesSyncedTextField: UITextField!
     @IBOutlet var extendedNumberOfTimesSyncedTextField: UITextField!
 
     var syncResult: HelloWorldPlainDataStructures.SyncResult
     var extendedSyncResult: HelloWorldPlainDataStructures.IdentifiableSyncResult
 
-    let dateFormater = DateFormatter()
-
     required init?(coder aDecoder: NSCoder) {
         syncResult = HelloWorldPlainDataStructures.SyncResult(lastUpdatedTimeStamp: 0, numberOfChanges: 0)
         extendedSyncResult = HelloWorldPlainDataStructures.IdentifiableSyncResult(id: 42, syncResult: syncResult)
         super.init(coder: aDecoder)
-
     }
 
     override func viewDidLoad() {
-        lastSyncTextField.text = "unknown"
-        numberOfTimesSyncedTextField.text = "n/a"
-        extendedNumberOfTimesSyncedTextField.text = "n/a"
+        populateFields()
+    }
+
+    func populateFields() {
+        numberOfTimesSyncedTextField.text = String(syncResult.numberOfChanges)
+        extendedNumberOfTimesSyncedTextField.text = String(extendedSyncResult.syncResult.numberOfChanges)
     }
 
     @IBAction func didPressSync(_ sender: Any) {
-
         if let result = HelloWorldPlainDataStructures.methodWithNonNestedType(input: syncResult) {
             syncResult = result
-            lastSyncTextField.text = dateFormater.string(from: Date(timeIntervalSince1970: TimeInterval(syncResult.lastUpdatedTimeStamp)))
-            numberOfTimesSyncedTextField.text = String(syncResult.numberOfChanges)
+            populateFields()
         }
     }
 
     @IBAction func disPressExtendedSync(_ sender: Any) {
         if let identifiableResult = HelloWorldPlainDataStructures.methodWithNestedType(input: extendedSyncResult) {
             extendedSyncResult = identifiableResult
-            extendedNumberOfTimesSyncedTextField.text = String(extendedSyncResult.syncResult.numberOfChanges)
+            populateFields()
         }
     }
 }
