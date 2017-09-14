1f92a1c4f3826b936ea844280e5531517bd6899e
Manuel Klahn <manuel.klahn@here.com>
2017-09-14 16:20:36 +0200

    APIGEN-614 Add HelloWorldProfileManager to android build + implementation
    - Add HelloWorldProfileManager and its Factory to android build to
      verify basic non-nested instance handling.
    - Add cpp implementations for them
    - Add java integration test
    
    Change-Id: I901a00c12255fe4620cffa10ae7421142f0a8930

---
+++ libhello/src/hello/HelloWorldProfileManager.cpp
@@ -0,0 +1,45 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include <string>
+#include <memory>
+#include "HelloWorldProfileManager.h"
+
+namespace hello {
+
+// Virtual destructor implementation is required to avoid UnsatisfiedLink error
+ProfileManager::~ProfileManager( ) {}
+
+void
+HelloWorldProfileManager::createProfile( const ::std::string& username )
+{
+    m_profile_name = username;
+}
+
+
+::std::string
+HelloWorldProfileManager::changeProfile( const ::std::string& username )
+{
+     std::string old = m_profile_name;
+     m_profile_name = username;
+     return old;
+}
+
+::std::string
+HelloWorldProfileManager::deleteProfile( )
+{
+     std::string old = m_profile_name;
+     m_profile_name = "";
+     return old;
+}
+
+}
