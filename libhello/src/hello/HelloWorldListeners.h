fec4b16be322a780d743619cbd5a854c0aa91077
elkasimi <abdessamad.elkasimi@here.com>
2017-09-15 11:21:57 +0200

    APIGEN-514: Implement libhello for HelloWorld-Listeners
    Change-Id: Ifecb27be372a96d3593999e7aefb7c3beb124f96

---
+++ libhello/src/hello/HelloWorldListeners.h
@@ -0,0 +1,76 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "hello/Calculator.h"
+
+#include <vector>
+
+namespace hello
+{
+class HelloCalculatorListener : public CalculatorListener
+{
+public:
+    HelloCalculatorListener( ) = default;
+    ~HelloCalculatorListener( ) = default;
+
+public:
+    /**
+     * The method to call on the listener when the calculation is done
+     */
+    void onCalculationResult( const double calculationResult ) override;
+
+    /**
+     * The method to call on the listener when the background calculation
+     * is done
+     */
+    void onCalculationInBackgroundResult( const double calculationResult ) override;
+};
+
+class HelloCalculator : public Calculator
+{
+public:
+    HelloCalculator( ) = default;
+    ~HelloCalculator( ) = default;
+
+    /**
+     * register listener to calculateInBackground results notifications
+     */
+    void registerListener(
+        const ::std::shared_ptr< ::hello::CalculatorListener >& listener ) override;
+
+    /**
+     * unregister listener to calculateInBackground results notifications
+     */
+    void unregisterListener(
+        const ::std::shared_ptr< ::hello::CalculatorListener >& listener ) override;
+
+    /**
+     * Compute the Euclidean distance between two positions.
+     *     It is guaranteed that listener will be called before method returns
+     */
+    void calculate( const ::hello::Calculator::Position& fromPosition,
+                    const ::hello::Calculator::Position& toPosition,
+                    const ::std::shared_ptr< ::hello::CalculatorListener >& listener ) override;
+
+    /**
+     * start computing the Euclidean distance between two positions and notify the registered
+     *     listeners when it is done
+     */
+    void calculateInBackground( const ::hello::Calculator::Position& fromPosition,
+                                const ::hello::Calculator::Position& toPosition ) override;
+
+private:
+    ::std::vector< ::std::shared_ptr< ::hello::CalculatorListener > > m_listeners;
+};
+}
