c99eeee5eb5e8f22359fe0a40a98226efe22f047
Daniel Kamkha <daniel.kamkha@here.com>
2017-09-25 14:05:37 +0200

    APIGEN-652: Validate interface names in NameValidator
    Added validation against duplicate interface names to NameValidator
    (formerly known as TypeNameValidator). Added corresponding unit tests.
    
    Change-Id: Ia6c1e6d71925f7acca238f215688acec8736aec0

---
+++ transpiler/src/main/java/com/here/ivi/api/validator/common/NameValidator.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ */
+
+package com.here.ivi.api.validator.common;
+
+import com.here.ivi.api.model.franca.FrancaModel;
+import com.here.ivi.api.model.franca.Interface;
+import com.here.ivi.api.model.franca.TypeCollection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import org.franca.core.franca.FModelElement;
+
+public class NameValidator {
+
+  private static final Logger LOGGER = Logger.getLogger(NameValidator.class.getName());
+
+  public static boolean validate(final FrancaModel model) {
+    return checkInterfaceNames(model) && checkTypeNamesInTypeCollection(model);
+  }
+
+  private static boolean checkTypeNamesInTypeCollection(final FrancaModel model) {
+    Map<String, List<String>> packageNameMapping = new HashMap<>();
+    for (TypeCollection typeCollection : model.getTypeCollections()) {
+
+      String packageName = typeCollection.getFrancaModel().getName();
+      List<String> value =
+          packageNameMapping.computeIfAbsent(packageName, key -> new LinkedList<>());
+      value.addAll(
+          typeCollection
+              .getFrancaTypeCollection()
+              .getTypes()
+              .stream()
+              .map(FModelElement::getName)
+              .collect(Collectors.toList()));
+    }
+
+    return checkForDuplicateNames(packageNameMapping, "type");
+  }
+
+  private static boolean checkInterfaceNames(final FrancaModel model) {
+
+    Map<String, List<String>> packageNameMapping = new HashMap<>();
+    for (Interface anInterface : model.getInterfaces()) {
+
+      String packageName = anInterface.getFrancaModel().getName();
+      List<String> value =
+          packageNameMapping.computeIfAbsent(packageName, key -> new LinkedList<>());
+      value.add(anInterface.getName());
+    }
+
+    return checkForDuplicateNames(packageNameMapping, "interface");
+  }
+
+  private static boolean checkForDuplicateNames(
+      final Map<String, List<String>> packageNameMapping, final String entityName) {
+
+    for (Map.Entry<String, List<String>> entry : packageNameMapping.entrySet()) {
+      List<String> names = entry.getValue();
+      if (names.size() != new HashSet<>(names).size()) {
+        LOGGER.severe("Package '" + entry.getKey() + "' has duplicate " + entityName + " names");
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
