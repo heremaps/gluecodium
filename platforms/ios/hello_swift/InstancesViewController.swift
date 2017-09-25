6114ae07571da96855badd046682f0d9aaa39889
Palmero, Antonio <antonio.palmero@here.com>
2017-09-25 16:04:37 +0200

    APIGEN-196 - Instances example in HelloWorld.
    Change-Id: I555d44b6a210d4acb2a01e8a14e238e1b4d17158

---
+++ platforms/ios/hello_swift/InstancesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InstancesViewController.swift
+//  hello_swift
+//
+//  Copyright © 2017 HERE. All rights reserved.
+//
+
+
+import UIKit
+import hello
+
+class InstancesViewController: UIViewController {
+
+    @IBOutlet weak var profileTextView: UITextField!
+    @IBOutlet weak var profileLabel: UILabel!
+
+    var profile : ProfileManager = HelloWorldProfileManagerFactory.createProfileManagerInstance()!
+
+    override func viewDidLoad() {
+        profile.createProfile(username: "Default Profile")
+        super.viewDidLoad()
+    }
+
+    @IBAction func createNewProfile(_ sender: Any) {
+        profileLabel.isHidden = false
+        profileLabel.text = profile.changeProfile(username: profileTextView.text ?? "")
+    }
+
+}
