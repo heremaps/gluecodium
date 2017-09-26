b65b935474e1954d9c170597eb623f430ece8d7e
Daniel Kamkha <daniel.kamkha@here.com>
2017-09-26 16:40:33 +0200

    APIGEN-720: Add smoke test infrastructure
    Created SmokeTest parametrized test.
    
    Parameter input data for the test is created by:
    * fetching a list of features-under-test by listing all sub-directories
    of "test/resources/smoke"
    * fetching a list of available generators
    * creating all possible "feature+generator" pairs
    
    Each test run:
    * fetches all reference files from "featureName/generatorName" sub-
    directory of "test/resources/smoke"
    * runs the generator using "featureName" sub-directory of
    "test/resources/smoke" as input directory (using all .fidl and .fdepl
    files inside )
    * makes sure each reference file has a corresponding file in the
    generated results
    * compares each reference file contents to the contents of the
    corresponding generated file
    
    Added a smoke test for the "Basic" milestone/feature as a showcase of
    the smoke-testing infrastructure. Other smoke tests for other features
    will be added separately.
    
    Updated Spotless config to ignore "resources" directory, so that the
    "reference" files are not affected by it.
    
    Change-Id: I6f05fb948c49628f17376623f8ed6974e86af422

---
+++ transpiler/src/test/resources/smoke/basic/cpp/smoke/Basic.h
@@ -0,0 +1,34 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+// 
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <string>
+
+namespace smoke {
+
+class Basic {
+public:
+/**
+ * Example static method for Basic feature which takes a String and returns a String
+ * * @param[in] inputString Input string
+ * * @return The result type, containing ::std::string value.
+ */
+static ::std::string basicMethod( const ::std::string& inputString );
+
+};
+
+}
