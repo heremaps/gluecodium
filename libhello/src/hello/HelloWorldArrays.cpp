5f637d3a0ed4789983c567718f93e417d2418d84
Rafal Parzych <rafal.parzych@here.com>
2017-09-27 15:51:28 +0200

    APIGEN-749: Split ComplexTypes fidl file
    To make it possible to implement complex types one by one all fidl files
    related to complex types where split into separate files for enum, map,
    union, typedef, arrays.
    
    Change-Id: Iafb8d86a1280f98703a243ccb42bf0bf10253631

---
+++ libhello/src/hello/HelloWorldArrays.cpp
@@ -0,0 +1,35 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include "hello/HelloWorldArrays.h"
+
+namespace hello
+{
+::std::vector< ::std::string >
+HelloWorldArrays::methodWithArray( const ::std::vector< ::std::string >& input )
+{
+    return {input.rbegin( ), input.rend( )};
+}
+
+::std::vector< uint8_t >
+HelloWorldArrays::methodWithArrayInline( const ::std::vector< uint8_t >& input )
+{
+    return {input.rbegin( ), input.rend( )};
+}
+
+::std::vector< ::hello::HelloWorldArrays::ExampleStruct >
+HelloWorldArrays::methodWithStructArray(
+    const ::std::vector< ::hello::HelloWorldArrays::ExampleStruct >& input )
+{
+    return {input.rbegin( ), input.rend( )};
+}
+}
