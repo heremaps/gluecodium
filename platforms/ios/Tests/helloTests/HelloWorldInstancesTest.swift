21bc9c77ab576592d0a9e7718fb3547f02546610
Palmero, Antonio <antonio.palmero@here.com>
2017-10-05 12:24:11 +0200

    [iOS] Functional test for Instances
    Change-Id: I3327f68de191565ed858fa9bb5fb09643a96d183

---
+++ platforms/ios/Tests/helloTests/HelloWorldInstancesTest.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import hello
+
+class HelloWorldInstancesTests: XCTestCase {
+
+    func testInstanceCreation() {
+        let simple = HelloWorldFactory.createSimple()
+        XCTAssertNotNil(simple)
+    }
+
+    func testInstanceMethod() {
+        let simple = HelloWorldFactory.createSimple()
+        simple!.setStringValue(stringValue:"HelloSimple")
+        let stringReturned = simple!.getStringValue()!
+        XCTAssertEqual(stringReturned,"HelloSimple")
+    }
+
+    func testNestedInstances() {
+        let simple = HelloWorldFactory.createSimple()
+        simple?.setStringValue(stringValue: "Hello")
+        XCTAssertNotNil(simple)
+        let complex = HelloWorldFactory.createNested(simpleInstanceRef: simple!)
+        XCTAssertNotNil(complex)
+        XCTAssertEqual(complex?.getInstantiable()?.getStringValue(), "Hello")
+    }
+
+    static var allTests = [
+        ("testInstanceCreation", testInstanceCreation),
+        ("testInstanceMethod", testInstanceMethod),
+        ("testNestedInstances", testNestedInstances),
+    ]
+}
