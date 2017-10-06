a631d313c27830f0bba07e2041adee145e6c5841
Dario Limongi <dario.limongi@here.com>
2017-10-06 16:08:27 +0200

    APIGEN-742: Add Android functional tests for methodWithTypeDef
    Change-Id: I7ccce9026cf0ee7ec1e9434ab3a98368bd473cc9

---
+++ platforms/android/app/src/test/java/com/here/android/hello/HelloWorldTypeDefTest.java
@@ -0,0 +1,77 @@
+package com.here.android.hello;
+
+import android.os.Build;
+import android.support.compat.BuildConfig;
+import com.here.android.RobolectricApplication;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.M, application = RobolectricApplication.class, constants = BuildConfig.class)
+public class HelloWorldTypeDefTest {
+  private final HelloWorldTypedefs.SomeStruct inputStruct = new HelloWorldTypedefs.SomeStruct();
+
+  @Test
+  public void methodWithTypeDef() {
+    // Arrange
+    inputStruct.text = "foo";
+
+    // Act
+    HelloWorldTypedefs.SomeStruct outputStruct = HelloWorldTypedefs.methodWithTypeDef(inputStruct);
+
+    // Assert
+    assertEquals("FOO", outputStruct.text);
+  }
+
+  @Test
+  public void methodWithTypeDef_stringWithSpace() {
+    // Arrange
+    inputStruct.text = "foo bar";
+
+    // Act
+    HelloWorldTypedefs.SomeStruct outputStruct = HelloWorldTypedefs.methodWithTypeDef(inputStruct);
+
+    // Assert
+    assertEquals("FOO BAR", outputStruct.text);
+  }
+
+  @Test
+  public void methodWithTypeDef_stringWithNumber() {
+    // Arrange
+    inputStruct.text = "1a";
+
+    // Act
+    HelloWorldTypedefs.SomeStruct outputStruct = HelloWorldTypedefs.methodWithTypeDef(inputStruct);
+
+    // Assert
+    assertEquals("1A", outputStruct.text);
+  }
+
+  @Test
+  public void methodWithTypeDef_emptyString() {
+    // Arrange
+    inputStruct.text = "";
+
+    // Act
+    HelloWorldTypedefs.SomeStruct outputStruct = HelloWorldTypedefs.methodWithTypeDef(inputStruct);
+
+    // Assert
+    assertEquals("", outputStruct.text);
+  }
+
+  @Test
+  public void methodWithTypeDef_nullString() {
+    // Arrange
+    inputStruct.text = null;
+
+    // Act
+    HelloWorldTypedefs.SomeStruct outputStruct = HelloWorldTypedefs.methodWithTypeDef(inputStruct);
+
+    // Assert
+    assertEquals("", outputStruct.text);
+  }
+}
