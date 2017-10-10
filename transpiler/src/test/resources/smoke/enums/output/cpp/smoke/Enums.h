6263e201c83077de60a38f5fe98a384bbc380df7
Daniel Kamkha <daniel.kamkha@here.com>
2017-10-10 11:10:10 +0200

    APIGEN-824: Add initial smoke test for Enums
    Change-Id: I76664f238b22c380d408903c9317a4bf94c8ed02

---
+++ transpiler/src/test/resources/smoke/enums/output/cpp/smoke/Enums.h
@@ -0,0 +1,53 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <string>
+
+namespace smoke {
+
+class Enums {
+public:
+enum class InternalError {
+    ERROR_NONE,
+    ERROR_FATAL = 999
+};
+struct ErrorStruct {
+    ::smoke::Enums::InternalError type;
+    ::std::string message;
+};
+
+public:
+/**
+ *
+ * * @return The result type, containing ::smoke::Enums::InternalError value.
+ */
+static ::smoke::Enums::InternalError flipEnumValue( const ::smoke::Enums::InternalError input );
+/**
+ *
+ * * @return The result type, containing ::smoke::Enums::InternalError value.
+ */
+static ::smoke::Enums::InternalError extractEnumFromStruct( const ::smoke::Enums::ErrorStruct& input );
+/**
+ *
+ * * @return The result type, containing ::smoke::Enums::ErrorStruct value.
+ */
+static ::smoke::Enums::ErrorStruct createStructWithEnumInside( const ::smoke::Enums::InternalError type, const ::std::string& message );
+
+};
+
+}
