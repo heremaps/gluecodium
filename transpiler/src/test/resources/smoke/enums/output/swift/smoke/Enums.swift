90fa7dbc20dd43b24cac1e35703c68904a7f1049
Daniel Kamkha <daniel.kamkha@here.com>
2017-10-10 11:10:10 +0200

    APIGEN-824: Add initial smoke test for Enums
    Change-Id: I76664f238b22c380d408903c9317a4bf94c8ed02

---
+++ transpiler/src/test/resources/smoke/enums/output/swift/smoke/Enums.swift
@@ -0,0 +1,75 @@
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+
+import Foundation
+
+
+
+public class Enums {
+
+
+
+
+    public typealias InternalError = smoke_Enums_InternalError
+
+    public struct ErrorStruct {
+        public var type: InternalError
+        public var message: String
+
+        public init(type: InternalError, message: String) {
+            self.type = type
+            self.message = message
+        }
+
+        internal init?(cErrorStruct: smoke_Enums_ErrorStructRef) {
+            type = smoke_Enums_ErrorStruct_type_get(cErrorStruct)
+            do {
+                let messageHandle = smoke_Enums_ErrorStruct_message_get(cErrorStruct)
+                message = String(cString:std_string_data_get(messageHandle))
+            }
+        }
+
+        internal func convertToCType() -> smoke_Enums_ErrorStructRef {
+            let result = smoke_Enums_ErrorStruct_create()
+            fillFunction(result)
+            return result
+        }
+
+        internal func fillFunction(_ cErrorStruct: smoke_Enums_ErrorStructRef) -> Void {
+            smoke_Enums_ErrorStruct_type_set(cErrorStruct, type)
+            smoke_Enums_ErrorStruct_message_set(cErrorStruct, message)
+        }
+    }
+
+    public static func flipEnumValue(input: InternalError) -> InternalError {
+        return smoke_Enums_flipEnumValue(input)
+    }
+    public static func extractEnumFromStruct(input: ErrorStruct) -> InternalError {
+        let inputHandle = input.convertToCType()
+        defer {
+            smoke_Enums_ErrorStruct_release(inputHandle)
+        }
+        return smoke_Enums_extractEnumFromStruct(inputHandle)
+    }
+    public static func createStructWithEnumInside(type: InternalError, message: String) -> ErrorStruct? {
+        let cResult = smoke_Enums_createStructWithEnumInside(type, message)
+
+
+        defer {
+            smoke_Enums_ErrorStruct_release(cResult)
+        }
+
+        return ErrorStruct(cErrorStruct: cResult)
+    }
+}
+
+
+
