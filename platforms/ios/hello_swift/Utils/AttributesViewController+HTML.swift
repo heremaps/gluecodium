d0ae6f496b85fb13ba08d2682c4f48814823ddc6
Rafal Parzych <rafal.parzych@here.com>
2017-10-20 10:20:56 +0200

    APIGEN-508: Attributes showcase in iOS App
    Change-Id: Idcc67fd33a320f954a084b9d25cd131482afdf9e

---
+++ platforms/ios/hello_swift/Utils/AttributesViewController+HTML.swift
@@ -0,0 +1,109 @@
+//
+//  TypedefsViewController.swift
+//  hello_swift
+///
+//  Copyright © 2017 HERE. All rights reserved.
+//
+
+public class AttributesLabels {
+    public static let protocolHTML = """
+        <span style="font-family: monospace;">
+          <span style="color: #3366ff;">
+            public protocol</span> HelloWorldAttributes {
+          <br />&nbsp;
+          <span style="color: #3366ff;">
+            var</span> builtInTypeAttribute:
+          <span style="color: #3495AF;">UInt32</span>
+          {
+          <span style="color: #3366ff;">get set</span> }
+          <br />&nbsp;
+          <span style="color: #3366ff;">
+            var</span> readonlyAttribute:
+          <span style="color: #3495AF;">Float</span>
+          {
+          <span style="color: #3366ff;">get</span> }
+          <br />&nbsp;
+          <span style="color: #3366ff;">var</span> structAttribute: ExampleStruct
+          {
+          <span style="color: #3366ff;">get set</span> }
+          <br />}
+        </span>
+    """
+
+    public static let getBuiltInAttrHTML = """
+        <center>
+          <span style="font-family: monospace;">
+            <span style="color: #3366ff;">let</span> result =
+            <span style="color: #0F7001;">
+              testClass</span>.builtInTypeAttribute
+            <br><br> result &rarr; %d
+          </span>
+        </center>
+    """
+
+    public static let setBuiltInAttrHTML = """
+        <center>
+          <span style="font-family: monospace;">
+          <span style="color: #0F7001;">testClass</span>.builtInTypeAttribute = %d
+          </span>
+        </center>
+    """
+
+    public static let getReadonlyAttributeHTML = """
+        <center>
+          <span style="font-family: monospace;">
+                        <span style="color: #3366ff;">let</span> result =
+          <span style="color: #0F7001;">
+                        testClass</span>.readonlyAttribute
+          <br><br> result &rarr; %f
+          </span>
+        </center>
+    """
+
+    public static let getStructAttrHTML = """
+        <center>
+          <span style="font-family: monospace;">
+          <table>
+            <tbody>
+              <tr>
+                <td colspan="3">
+                  <span style="color: #3366ff;">let</span> result =
+                  <span style="color: #0f7001;">testClass</span>.structAttribute
+                  <br><br>
+                </td>
+              </tr>
+              <tr>
+                <td>result &rarr; </td>
+                <td>
+                  <span style="color: #3366ff;">struct</span> ExampleStruct {
+                </td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <span style="color: #3366ff;">&nbsp;&nbsp;var</span> value:
+                  <span style="color: #3495af;">Float</span> &rarr; %f
+                </td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>}</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </span>
+        </center>
+    """
+
+    public static let setStructAttrHTML = """
+        <center>
+          <span style="font-family: monospace;">
+            <span style="color: #0F7001;">
+              testClass</span>.structAttribute&nbsp;= ExampleStruct(value:&nbsp;%f)
+          </span>
+        </center>
+    """
+}
