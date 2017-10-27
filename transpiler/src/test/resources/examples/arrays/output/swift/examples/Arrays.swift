0792def6a2470a2f9416b8703501763d6771639e
Daniel Kamkha <daniel.kamkha@here.com>
2017-10-27 11:55:28 +0200

    APIGEN-842: Add "example" test for Arrays
    Change-Id: I68f8a5c8b7307dfb337be52be6733907f6811af5

---
+++ transpiler/src/test/resources/examples/arrays/output/swift/examples/Arrays.swift
@@ -0,0 +1,52 @@
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+import Foundation
+public class Arrays {
+    public struct SyncResult {
+        public var lastUpdatedTimeStamp: UInt64
+        public var numberOfChanges: UInt32
+        public init(lastUpdatedTimeStamp: UInt64, numberOfChanges: UInt32) {
+            self.lastUpdatedTimeStamp = lastUpdatedTimeStamp
+            self.numberOfChanges = numberOfChanges
+        }
+        internal init?(cSyncResult: examples_Arrays_SyncResultRef) {
+            lastUpdatedTimeStamp = examples_Arrays_SyncResult_lastUpdatedTimeStamp_get(cSyncResult)
+            numberOfChanges = examples_Arrays_SyncResult_numberOfChanges_get(cSyncResult)
+        }
+        internal func convertToCType() -> examples_Arrays_SyncResultRef {
+            let result = examples_Arrays_SyncResult_create()
+            fillFunction(result)
+            return result
+        }
+        internal func fillFunction(_ cSyncResult: examples_Arrays_SyncResultRef) -> Void {
+            examples_Arrays_SyncResult_lastUpdatedTimeStamp_set(cSyncResult, lastUpdatedTimeStamp)
+            examples_Arrays_SyncResult_numberOfChanges_set(cSyncResult, numberOfChanges)
+        }
+    }
+    public static func explicitArrayMethod<T: Collection>(input: T) -> CollectionOf<Arrays.SyncResult> {
+        let inputArray = input.flatMap{ $0 as? SyncResult }
+        let inputHandle = inputArray.c_conversion()
+        defer {
+            inputHandle.cleanup()
+        }
+        let handle =  examples_Arrays_explicitArrayMethod(inputHandle.c_type)
+        return SyncResultList(handle)
+    }
+    public static func implicitArrayMethod<T: Collection>(input: T) -> CollectionOf<Arrays.SyncResult> {
+        let inputArray = input.flatMap{ $0 as? SyncResult }
+        let inputHandle = inputArray.c_conversion()
+        defer {
+            inputHandle.cleanup()
+        }
+        let handle =  examples_Arrays_implicitArrayMethod(inputHandle.c_type)
+        return SyncResultList(handle)
+    }
+}
