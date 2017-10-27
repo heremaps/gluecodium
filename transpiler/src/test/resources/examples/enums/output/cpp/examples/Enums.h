557f4b8555c5cbb9e9674cb6190475aaedb22eaf
Daniel Kamkha <daniel.kamkha@here.com>
2017-10-27 10:41:29 +0200

    APIGEN-842: Add "example" test for Enums
    Change-Id: Ib3fa683b80e80473a7264a06165ef106ec9f4b4d

---
+++ transpiler/src/test/resources/examples/enums/output/cpp/examples/Enums.h
@@ -0,0 +1,28 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+namespace examples {
+
+enum class ShoeSizes {
+    SMALL = 39,
+    BIGGER_THAN_SMALL,
+    NORMAL = 43,
+    BIG = 46
+};
+
+}
