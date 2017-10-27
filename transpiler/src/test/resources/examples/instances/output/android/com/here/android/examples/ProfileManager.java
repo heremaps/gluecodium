291a468caf76a04eabdcdda94cbfcc1b772e0264
Daniel Kamkha <daniel.kamkha@here.com>
2017-10-27 10:20:02 +0200

    APIGEN-842: Add "example" test for Instances
    Change-Id: I2796951dde0d6e91fb730885c1730d6eb7e3fae5

---
+++ transpiler/src/test/resources/examples/instances/output/android/com/here/android/examples/ProfileManager.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+ *
+ * This software, including documentation, is protected by copyright controlled by
+ * HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+ * adapting or translating, any or all of this material requires the prior written
+ * consent of HERE Global B.V. This material also contains confidential information,
+ * which may not be disclosed to others without prior written consent of HERE Global B.V.
+ *
+ * Automatically generated. Do not modify. Your changes will be lost.
+ */
+
+package com.here.android.examples;
+
+public interface ProfileManager {
+    void createProfile(final String username);
+    String changeProfile(final String username);
+    void deleteProfile(final String username);
+}
---
+++ transpiler/src/test/resources/examples/instances/output/swift/examples/ProfileManagerFactory.swift
@@ -0,0 +1,24 @@
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+
+import Foundation
+
+
+public class ProfileManagerFactory {
+
+    public static func createProfileManager() -> ProfileManager? {
+        let cResult = examples_ProfileManagerFactory_createProfileManager()
+
+
+        return _ProfileManager(cProfileManager: cResult)
+    }
+
+}
