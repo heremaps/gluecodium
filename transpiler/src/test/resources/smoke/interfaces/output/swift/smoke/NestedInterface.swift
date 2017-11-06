7861d0b9cd2e2e69fbe7f9169e0db5ad4afe3fc1
Dario Limongi <dario.limongi@here.com>
2017-11-06 09:44:59 +0100

    APIGEN-848: Add smoke tests for IsInterface true
    Added smoke tests for Cpp, iOS and Android, which cover same cases
    as instances, but in this case IsInterface is true
    
    Change-Id: I499da30567dda8812f6872c586333b1e4a0993fb

---
+++ transpiler/src/test/resources/smoke/interfaces/output/swift/smoke/NestedInterface.swift
@@ -0,0 +1,64 @@
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+
+import Foundation
+
+
+internal func getRef(_ ref: NestedInterface) -> RefHolder<smoke_NestedInterfaceRef> {
+    guard let instanceReference = ref as? _NestedInterface else {
+        fatalError("Not implemented yet")
+    }
+    return RefHolder<smoke_NestedInterfaceRef>(instanceReference.c_instance)
+}
+
+
+public protocol NestedInterface {
+
+
+        func setSameTypeInstances(interfaceOne: SimpleInterface, interfaceTwo: SimpleInterface) -> Void;
+        func getInstanceOne() -> SimpleInterface?;
+        func getInstanceTwo() -> SimpleInterface?;
+
+}
+
+internal class _NestedInterface: NestedInterface {
+
+
+    let c_instance : smoke_NestedInterfaceRef
+
+    required init?(cNestedInterface: smoke_NestedInterfaceRef) {
+        c_instance = cNestedInterface
+    }
+
+    deinit {
+        smoke_NestedInterface_release(c_instance)
+    }
+    public func setSameTypeInstances(interfaceOne: SimpleInterface, interfaceTwo: SimpleInterface) -> Void {
+        let interfaceOneHandle = getRef(interfaceOne)
+        let interfaceTwoHandle = getRef(interfaceTwo)
+        return smoke_NestedInterface_setSameTypeInstances(c_instance, interfaceOneHandle.ref, interfaceTwoHandle.ref)
+    }
+
+    public func getInstanceOne() -> SimpleInterface? {
+        let cResult = smoke_NestedInterface_getInstanceOne(c_instance)
+
+
+        return _SimpleInterface(cSimpleInterface: cResult)
+    }
+
+    public func getInstanceTwo() -> SimpleInterface? {
+        let cResult = smoke_NestedInterface_getInstanceTwo(c_instance)
+
+
+        return _SimpleInterface(cSimpleInterface: cResult)
+    }
+
+}
