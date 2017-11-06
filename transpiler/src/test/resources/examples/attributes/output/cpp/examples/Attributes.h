84338e6ae2cb9e4c05201e260d6e7b61d608711a
Daniel Kamkha <daniel.kamkha@here.com>
2017-11-06 13:16:49 +0100

    APIGEN-842: Add "example" test for Attributes
    Change-Id: I915c464376160fe1fc6be7b7cc0d0a3935115867

---
+++ transpiler/src/test/resources/examples/attributes/output/cpp/examples/Attributes.h
@@ -0,0 +1,34 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <cstdint>
+
+namespace examples {
+
+class Attributes {
+public:
+    virtual ~Attributes() = 0;
+
+public:
+virtual uint32_t get_built_in_type_attribute(  ) = 0;
+virtual void set_built_in_type_attribute( const uint32_t value ) = 0;
+virtual float get_readonly_attribute(  ) = 0;
+
+};
+
+}
