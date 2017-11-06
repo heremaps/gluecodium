feba739269177021be725407a8d84a6c2de973c1
Daniel Kamkha <daniel.kamkha@here.com>
2017-11-06 13:03:55 +0100

    APIGEN-842: Add "example" test for Listeners
    Change-Id: I31962da29bb4bf610a08ad035493ab18cc0cc55c

---
+++ transpiler/src/test/resources/examples/listeners/output/swift/examples/Calculator.swift
@@ -0,0 +1,30 @@
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+
+import Foundation
+
+public class Calculator {
+
+    public static func registerListener(listener: CalculatorListener) -> Void {
+        let listenerHandle = getRef(listener)
+        return examples_Calculator_registerListener(listenerHandle.ref)
+    }
+
+    public static func unregisterListener(listener: CalculatorListener) -> Void {
+        let listenerHandle = getRef(listener)
+        return examples_Calculator_unregisterListener(listenerHandle.ref)
+    }
+
+    public static func calculate() -> Void {
+        return examples_Calculator_calculate()
+    }
+
+}
