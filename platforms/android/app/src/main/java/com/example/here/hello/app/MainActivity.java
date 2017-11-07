85669c227021b568de22daf6b455adfcc27b5168
Sascha Ebel <sascha.ebel@here.com>
2017-11-07 12:02:17 +0100

    APIGEN-756: showcase for enums on android
    Change-Id: Ia494b057f0353c54ced9a365a72c738466b1ccf9

--- platforms/android/app/src/main/java/com/example/here/hello/app/MainActivity.java
+++ platforms/android/app/src/main/java/com/example/here/hello/app/MainActivity.java
@@ -54,7 +54,8 @@ public final class MainActivity extends AppCompatActivity {
                     context.getString(R.string.fragment_instance_methods),
                     context.getString(R.string.fragment_attributes),
                     context.getString(R.string.fragment_typedef),
-                    context.getString(R.string.fragment_listeners_title)
+                    context.getString(R.string.fragment_listeners_title),
+                    context.getString(R.string.fragment_enumerations_title),
             };
         }
 
@@ -75,6 +76,8 @@ public final class MainActivity extends AppCompatActivity {
                     return new TypeDefFragment();
                 case 6:
                     return new ListenersFragment();
+                case 7:
+                    return new EnumerationFragment();
                 default:
                     return null;
             }
