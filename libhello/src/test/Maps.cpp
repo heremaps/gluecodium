c901a0985f29d27c44f5732d6de511f899210547
Daniel Kamkha <daniel.kamkha@here.com>
2017-11-09 10:43:44 +0100

    APIGEN-734: Add functional tests for Android Maps
    Change-Id: Ie14b76710b565c5be24bf2085b082f3e125935ed

---
+++ libhello/src/test/Maps.cpp
@@ -0,0 +1,76 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include "test/Maps.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <string>
+
+namespace test {
+
+Maps::ErrorCodeToMessageMap
+Maps::method_with_map( const Maps::ErrorCodeToMessageMap& input )
+{
+    Maps::ErrorCodeToMessageMap result;
+    for ( const auto& entry : input )
+    {
+        std::string uppercase_string = entry.second;
+        std::transform( uppercase_string.begin( ), uppercase_string.end( ),
+                        uppercase_string.begin( ), ::toupper );
+        result.emplace( entry.first, uppercase_string );
+    }
+    return result;
+}
+
+Maps::NumberToStruct
+Maps::method_with_map_to_struct( const Maps::NumberToStruct& input )
+{
+    Maps::NumberToStruct result;
+    for ( const auto& entry : input )
+    {
+        Maps::SomeStruct uppercase_string_inabox = entry.second;
+        std::string& uppercase_string = uppercase_string_inabox.value;
+        std::transform( uppercase_string.begin( ), uppercase_string.end( ),
+                        uppercase_string.begin( ), ::toupper );
+        result.emplace( entry.first, uppercase_string_inabox );
+    }
+    return result;
+}
+
+Maps::NestedMap
+Maps::method_with_nested_map( const Maps::NestedMap& input )
+{
+    Maps::NestedMap result;
+    for ( const auto& entry : input )
+    {
+        Maps::NumberToStruct result_value = method_with_map_to_struct( entry.second );
+        result.emplace( entry.first, result_value );
+    }
+    return result;
+}
+
+Maps::StructWithMap
+Maps::method_with_struct_with_map( const Maps::StructWithMap& input )
+{
+    Maps::StructWithMap result;
+    for ( const auto& entry : input.error_mapping )
+    {
+        std::string uppercase_string = entry.second;
+        std::transform( uppercase_string.begin( ), uppercase_string.end( ),
+                        uppercase_string.begin( ), ::toupper );
+        result.error_mapping.emplace( entry.first, uppercase_string );
+    }
+    return result;
+}
+
+}
