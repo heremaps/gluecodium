32e988c4abcac413a64d7003331e19c463361656
Daniel Kamkha <daniel.kamkha@here.com>
2017-11-20 13:58:16 +0100

    APIGEN-698: Add stub implementation for HelloWorldErrors
    Change-Id: I4be3ef2c555fc773fd30dd7e2ba2897a3aa3bfb6

---
+++ libhello/src/hello/HelloWorldErrors.cpp
@@ -0,0 +1,39 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include "hello/HelloWorldErrors.h"
+
+namespace hello
+{
+    hf::Error
+    HelloWorldErrors::hello_world_method_with_error( const bool errorFlag )
+    {
+        HelloWorldErrors::InternalErrors error_code = errorFlag ?
+            HelloWorldErrors::InternalErrors::CRASHED : HelloWorldErrors::InternalErrors::NONE;
+        return hf::Error( static_cast< hf::ErrorCode::Code >( error_code ) );
+    }
+
+    here::internal::Expected< hf::Error, std::string >
+    HelloWorldErrors::hello_world_method_with_error_and_string( const bool errorFlag )
+    {
+        if ( errorFlag )
+        {
+            return here::internal::Expected< hf::Error, std::string >::fail(
+                hf::Error( static_cast< hf::ErrorCode::Code >(
+                    HelloWorldErrors::InternalErrors::CRASHED ) ) );
+        }
+        else
+        {
+            return here::internal::Expected< hf::Error, std::string >::result( "Hello!" );
+        }
+    }
+}
