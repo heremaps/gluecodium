d5fa8aab90e3551f69b2f880f7d6d3e3721ca72e
Dario Limongi <dario.limongi@here.com>
2017-11-23 13:30:04 +0100

    MINOR: Update Array Android app design
    Updated the look and error messages of ArraysFragment based on the
    discussion emerged during the showcase.
    
    Change-Id: I8a5578dda31847a6fe82f0c6143533458125e7e3

--- platforms/android/app/src/main/java/com/example/here/hello/app/ArraysFragment.java
+++ platforms/android/app/src/main/java/com/example/here/hello/app/ArraysFragment.java
@@ -19,6 +19,7 @@ import com.example.here.hello.R;
 import com.here.android.hello.HelloWorldArrays;
 import com.here.android.hello.HelloWorldArrays.ExampleStruct;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,10 +28,7 @@ import static android.content.Context.INPUT_METHOD_SERVICE;
 public final class ArraysFragment extends Fragment {
 
   private Button submitButton;
-  private EditText input1;
-  private EditText input2;
-  private EditText input3;
-  private EditText input4;
+  private EditText input;
   private TextView result;
   private Spinner spinner;
   private TextView description;
@@ -38,12 +36,9 @@ public final class ArraysFragment extends Fragment {
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
+      Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_arrays, container, false);
-    input1 = rootView.findViewById(R.id.arrays_edit_1);
-    input2 = rootView.findViewById(R.id.arrays_edit_2);
-    input3 = rootView.findViewById(R.id.arrays_edit_3);
-    input4 = rootView.findViewById(R.id.arrays_edit_4);
+    input = rootView.findViewById(R.id.arrays_edit);
     result = rootView.findViewById(R.id.arrays_result);
     spinner = rootView.findViewById(R.id.arrays_spinner);
     submitButton = rootView.findViewById(R.id.arrays_submit_button);
@@ -59,84 +54,85 @@ public final class ArraysFragment extends Fragment {
       @Override
       public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position,
           long id) {
-        description.setText(descriptionsText[position]);
+        String methodDescription = descriptionsText[position] + "\n\n" + getResources()
+            .getString(R.string.fragment_arrays_usage);
+        description.setText(methodDescription);
         result.setText("");
-        input1.setText("");
-        input2.setText("");
-        input3.setText("");
-        input4.setText("");
+        input.setText("");
       }
 
       @Override
       public void onNothingSelected(AdapterView<?> parentView) {
         description.setText("");
         result.setText("");
-        input1.setText("");
-        input2.setText("");
-        input3.setText("");
-        input4.setText("");
+        input.setText("");
       }
     });
     submitButton.setOnClickListener((View v) -> {
-      String inputText1 = input1.getText().toString();
-      String inputText2 = input2.getText().toString();
-      String inputText3 = input3.getText().toString();
-      String inputText4 = input4.getText().toString();
+      String inputText = input.getText().toString();
 
       try {
-        executeArrayMethod(spinner.getSelectedItemPosition(), inputText1, inputText2, inputText3,
-            inputText4);
+        executeArrayMethod(spinner.getSelectedItemPosition(), inputText.split(","));
       } catch (NumberFormatException e) {
-        String exceptionMessage = R.string.number_format_exception + " " + e.getMessage();
+        String exceptionMessage =
+            getResources().getString(R.string.number_format_exception) + "\n" + e.getMessage();
         result.setText(exceptionMessage);
       }
 
       // hide virtual keyboard
-      InputMethodManager imm = (InputMethodManager) getContext()
+      InputMethodManager inputMethodManager = (InputMethodManager) getContext()
           .getSystemService(INPUT_METHOD_SERVICE);
-      imm.hideSoftInputFromWindow(result.getWindowToken(), 0);
+      inputMethodManager.hideSoftInputFromWindow(result.getWindowToken(), 0);
     });
   }
 
-  private void executeArrayMethod(final int selectedItemPosition, final String input1,
-      final String input2, final String input3, final String input4) {
+  private void executeArrayMethod(final int selectedItemPosition, final String[] parameters) {
+    List<String> stringList = Arrays.asList(parameters);
     switch (selectedItemPosition) {
       case 0:
-        List<String> stringList = Arrays.asList(input1, input2, input3, input4);
         result.setText(HelloWorldArrays.methodWithArray(stringList).toString());
         break;
       case 1:
-        List<Long> longList = Arrays.asList(
-            Long.parseLong(input1),
-            Long.parseLong(input2),
-            Long.parseLong(input3),
-            Long.parseLong(input4));
+        List<Long> longList = new ArrayList<>();
+        for (String string : stringList) {
+          if (!string.isEmpty()) {
+            longList.add(Long.valueOf(string));
+          }
+        }
+
         result.setText(HelloWorldArrays.methodWithSimpleArray(longList).toString());
         break;
       case 2:
-        List<Long> longList2 = Arrays.asList(
-            Long.parseLong(input1),
-            Long.parseLong(input2),
-            Long.parseLong(input3),
-            Long.parseLong(input4));
+        List<Long> longList2 = new ArrayList<>();
+        for (String string : stringList) {
+          if (!string.isEmpty()) {
+            longList2.add(Long.valueOf(string));
+          }
+        }
+
         result.setText(HelloWorldArrays.methodWithArrayInline(longList2).toString());
         break;
       case 3:
-        List<ExampleStruct> exampleStructList = Arrays.asList(
-            createExampleStruct(input1),
-            createExampleStruct(input2),
-            createExampleStruct(input3),
-            createExampleStruct(input4));
+        List<ExampleStruct> exampleStructList = new ArrayList<>();
+        for (String string : stringList) {
+          if (!string.isEmpty()) {
+            exampleStructList.add(createExampleStruct(string));
+          }
+        }
+
         List<ExampleStruct> resultList = HelloWorldArrays.methodWithStructArray(exampleStructList);
         result.setText(formatExampleStructResult(resultList));
         break;
       case 4:
-        List<List<Long>> nestedLongList = Arrays.asList(
-            createListOfLongs(input1),
-            createListOfLongs(input2),
-            createListOfLongs(input3),
-            createListOfLongs(input4));
-        List<List<Long>> resultList2 = HelloWorldArrays.methodWithNestedArraysInline(nestedLongList);
+        List<List<Long>> nestedLongList = new ArrayList<>();
+        for (String string : stringList) {
+          if (!string.isEmpty()) {
+            nestedLongList.add(createListOfLongs(string));
+          }
+        }
+
+        List<List<Long>> resultList2 = HelloWorldArrays
+            .methodWithNestedArraysInline(nestedLongList);
         String resultText = "INPUT:\n"
             + formatNestedLongListResult(nestedLongList)
             + "\n\n\nOUTPUT:\n"
@@ -162,7 +158,9 @@ public final class ArraysFragment extends Fragment {
     for (ExampleStruct exampleStruct : exampleStructList) {
       resultText.append(" ExampleStruct.value = ").append(exampleStruct.value).append(",\n");
     }
-    resultText.setLength(resultText.length() - 2);
+    if (resultText.length() > 1) {
+      resultText.setLength(resultText.length() - 2);
+    }
     resultText.append(" ]");
 
     return resultText.toString();
@@ -173,7 +171,9 @@ public final class ArraysFragment extends Fragment {
     for (List<Long> longList : nestedLongList) {
       resultText.append(longList.toString()).append(",\n");
     }
-    resultText.setLength(resultText.length() - 2);
+    if (resultText.length() > 1) {
+      resultText.setLength(resultText.length() - 2);
+    }
     resultText.append("]");
 
     return resultText.toString();
