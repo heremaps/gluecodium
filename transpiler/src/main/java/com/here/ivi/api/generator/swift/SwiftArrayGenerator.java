6f9916fca1bb0fb0745029af2cf33c9c8ce32e6c
Daniel Kamkha <daniel.kamkha@here.com>
2017-11-28 09:31:11 +0100

    MINOR: Sort the list of all SwiftArray elements
    Added sorting to SwiftArrayGenerator, so that the resulting generated
    Swift code is stable and does not depend on unimportant orderings like
    file order, traversal order, etc.
    
    Updated affected smoke test.
    
    Change-Id: Id5cd29ab4e7a36f2d1c1f0c35c5b401789b9861f

--- transpiler/src/main/java/com/here/ivi/api/generator/swift/SwiftArrayGenerator.java
+++ transpiler/src/main/java/com/here/ivi/api/generator/swift/SwiftArrayGenerator.java
@@ -31,7 +31,7 @@ public final class SwiftArrayGenerator {
 
   public List<GeneratedFile> generate() {
     SwiftFile arrayFile = new SwiftFile();
-    arrayFile.arrays.addAll(arrayCollector.values());
+    arrayFile.arrays.addAll(new TreeMap<>(arrayCollector).values());
 
     if (arrayFile.isEmpty()) {
       return emptyList();
