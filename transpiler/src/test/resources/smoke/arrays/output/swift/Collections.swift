457ac3e36a142906210e3d2aade0132290e05fa4
Daniel Kamkha <daniel.kamkha@here.com>
2017-11-28 09:31:11 +0100

    MINOR: Sort the list of all SwiftArray elements
    Added sorting to SwiftArrayGenerator, so that the resulting generated
    Swift code is stable and does not depend on unimportant orderings like
    file order, traversal order, etc.
    
    Updated affected smoke test.
    
    Change-Id: Id5cd29ab4e7a36f2d1c1f0c35c5b401789b9861f

--- transpiler/src/test/resources/smoke/arrays/output/swift/Collections.swift
+++ transpiler/src/test/resources/smoke/arrays/output/swift/Collections.swift
@@ -9,47 +9,42 @@
 //
 // Automatically generated. Do not modify. Your changes will be lost.
 import Foundation
-
-internal class UInt8ListList: CollectionOf<CollectionOf<UInt8>> {
-    let c_element: arrayCollection_UInt8Array
-
-    init(_ c_element: arrayCollection_UInt8Array) {
+internal class BasicStructList: CollectionOf<Arrays.BasicStruct> {
+    let c_element: arrayCollection_BasicStruct
+    init(_ c_element: arrayCollection_BasicStruct) {
         self.c_element = c_element
         super.init([])
         self.startIndex = 0
-        self.endIndex = Int(arrayCollection_UInt8Array_count(c_element))
+        self.endIndex = Int(arrayCollection_BasicStruct_count(c_element))
     }
-
-    public override subscript(index: Int) -> CollectionOf<UInt8> {
-        let handle = arrayCollection_UInt8Array_get(c_element, UInt64(index))
-        return UInt8List(handle)
+    public override subscript(index: Int) -> Arrays.BasicStruct {
+        let handle = arrayCollection_BasicStruct_get(c_element, UInt64(index))
+        guard let result = Arrays.BasicStruct(cBasicStruct: handle) else {
+            fatalError("Not implemented")
+        }
+        return result
     }
 }
-extension Collection where Element: Collection, Element.Element == UInt8  {
-    public func c_conversion()-> (c_type: arrayCollection_UInt8Array, cleanup: () ->Void) {
-        let handle = arrayCollection_UInt8Array_create()
-        for item in self { 
-        let conversion = item.c_conversion()
-        arrayCollection_UInt8Array_append(handle, conversion.c_type)
-        conversion.cleanup()
+extension Collection where Element == Arrays.BasicStruct  {
+    public func c_conversion()-> (c_type: arrayCollection_BasicStruct, cleanup: () ->Void) {
+        let handle = arrayCollection_BasicStruct_create()
+        for item in self {
+           arrayCollection_BasicStruct_append(handle, item.convertToCType())
         }
         let cleanup_function = { () -> Void in
-            arrayCollection_UInt8Array_release(handle)
+            arrayCollection_BasicStruct_release(handle)
         }
         return (handle, cleanup_function)
     }
 }
-
 internal class FancyStructList: CollectionOf<Arrays.FancyStruct> {
     let c_element: arrayCollection_FancyStruct
-
     init(_ c_element: arrayCollection_FancyStruct) {
         self.c_element = c_element
         super.init([])
         self.startIndex = 0
         self.endIndex = Int(arrayCollection_FancyStruct_count(c_element))
     }
-
     public override subscript(index: Int) -> Arrays.FancyStruct {
         let handle = arrayCollection_FancyStruct_get(c_element, UInt64(index))
         guard let result = Arrays.FancyStruct(cFancyStruct: handle) else {
@@ -70,7 +65,33 @@ extension Collection where Element == Arrays.FancyStruct  {
         return (handle, cleanup_function)
     }
 }
-
+internal class UInt8ListList: CollectionOf<CollectionOf<UInt8>> {
+    let c_element: arrayCollection_UInt8Array
+    init(_ c_element: arrayCollection_UInt8Array) {
+        self.c_element = c_element
+        super.init([])
+        self.startIndex = 0
+        self.endIndex = Int(arrayCollection_UInt8Array_count(c_element))
+    }
+    public override subscript(index: Int) -> CollectionOf<UInt8> {
+        let handle = arrayCollection_UInt8Array_get(c_element, UInt64(index))
+        return UInt8List(handle)
+    }
+}
+extension Collection where Element: Collection, Element.Element == UInt8  {
+    public func c_conversion()-> (c_type: arrayCollection_UInt8Array, cleanup: () ->Void) {
+        let handle = arrayCollection_UInt8Array_create()
+        for item in self {
+        let conversion = item.c_conversion()
+        arrayCollection_UInt8Array_append(handle, conversion.c_type)
+        conversion.cleanup()
+        }
+        let cleanup_function = { () -> Void in
+            arrayCollection_UInt8Array_release(handle)
+        }
+        return (handle, cleanup_function)
+    }
+}
 internal class StringList: CollectionOf<String> {
     let c_element: arrayCollection_String
     init(_ c_element: arrayCollection_String) {
@@ -100,48 +121,14 @@ extension Collection where Element == String  {
         return (handle, cleanup_function)
     }
 }
-
-internal class BasicStructList: CollectionOf<Arrays.BasicStruct> {
-    let c_element: arrayCollection_BasicStruct
-
-    init(_ c_element: arrayCollection_BasicStruct) {
-        self.c_element = c_element
-        super.init([])
-        self.startIndex = 0
-        self.endIndex = Int(arrayCollection_BasicStruct_count(c_element))
-    }
-
-    public override subscript(index: Int) -> Arrays.BasicStruct {
-        let handle = arrayCollection_BasicStruct_get(c_element, UInt64(index))
-        guard let result = Arrays.BasicStruct(cBasicStruct: handle) else {
-            fatalError("Not implemented")
-        }
-        return result
-    }
-}
-extension Collection where Element == Arrays.BasicStruct  {
-    public func c_conversion()-> (c_type: arrayCollection_BasicStruct, cleanup: () ->Void) {
-        let handle = arrayCollection_BasicStruct_create()
-        for item in self { 
-           arrayCollection_BasicStruct_append(handle, item.convertToCType())
-        }
-        let cleanup_function = { () -> Void in
-            arrayCollection_BasicStruct_release(handle)
-        }
-        return (handle, cleanup_function)
-    }
-}
-
 internal class UInt8List: CollectionOf<UInt8> {
     let c_element: arrayCollection_UInt8
-
     init(_ c_element: arrayCollection_UInt8) {
         self.c_element = c_element
         super.init([])
         self.startIndex = 0
         self.endIndex = Int(arrayCollection_UInt8_count(c_element))
     }
-
     public override subscript(index: Int) -> UInt8 {
         let handle = arrayCollection_UInt8_get(c_element, UInt64(index))
         return handle
@@ -150,7 +137,7 @@ internal class UInt8List: CollectionOf<UInt8> {
 extension Collection where Element == UInt8  {
     public func c_conversion()-> (c_type: arrayCollection_UInt8, cleanup: () ->Void) {
         let handle = arrayCollection_UInt8_create()
-        for item in self { 
+        for item in self {
              arrayCollection_UInt8_append(handle, item)
         }
         let cleanup_function = { () -> Void in
@@ -159,23 +146,3 @@ extension Collection where Element == UInt8  {
         return (handle, cleanup_function)
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
