2a05602c33036831679712a19488f24a8cfa1f4a
Rafal Parzych <rafal.parzych@here.com>
2017-11-30 11:10:56 +0100

    APIGEN-789: Functional tests for errors.
    - added fidl and fdepl files
    - added stub implementation for static functions
    - turned on above tests for cpp generator
    
    Change-Id: Icad7d38d438ad2a96db5c04789dd182440cd80b2

---
+++ libhello/src/test/Errors.cpp
@@ -0,0 +1,69 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include "test/Errors.h"
+#include "another/AdditionalErrors.h"
+#include "another/TypeCollectionWithEnums.h"
+
+namespace test
+{
+hf::Error
+Errors::method_with_error( const bool errorFlag )
+{
+    Errors::InternalErrors error_code =
+        errorFlag ? Errors::InternalErrors::CRASHED : Errors::InternalErrors::NONE;
+    return hf::Error( static_cast< hf::ErrorCode::Code >( error_code ) );
+}
+
+hf::Return< std::string, hf::Error >
+Errors::method_with_error_and_string( const bool errorFlag )
+{
+    if ( errorFlag )    {
+        return hf::Return< std::string, hf::Error >( hf::Error( static_cast< hf::ErrorCode::Code >(
+            another::AdditionalErrors::ExternalErrors::FAILED ) ) );
+    }
+    else
+    {
+        return hf::Return< std::string, hf::Error >( "SUCCEEDED" );
+    }
+}
+
+hf::Return< std::vector< uint8_t >, hf::Error >
+Errors::method_that_explodes( const bool error_flag )
+{
+    if ( error_flag )    {
+        return hf::Return< std::vector< uint8_t >, hf::Error >(
+            hf::Error( static_cast< hf::ErrorCode::Code >(
+                another::ExplosiveErrors::EXPLODED ) ) );
+    }
+    else
+    {
+        return hf::Return< std::vector< uint8_t >, hf::Error >(
+            std::vector< uint8_t >{0x00, 0x01, 0x02} );
+    }
+}
+
+hf::Return< another::SomeEnum, hf::Error >
+Errors::method_with_good_and_bad( const bool error_flag )
+{
+    if ( error_flag )
+    {
+        return hf::Return< another::SomeEnum, hf::Error >( hf::Error(
+            static_cast< hf::ErrorCode::Code >( another::YetAnotherErrors::BAD ) ) );
+    }
+    else
+    {
+        return hf::Return< another::SomeEnum, hf::Error >(
+            another::SomeEnum::ANOTHER_RESULT );
+    }
+}
+}
