490aa7bb380ec4f8868abae1cdc0b56c8748ca65
Dario Limongi <dario.limongi@here.com>
2017-12-04 10:25:20 +0100

    APIGEN-926: Add struct inheritance showcase for Android
    Change-Id: I064c1650d4f678ed7fcc338d5e47037e004d2c46

--- platforms/android/app/src/test/java/com/here/android/hello/HelloWorldPlainDataStructuresTest.java
+++ platforms/android/app/src/test/java/com/here/android/hello/HelloWorldPlainDataStructuresTest.java
@@ -6,6 +6,7 @@ import android.support.compat.BuildConfig;
 
 import com.here.android.RobolectricApplication;
 
+import com.here.android.hello.HelloWorldPlainDataStructures.NumericSyncResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -40,6 +41,20 @@ public final class HelloWorldPlainDataStructuresTest {
         assertEquals(input.syncResult.lastUpdatedTimeStamp, result.syncResult.lastUpdatedTimeStamp);
         assertEquals(input.syncResult.numberOfChanges + 1, result.syncResult.numberOfChanges);
         assertEquals(input.id, result.id);
+    }
+
+    @Test
+    public void methodWithInheritedStruct() {
+        NumericSyncResult input = new NumericSyncResult();
+        input.lastUpdatedTimeStamp = 10;
+        input.numberOfChanges = 100;
+        input.result = 1000;
+
+        NumericSyncResult result = HelloWorldPlainDataStructures.methodWithInheritedStruct(input);
+
+        assertEquals(10, result.lastUpdatedTimeStamp);
+        assertEquals(101, result.numberOfChanges);
+        assertEquals(1000, result.result);
     }
 
     @NonNull
