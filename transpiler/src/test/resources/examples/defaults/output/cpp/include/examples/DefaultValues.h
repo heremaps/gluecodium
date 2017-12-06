0a0e488b3431d2780b2790675b8db428d373d9c8
Daniel Kamkha <daniel.kamkha@here.com>
2017-12-06 15:20:46 +0100

    APIGEN-568: Add "example" test for Default Values in C++
    Change-Id: I56d91769800e24f9d609495be365422292d65dba

---
+++ transpiler/src/test/resources/examples/defaults/output/cpp/include/examples/DefaultValues.h
@@ -0,0 +1,43 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace examples {
+
+class DefaultValues {
+public:
+    virtual ~DefaultValues() = 0;
+
+public:
+enum class SomeEnum {
+    FOO_VALUE,
+    BAR_VALUE
+};
+
+struct StructWithDefaults {
+    int32_t int_field = 42;
+    float float_field = 3.14;
+    bool bool_field = true;
+    ::std::string string_field = "some string";
+    ::examples::DefaultValues::SomeEnum enum_field = ::examples::DefaultValues::SomeEnum::BAR_VALUE;
+};
+};
+
+}
