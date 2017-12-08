b34609fcfeabbaf870d8a2c98158c2f90ab973e6
Georg Bremer <georg.bremer@here.com>
2017-12-08 14:11:27 +0000

    APIGEN-994: Unit tests for CppTypeInfo::createErrorTypeInfo
    Additional test for previously introduced createErrorTypeInfo function.
    
    Change-Id: I24bd91b821bb5b5daf063d8500da54acddd37a03

--- transpiler/src/test/java/com/here/ivi/api/generator/cbridge/CppTypeInfoTest.java
+++ transpiler/src/test/java/com/here/ivi/api/generator/cbridge/CppTypeInfoTest.java
@@ -11,14 +11,19 @@
 
 package com.here.ivi.api.generator.cbridge;
 
+import static com.here.ivi.api.test.Assert.assertContains;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
+import com.here.ivi.api.model.cmodel.CType;
 import com.here.ivi.api.model.cmodel.IncludeResolver;
 import com.here.ivi.api.model.cmodel.IncludeResolver.HeaderType;
 import com.here.ivi.api.model.common.Include;
+import org.franca.core.franca.FEnumerationType;
 import org.franca.core.franca.FModel;
 import org.franca.core.franca.FStructType;
 import org.franca.core.franca.FTypeCollection;
@@ -26,17 +31,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(CBridgeNameRules.class)
 public final class CppTypeInfoTest {
 
   private static final String PACKAGE = "TEST.PACKAGE";
+  private static final String ENUM_NAME = "MyEnum";
 
   @Mock private IncludeResolver resolver;
   @Mock private FTypeCollection typeCollection;
   @Mock private FModel francaModel;
   @Mock private FStructType francaStruct;
+  @Mock private FEnumerationType francaEnum;
 
   private final Include cbridgePublicHeaderInclude = Include.createInternalInclude("foo");
   private final Include cbridgePrivateHeaderInclude = Include.createInternalInclude("bar");
@@ -44,6 +53,7 @@ public final class CppTypeInfoTest {
 
   @Before
   public void setUp() {
+    mockStatic(CBridgeNameRules.class);
     when(francaStruct.eContainer()).thenReturn(typeCollection);
     when(typeCollection.eContainer()).thenReturn(francaModel);
     when(francaModel.getName()).thenReturn(PACKAGE);
@@ -53,6 +63,7 @@ public final class CppTypeInfoTest {
         .thenReturn(cbridgePrivateHeaderInclude);
     when(resolver.resolveInclude(any(), eq(HeaderType.BASE_API_HEADER)))
         .thenReturn(baseApiPublicHeaderInclude);
+    when(CBridgeNameRules.getEnumName(any())).thenReturn(ENUM_NAME);
   }
 
   @Test
@@ -67,5 +78,19 @@ public final class CppTypeInfoTest {
     assertTrue(typeInfo.includes.contains(cbridgePrivateHeaderInclude));
     assertTrue(typeInfo.includes.contains(baseApiPublicHeaderInclude));
     assertTrue(typeInfo.includes.contains(cbridgePublicHeaderInclude));
+  }
+
+  @Test
+  public void createErrorTypeInfoSetsProperIncludes() {
+    CppTypeInfo typeInfo = CppTypeInfo.createErrorTypeInfo(resolver, francaEnum);
+
+    assertContains(typeInfo.functionReturnType.includes, CType.BOOL_INCLUDE);
+  }
+
+  @Test
+  public void createErrorTypeSetsEnumName() {
+    CppTypeInfo typeInfo = CppTypeInfo.createErrorTypeInfo(resolver, francaEnum);
+
+    assertEquals(typeInfo.functionReturnType.name, ENUM_NAME);
   }
 }
