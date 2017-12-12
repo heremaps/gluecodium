d5479d88c430cfd6fe4196a95e406240381cbee5
Sascha Ebel <sascha.ebel@here.com>
2017-12-12 13:24:01 +0100

    APIGEN-578: implements defaults showcase in android app
    Change-Id: I95b445890d66e9a1982e33292d64b6fbd97f6c4d

---
+++ platforms/android/app/src/main/java/com/example/here/hello/app/DefaultsFragment.java
@@ -0,0 +1,51 @@
+package com.example.here.hello.app;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.here.hello.R;
+import com.example.here.hello.utils.InputMethodHelper;
+import com.here.android.hello.HelloWorldDefaults;
+
+public final class DefaultsFragment extends Fragment {
+    private TextView result;
+    private Button submitButton;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_defaults, container, false);
+        result = rootView.findViewById(R.id.defaults_text);
+        submitButton = rootView.findViewById(R.id.defaults_submit);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        submitButton.setOnClickListener(v -> {
+
+            result.setText(generateStructString(HelloWorldDefaults.getStructWithDefaults()));
+
+            // hide virtual keyboard
+            InputMethodHelper.hideSoftKeyboard(getContext(), result.getWindowToken());
+        });
+    }
+
+    private static String generateStructString(final HelloWorldDefaults.StructWithDefaults struct){
+        return "StructWithDefaults {\n" +
+        "intField=" + struct.intField + "\n" +
+        "floatField=" + struct.floatField + "\n" +
+        "boolField=" + struct.boolField + "\n" +
+        "stringField= \"" + struct.stringField + "\"\n" +
+        "enumField=" + struct.enumField + "\n" +
+        "}";
+    }
+}
