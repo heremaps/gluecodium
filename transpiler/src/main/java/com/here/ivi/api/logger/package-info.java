1d990d5923435ccdb760100522e0abb5078b081e
Georg Bremer <georg.bremer@here.com>
2017-12-14 14:32:24 +0000

    APIGEN-931: Document logger
    Change-Id: I1f4eb8e8f70f8c8f61bdc4f02819369bb8187319

---
+++ transpiler/src/main/java/com/here/ivi/api/logger/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Enables logging for the classes inside the generator.
+ *
+ * <h1>Input</h1>
+ *
+ * File with the logging properties.
+ *
+ * <h1>Output</h1>
+ *
+ * It will produce logging messages at different levels.
+ */
+package com.here.ivi.api.logger;
