41d6d3abf749e2fa69e84ea7517b78e6abcd9d45
Sascha Ebel <sascha.ebel@here.com>
2017-12-14 10:42:48 +0100

    MINOR: final adjustments to defaults showcase in android app
    * added expected value text field and spinner to select source
      from which values will be retrieved
    
    Change-Id: I884bfd0a7316a54bc37e157be0bf351944df810f

--- platforms/android/app/src/main/java/com/example/here/hello/app/DefaultsFragment.java
+++ platforms/android/app/src/main/java/com/example/here/hello/app/DefaultsFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.here.hello.R;
@@ -15,37 +16,46 @@ import com.example.here.hello.utils.InputMethodHelper;
 import com.here.android.hello.HelloWorldDefaults;
 
 public final class DefaultsFragment extends Fragment {
-    private TextView result;
-    private Button submitButton;
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_defaults, container, false);
-        result = rootView.findViewById(R.id.defaults_text);
-        submitButton = rootView.findViewById(R.id.defaults_submit);
-        return rootView;
-    }
+  private Spinner spinner;
+  private TextView result;
+  private Button submitButton;
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        submitButton.setOnClickListener(v -> {
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.fragment_defaults, container, false);
+    spinner = rootView.findViewById(R.id.defaults_spinner);
+    result = rootView.findViewById(R.id.defaults_text);
+    submitButton = rootView.findViewById(R.id.defaults_submit);
+    return rootView;
+  }
 
-            result.setText(generateStructString(HelloWorldDefaults.getStructWithDefaults()));
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    submitButton.setOnClickListener(v -> {
 
-            // hide virtual keyboard
-            InputMethodHelper.hideSoftKeyboard(getContext(), result.getWindowToken());
-        });
-    }
+      HelloWorldDefaults.StructWithDefaults resultStruct = spinner.getSelectedItemPosition() == 0
+          ? new HelloWorldDefaults.StructWithDefaults()
+          : HelloWorldDefaults.getStructWithDefaults();
 
-    private static String generateStructString(final HelloWorldDefaults.StructWithDefaults struct){
-        return "StructWithDefaults {\n" +
+      String prefix = getResources().getString(R.string.defaults_retrieval_message) + getResources()
+          .getStringArray(R.array.defaults_source)[spinner.getSelectedItemPosition()];
+      result.setText(prefix + "\n\n" + generateStructString(resultStruct));
+
+      // hide virtual keyboard
+      InputMethodHelper.hideSoftKeyboard(getContext(), result.getWindowToken());
+    });
+  }
+
+  private static String generateStructString(final HelloWorldDefaults.StructWithDefaults struct) {
+    return "StructWithDefaults {\n" +
         "intField=" + struct.intField + "\n" +
         "floatField=" + struct.floatField + "\n" +
         "boolField=" + struct.boolField + "\n" +
         "stringField= \"" + struct.stringField + "\"\n" +
         "enumField=" + struct.enumField + "\n" +
         "}";
-    }
+  }
 }
