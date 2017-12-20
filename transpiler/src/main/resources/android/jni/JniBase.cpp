53e3985d06d705ce2dd99e8a88c2208bb8a54683
Sascha Peilicke <sascha.peilicke@here.com>
2017-12-20 16:12:25 +0100

    Add method here::internal::jni::get_java_vm
    And group JNI base code in android/jni/JniBase.h. Other code needs
    access to the JavaVM as well.
    
    Change-Id: If9502cb5793babce94d87c5b5473807f1f04eafa

---
+++ transpiler/src/main/resources/android/jni/JniBase.cpp
@@ -0,0 +1,55 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2017 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include "JniBase.h"
+
+namespace
+{
+
+static JavaVM* jvm;
+
+} // namespace
+
+
+//JNI_OnLoad() gets automatically called by java vm while loading the library.
+//To make this work neither 'static' keyword (causes "static declaration of 'JNI_OnLoad' follows
+//non-static declaration" - error) nor adding to anonymous namespace (prevents method from being
+//called) is allowed.
+jint
+JNI_OnLoad( JavaVM* vm, void* )
+{
+    JNIEnv* env = nullptr;
+    if ( vm->GetEnv( ( void** )&env, JNI_VERSION_1_6 ) != JNI_OK )
+    {
+        return 0;
+    }
+
+    jvm = vm;
+    return JNI_VERSION_1_6;
+}
+
+
+namespace here
+{
+namespace internal
+{
+namespace jni
+{
+
+JavaVM* get_java_vm()
+{
+    return jvm;
+}
+
+} // namespace jni
+} // namespace internal
+} // namespace here
