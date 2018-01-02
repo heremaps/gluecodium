7baf385bc3271dc18e1c8ea2795f88067f422f50
Robin Wind <ext-robin.wind@here.com>
2018-01-02 11:15:16 +0100

    Use argument parsing in apigen_swift_framework_bundle
    Use argument parsing in apigen_swift_framework_bundle
    function instead of list of sources
    
    Resolve ATEAM-72
    
    Change-Id: I7f566a6ba459a3d3ba7ff0b34b69108263b39c75

--- modules/apigen/ApigenSwiftFrameworkBundle.cmake
+++ modules/apigen/ApigenSwiftFrameworkBundle.cmake
@@ -29,27 +29,31 @@ cmake_minimum_required(VERSION 3.5)
 #
 # The general form of the command is::
 #
-#     apigen_swift_framework_bundle(target assets)
+#     apigen_swift_framework_bundle(TARGET target ASSETS asset1 asset2 ... assetN)
 #
 
-function(apigen_swift_framework_bundle target assets)
+function(apigen_swift_framework_bundle)
+    set(oneValueArgs TARGET)
+    set(multiValueArgs ASSETS)
+    cmake_parse_arguments(apigen_swift_framework_bundle "${options}" "${oneValueArgs}"
+                                           "${multiValueArgs}" ${ARGN})
 
-    get_target_property(GENERATOR ${target} APIGEN_TRANSPILER_GENERATOR)
-    get_target_property(SWIFT_OUTPUT_DIR ${target} APIGEN_SWIFT_BUILD_OUTPUT_DIR)
-    get_target_property(SWIFT_FRAMEWORK_VERSION ${target} APIGEN_SWIFT_FRAMEWORK_VERSION)
-    get_target_property(SWIFT_RESOURCES_DIR ${target} APIGEN_SWIFT_RESOURCES_DIR)
+    get_target_property(GENERATOR ${apigen_swift_framework_bundle_TARGET} APIGEN_TRANSPILER_GENERATOR)
+    get_target_property(SWIFT_OUTPUT_DIR ${apigen_swift_framework_bundle_TARGET} APIGEN_SWIFT_BUILD_OUTPUT_DIR)
+    get_target_property(SWIFT_FRAMEWORK_VERSION ${apigen_swift_framework_bundle_TARGET} APIGEN_SWIFT_FRAMEWORK_VERSION)
+    get_target_property(SWIFT_RESOURCES_DIR ${apigen_swift_framework_bundle_TARGET} APIGEN_SWIFT_RESOURCES_DIR)
 
     if(NOT ${GENERATOR} MATCHES swift)
         return()
     endif()
 
-    message(STATUS "Assets ${SWIFT_BUNDLE_SRC}")
+    message(STATUS "Assets ${apigen_swift_framework_bundle_ASSETS}")
     
     # Copy the folders that need to be in the bundle.
-    foreach(FOLDER ${assets})
-        add_custom_command(TARGET ${target} POST_BUILD
+    foreach(FOLDER ${apigen_swift_framework_bundle_ASSETS})
+        add_custom_command(TARGET ${apigen_swift_framework_bundle_TARGET} POST_BUILD
             COMMAND cp -fR ${FOLDER} 
-                "${SWIFT_OUTPUT_DIR}/${target}.framework/Versions/Current/Resources/")
+                "${SWIFT_OUTPUT_DIR}/${apigen_swift_framework_bundle_TARGET}.framework/Versions/Current/Resources/")
     endforeach()
 
 endfunction(apigen_swift_framework_bundle)
