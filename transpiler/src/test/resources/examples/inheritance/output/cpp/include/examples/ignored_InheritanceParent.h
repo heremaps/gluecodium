163dd074ddf4b2ce291d67e65ca233e4bf556a15
Daniel Kamkha <daniel.kamkha@here.com>
2018-01-03 15:04:30 +0100

    APIGEN-936: Add BaseAPI example for Inheritance
    Change-Id: Ia980d8533d9d969df1985302499fd7a212c2f089

---
+++ transpiler/src/test/resources/examples/inheritance/output/cpp/include/examples/ignored_InheritanceParent.h
@@ -0,0 +1,31 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2018 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <string>
+
+namespace examples {
+
+class InheritanceParent {
+public:
+    virtual ~InheritanceParent() = 0;
+
+public:
+    virtual ::std::string parent_method( const ::std::string& input ) = 0;
+};
+
+}
