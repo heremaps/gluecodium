5d03b2a6e507fbb16c2673296ca8efc95f9ad81f
Dario Limongi <dario.limongi@here.com>
2018-01-04 14:26:50 +0100

    APIGEN-938: Implement BaseAPI for HelloWorldInheritance.fidl
    Change-Id: Ida331cf85926d46184bc97a03ab5ec2c09240e13

---
+++ libhello/src/hello/HelloWorldInheritanceChild.cpp
@@ -0,0 +1,24 @@
+// -------------------------------------------------------------------------------------------------
+//
+// Copyright (C) 2018 HERE Global B.V. and/or its affiliated companies. All rights reserved.
+//
+// This software, including documentation, is protected by copyright controlled by
+// HERE Global B.V. All rights are reserved. Copying, including reproducing, storing,
+// adapting or translating, any or all of this material requires the prior written
+// consent of HERE Global B.V. This material also contains confidential information,
+// which may not be disclosed to others without prior written consent of HERE Global B.V.
+//
+// -------------------------------------------------------------------------------------------------
+
+#include "hello/HelloWorldInheritanceChild.h"
+
+namespace hello
+{
+
+int16_t
+HelloWorldInheritanceChild::hello_world_child_method( const uint8_t input )
+{
+    return input;
+}
+
+}
