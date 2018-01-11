55e91f17668683e19bad7d500c9f5fd902f3671b
elkasimi <abdessamad.elkasimi@here.com>
2018-01-11 15:29:02 +0100

    APIGEN-942: Add inheritance fragment in Android App
    Android show case for inheritance showing that:
    - inheritance relationships are maintained the same as
      declared in the fidl files.
    - display the output of calling overrieded methods on
      the different classes.
    - Update buildToolsVersion to 27.0.3 to fix lint error.
    
    Change-Id: I71ef9a5d43f521f8a723986cce6a9e80ea74253d

---
+++ platforms/android/app/src/main/java/com/example/here/hello/app/InheritanceFragment.java
@@ -0,0 +1,110 @@
+package com.example.here.hello.app;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.here.hello.R;
+
+import com.here.android.hello.Shape;
+import com.here.android.hello.Circle;
+import com.here.android.hello.Rectangle;
+import com.here.android.hello.Square;
+
+public class InheritanceFragment extends Fragment {
+
+    private static final Circle CIRCLE = Circle.createCircle(1.0);
+    private static final Rectangle RECTANGLE = Rectangle.createRectangle(1.0, 2.0);
+    private static final Square SQUARE = Square.createSquare(5.0);
+
+    private Button submitButton;
+    private TextView result;
+    private Spinner spinner;
+    private TextView description;
+    private String[] descriptionsText;
+
+    public InheritanceFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inheritance, container, false);
+
+        spinner = rootView.findViewById(R.id.inheritance_spinner);
+        description = rootView.findViewById(R.id.inheritance_description);
+        submitButton = rootView.findViewById(R.id.inheritance_submit_button);
+        result = rootView.findViewById(R.id.inheritance_result);
+        descriptionsText = getResources().getStringArray(R.array.inheritance_methods_description);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position,
+                                       long id) {
+                description.setText(descriptionsText[position]);
+                result.setText("");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                description.setText("");
+                result.setText("");
+            }
+        });
+
+        submitButton.setOnClickListener((View v) ->
+                handleSelected(spinner.getSelectedItemPosition()));
+    }
+
+    private static String inheritanceCheckResult(Class<?> c, Class<?> d) {
+        return d.getSimpleName() + " is " + (c.isAssignableFrom(d) ? "a" : "not a") + " " + c.getSimpleName() + "\n";
+    }
+
+    private void handleSelected(int selectedItem) {
+        switch (selectedItem) {
+            case 0: {
+                StringBuilder builder = new StringBuilder();
+
+                builder.append(inheritanceCheckResult(Shape.class, Circle.class));
+                builder.append(inheritanceCheckResult(Shape.class, Rectangle.class));
+                builder.append(inheritanceCheckResult(Shape.class, Square.class));
+                builder.append(inheritanceCheckResult(Rectangle.class, Circle.class));
+                builder.append(inheritanceCheckResult(Circle.class, Rectangle.class));
+                builder.append(inheritanceCheckResult(Rectangle.class, Square.class));
+
+                result.setText(builder.toString());
+            }
+            break;
+
+            case 1: {
+                result.setText(CIRCLE.getType());
+            }
+            break;
+
+            case 2: {
+                result.setText(RECTANGLE.getType());
+            }
+            break;
+
+            case 3: {
+                result.setText(SQUARE.getType());
+            }
+            break;
+        }
+    }
+}
