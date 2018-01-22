3b061db9e731052b8a973df8eff3096e0f6faebb
Sascha Peilicke <sascha.peilicke@here.com>
2018-01-22 11:43:46 +0100

    Add ADD_JAR parameter to apigen_android_archive()
    To allow merging additional *.jar files into the resulting *.aar. The
    default behavior is kept that just copies the transpiler-generated jar
    file. If one or multiple additional jars are passed via ADD_JAR, those
    are all extracted into a temporary folder from which the final
    classes.jar is generated.
    
    Resolves: ATEAM-109
    Change-Id: Ice7b0d0f611ac1a22690285a4bd68a41c17dd282

--- modules/apigen/ApigenAndroidArchive.cmake
+++ modules/apigen/ApigenAndroidArchive.cmake
@@ -19,7 +19,7 @@ cmake_minimum_required(VERSION 3.5)
 # -------------------
 #
 # This module assembles an Android Archive (*.aar) file for a target.
-# It requires that apigen_transpile(), apigen_java_compile() and
+# It requires that apigen_android_archive(), apigen_java_compile() and
 # apigen_java_jar() have been invoked on it first.
 #
 # .. command:: apigen_java_jar
@@ -35,19 +35,18 @@ function(apigen_android_archive)
 
     set(options)
     set(oneValueArgs TARGET)
-    set(multiValueArgs ASSETS)
+    set(multiValueArgs ASSETS ADD_JAR)
     cmake_parse_arguments(apigen_android_archive "${options}" "${oneValueArgs}"
                                                  "${multiValueArgs}" ${ARGN})
 
     get_target_property(GENERATOR ${apigen_android_archive_TARGET} APIGEN_TRANSPILER_GENERATOR)
     get_target_property(OUTPUT_DIR ${apigen_android_archive_TARGET} APIGEN_TRANSPILER_GENERATOR_OUTPUT_DIR)
-    get_target_property(APIGEN_JAVA_JAR ${apigen_android_archive_TARGET} APIGEN_JAVA_JAR)
 
     if(NOT ${GENERATOR} MATCHES "android")
-        message(FATAL_ERROR "apigen_android_archive() depends on apigen_transpiler() configured with generator 'android'")
+        message(FATAL_ERROR "apigen_android_archive() depends on apigen_android_archiver() configured with generator 'android'")
     endif()
 
-    set(APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/apigen/android-android-archive)
+    set(APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/apigen/${GENERATOR}-android-archive)
     set(APIGEN_ANDROID_ARCHIVE_JNI_OUTPUT_DIR ${APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR}/jni/${CMAKE_ANDROID_ARCH_ABI})
     set(APIGEN_ANDROID_ARCHIVE_CLASSES_JAR ${APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR}/classes.jar)
     set(APIGEN_ANDROID_ARCHIVE_MANIFEST_XML ${OUTPUT_DIR}/android/AndroidManifest.xml)
@@ -56,7 +55,6 @@ function(apigen_android_archive)
     add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
         COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${APIGEN_ANDROID_ARCHIVE_JNI_OUTPUT_DIR}
         COMMAND ${CMAKE_COMMAND} ARGS -E copy $<TARGET_FILE:${apigen_android_archive_TARGET}> ${APIGEN_ANDROID_ARCHIVE_JNI_OUTPUT_DIR}
-        COMMAND ${CMAKE_COMMAND} ARGS -E copy ${APIGEN_JAVA_JAR} ${APIGEN_ANDROID_ARCHIVE_CLASSES_JAR}
         COMMAND ${CMAKE_COMMAND} ARGS -E copy ${APIGEN_ANDROID_ARCHIVE_MANIFEST_XML} ${APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR}/
         COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR}/assets/
         COMMENT "Assembling Android Archive contents...")
@@ -64,6 +62,28 @@ function(apigen_android_archive)
         add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
             COMMAND cp -fR ${asset} ${APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR}/assets/)
     endforeach()
+
+    get_target_property(APIGEN_JAVA_JAR ${apigen_android_archive_TARGET} APIGEN_JAVA_JAR)
+    if(apigen_android_archive_ADD_JAR)
+      # Copy all jar content into one folder and create a new jar from there
+      set(JAR_MERGE_DIR ${CMAKE_CURRENT_BINARY_DIR}/apigen/${GENERATOR}-jar-merge)
+      add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${JAR_MERGE_DIR})
+
+      foreach(jar ${APIGEN_JAVA_JAR} ${apigen_android_archive_ADD_JAR})
+        add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
+          COMMAND ${Java_JAR_EXECUTABLE} -xvf ${jar}
+          WORKING_DIRECTORY ${JAR_MERGE_DIR}
+          COMMENT "Extracting '${jar}' into '${JAR_MERGE_DIR}'...")
+      endforeach()
+
+      add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
+        COMMAND ${Java_JAR_EXECUTABLE} -cfM ${APIGEN_ANDROID_ARCHIVE_CLASSES_JAR} -C ${JAR_MERGE_DIR} .)
+    else()
+      # Just copy the transpiler-generated jar file as there are no others
+      add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} ARGS -E copy ${APIGEN_JAVA_JAR} ${APIGEN_ANDROID_ARCHIVE_CLASSES_JAR})
+    endif()
 
     add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
         COMMAND ${Java_JAR_EXECUTABLE} -cfM ${APIGEN_ANDROID_ARCHIVE} .
