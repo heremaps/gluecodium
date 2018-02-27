b79768220b9a996ced17ee1cd719c177e9315611
Daniel Kamkha <daniel.kamkha@here.com>
2018-02-27 16:22:41 +0100

    MINOR: Remove useless comments from "jni/Header" template
    Change-Id: I4b4ea3fbfe2d6ea644c02501a7e4695db47486f4

--- transpiler/src/test/resources/smoke/errors/output/android/jni/com_example_smoke_Errors.h
+++ transpiler/src/test/resources/smoke/errors/output/android/jni/com_example_smoke_Errors.h
@@ -2,19 +2,18 @@
  *
  * Automatically generated. Do not modify. Your changes will be lost.
  */
-/**
- * JNI header for class Errors
- */
+
 #pragma once
+
 #include <jni.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-/**
- * Function for Errors.methodWithErrors()
- */
+
 JNIEXPORT void JNICALL
 Java_com_example_smoke_Errors_methodWithErrors(JNIEnv* _jenv, jobject _jinstance);
+
 #ifdef __cplusplus
 }
 #endif
