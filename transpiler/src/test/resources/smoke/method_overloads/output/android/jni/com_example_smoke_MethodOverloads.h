84e4d82480139e8805966f5d956b998bd64e4f4b
Daniel Kamkha <daniel.kamkha@here.com>
2018-02-27 16:22:41 +0100

    MINOR: Remove useless comments from "jni/Header" template
    Change-Id: I4b4ea3fbfe2d6ea644c02501a7e4695db47486f4

--- transpiler/src/test/resources/smoke/method_overloads/output/android/jni/com_example_smoke_MethodOverloads.h
+++ transpiler/src/test/resources/smoke/method_overloads/output/android/jni/com_example_smoke_MethodOverloads.h
@@ -2,9 +2,7 @@
  *
  * Automatically generated. Do not modify. Your changes will be lost.
  */
-/**
- * JNI header for class MethodOverloads
- */
+
 #pragma once
 
 #include <jni.h>
@@ -13,44 +11,27 @@
 extern "C" {
 #endif
 
-/**
- * Function for MethodOverloads.isBoolean()
- */
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBoolean__Z(JNIEnv* _jenv, jobject _jinstance, jboolean jinput);
-/**
- * Function for MethodOverloads.isBoolean()
- */
+
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBoolean__B(JNIEnv* _jenv, jobject _jinstance, jbyte jinput);
-/**
- * Function for MethodOverloads.isBoolean()
- */
+
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBoolean__Ljava_lang_String_2(JNIEnv* _jenv, jobject _jinstance, jstring jinput);
-/**
- * Function for MethodOverloads.isBoolean()
- */
+
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBoolean__Lcom_example_smoke_MethodOverloads_00024Point_2(JNIEnv* _jenv, jobject _jinstance, jobject jinput);
-/**
- * Function for MethodOverloads.isBoolean()
- */
+
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBoolean__ZBLjava_lang_String_2Lcom_example_smoke_MethodOverloads_00024Point_2(JNIEnv* _jenv, jobject _jinstance, jboolean jinput1, jbyte jinput2, jstring jinput3, jobject jinput4);
-/**
- * Function for MethodOverloads.isBooleanStringArrayOverload()
- */
+
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBooleanStringArrayOverload__Ljava_util_List_2(JNIEnv* _jenv, jobject _jinstance, jobject jinput);
-/**
- * Function for MethodOverloads.isBooleanIntArrayOverload()
- */
+
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBooleanIntArrayOverload__Ljava_util_List_2(JNIEnv* _jenv, jobject _jinstance, jobject jinput);
-/**
- * Function for MethodOverloads.isBoolean()
- */
+
 JNIEXPORT jboolean JNICALL
 Java_com_example_smoke_MethodOverloads_isBoolean__(JNIEnv* _jenv, jobject _jinstance);
 
