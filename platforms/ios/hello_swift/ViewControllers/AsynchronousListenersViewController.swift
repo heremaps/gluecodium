f4efdc4412e3e21145297e3acd96f1302cd79127
elkasimi <abdessamad.elkasimi@here.com>
2018-03-08 10:52:44 +0100

    APIGEN-1231: Correct copyright holder
    Added new copyright holder for swift and c++ files.
    
    Change-Id: I72931b7c018dd2069e5c4e8060bb6999b6adee85

--- platforms/ios/hello_swift/ViewControllers/AsynchronousListenersViewController.swift
+++ platforms/ios/hello_swift/ViewControllers/AsynchronousListenersViewController.swift
@@ -1,9 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) 2016-2018 HERE Europe B.V.
 //
-//  ListenersViewController.swift
-//  hello_swift
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Copyright © 2017 HERE. All rights reserved.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+// License-Filename: LICENSE
+//
+// -------------------------------------------------------------------------------------------------
 
 import UIKit
 import hello
