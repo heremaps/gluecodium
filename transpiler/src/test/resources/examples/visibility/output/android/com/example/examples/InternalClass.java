e4a7fad828ac7e09b7485375000ca9da2c792d6b
Daniel Kamkha <daniel.kamkha@here.com>
2018-03-22 16:00:37 +0100

    Add examples for "Internal" FDEPL property
    Change-Id: I0fe9452dcabf44dcabe92f40b69b51485a77935a
    Related: APIGEN-1235, APIGEN-1237

---
+++ transpiler/src/test/resources/examples/visibility/output/android/com/example/examples/InternalClass.java
@@ -0,0 +1,32 @@
+/*
+ *
+ * Automatically generated. Do not modify. Your changes will be lost.
+ */
+
+package com.example.examples;
+
+import com.example.NativeBase;
+
+class InternalClass extends NativeBase {
+
+    static class InternalStruct {
+        public String stringField;
+        public InternalStruct() {}
+        public InternalStruct(String stringField) {
+            this.stringField = stringField;
+        }
+    }
+
+    protected InternalClass(final long nativeHandle) {
+        super(nativeHandle, new Disposer() {
+            @Override
+            public void disposeNative(long handle) {
+                disposeNativeHandle(handle);
+            }
+        });
+    }
+
+    private static native void disposeNativeHandle(long nativeHandle);
+
+    native void internalMethod(final InternalClass.InternalStruct input);
+}
