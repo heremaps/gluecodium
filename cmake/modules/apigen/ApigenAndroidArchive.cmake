14f244ebef48bb005e220da58986e3f8589944ce
Georg Bremer <georg.bremer@here.com>
2018-04-10 12:01:39 +0000

    Remove references to Sparta projects
    Change-Id: I170b3290aed2720aec8d02018b14f3dd0567e5d0
    Relates-To: APIGEN-1283

--- cmake/modules/apigen/ApigenAndroidArchive.cmake
+++ cmake/modules/apigen/ApigenAndroidArchive.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 HERE Europe B.V.
+# Copyright (c) 2018 HERE Europe B.V.
 #
 # All rights reserved.  This software, including documentation, is protected
 # by copyright controlled by HERE.  All rights are reserved.  Copying,
@@ -72,7 +72,7 @@ function(apigen_android_archive)
     endforeach()
 
     # Remove any dead links from the above copy. This can happen for generated files that weren't
-    # built as part of the dependencies for project, such as the  shaders.
+    # built as part of the dependencies for project.
     add_custom_command(TARGET ${apigen_android_archive_TARGET} POST_BUILD
         COMMAND find ${APIGEN_ANDROID_ARCHIVE_OUTPUT_DIR}/assets/ -type l -delete )
 
