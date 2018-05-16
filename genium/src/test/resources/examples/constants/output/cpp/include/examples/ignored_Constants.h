111d8f0e83085485efcb568fface6ee47b93f14e
Daniel Kamkha <daniel.kamkha@here.com>
2018-05-16 10:58:30 +0200

    Add examples for BaseConstants in C++
    Change-Id: I467119e813fc83119de213b3ccfcbd0e4a8e06b2
    Resolves: APIGEN-476

---
+++ genium/src/test/resources/examples/constants/output/cpp/include/examples/ignored_Constants.h
@@ -0,0 +1,29 @@
+// -------------------------------------------------------------------------------------------------
+//
+//
+// -------------------------------------------------------------------------------------------------
+//
+// Automatically generated. Do not modify. Your changes will be lost.
+//
+// -------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace examples {
+
+static const bool BOOL_CONSTANT;
+static const int32_t INT_CONSTANT;
+static const float FLOAT_CONSTANT;
+static const ::std::string STRING_CONSTANT;
+
+enum class StateEnum {
+    OFF,
+    ON
+};
+
+static const ::examples::StateEnum ENUM_CONSTANT;
+
+}
