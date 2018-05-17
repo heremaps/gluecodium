55c72fd5112da7f6a4b533eb96e5ade59fada9ca
Daniel Kamkha <daniel.kamkha@here.com>
2018-05-17 10:40:50 +0200

    Add Java example for BaseConstants
    Change-Id: I7ec9968dffb253a04fccdb0793684159c4b533f4
    Resolves: APIGEN-477

---
+++ genium/src/test/resources/examples/constants/output/android/com/example/examples/ignored_Constants.java
@@ -0,0 +1,15 @@
+/*
+ *
+ * Automatically generated. Do not modify. Your changes will be lost.
+ */
+
+package com.example.examples;
+
+public final class Constants {
+
+    public static final boolean BOOL_CONSTANT = true;
+    public static final int INT_CONSTANT = 42;
+    public static final float FLOAT_CONSTANT = 3.14f;
+    public static final String STRING_CONSTANT = "Foo bar";
+    public static final StateEnum ENUM_CONSTANT = StateEnum.ON;
+}
