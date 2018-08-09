5d294702e0dd3126ee216100ca14ea18ed053b44
Daniel Kamkha <daniel.kamkha@here.com>
2018-08-09 15:04:02 +0200

    Add "ObjcInterface" deployment property
    Added "ObjcInterface" deployment property for interfaces. This property
    is intended for generation of @objc annotations in Swift generated code
    currently, having no effect on generated code in other languages.
    
    Change-Id: Ifd1795fea28679f13e9aeb330494b093e51d6865
    Related: APIGEN-1344

--- genium/src/main/java/com/here/genium/validator/InheritanceValidatorPredicate.java
+++ genium/src/main/java/com/here/genium/validator/InheritanceValidatorPredicate.java
@@ -24,13 +24,24 @@ import com.here.genium.model.franca.FrancaDeploymentModel;
 import org.franca.core.franca.FInterface;
 
 /**
- * Validate that interfaces with IsInterface set to "true" do not inherit from any interface with
- * IsInterface set to "false".
+ * Validate against the inheritance restrictions for interfaces:
+ *
+ * <ul>
+ *   <li>An interface with "IsInterface" set to "true" should not inherit from an interface with
+ *       "IsInterface" set to "false".
+ *   <li>An interface with "ObjcInterface" set to "true" should inherit from an interface with
+ *       "ObjcInterface" set to "false" only if the child interface has "IsInterface" set to "false"
+ *       and the parent interface has "IsInterface" set to "true".
+ * </ul>
  */
 public final class InheritanceValidatorPredicate implements ValidatorPredicate<FInterface> {
 
-  private static final String INVALID_INHERITANCE_MESSAGE =
+  private static final String INVALID_INTERFACE_INHERITANCE_MESSAGE =
       "Interface '%s' is not allowed to inherit from class '%s'.";
+  private static final String INVALID_OBJC_CLASS_INHERITANCE_MESSAGE =
+      "ObjC-compatible class '%s' is not allowed to inherit from non-compatible class '%s'.";
+  private static final String INVALID_OBJC_PROTOCOL_INHERITANCE_MESSAGE =
+      "ObjC-compatible protocol '%s' is not allowed to inherit from non-compatible protocol '%s'.";
 
   @Override
   public Class<FInterface> getElementClass() {
@@ -41,18 +52,37 @@ public final class InheritanceValidatorPredicate implements ValidatorPredicate<F
   public String validate(
       final FrancaDeploymentModel deploymentModel, final FInterface francaInterface) {
 
-    if (!deploymentModel.isInterface(francaInterface)) {
-      return null;
-    }
-
+    // If there is no parent interface, no restrictions are applied.
     FInterface parentInterface = francaInterface.getBase();
-    if (parentInterface == null || deploymentModel.isInterface(parentInterface)) {
+    if (parentInterface == null) {
       return null;
     }
 
-    return String.format(
-        INVALID_INHERITANCE_MESSAGE,
-        FrancaTypeHelper.getFullName(francaInterface),
-        FrancaTypeHelper.getFullName(parentInterface));
+    boolean childIsInterface = deploymentModel.isInterface(francaInterface);
+    boolean parentIsInterface = deploymentModel.isInterface(parentInterface);
+    String errorMessageFormat = null;
+    if (childIsInterface && !parentIsInterface) {
+      // An interface/protocol cannot inherit from a class.
+      errorMessageFormat = INVALID_INTERFACE_INHERITANCE_MESSAGE;
+    } else if (deploymentModel.isObjcInterface(francaInterface)
+        && !deploymentModel.isObjcInterface(francaInterface)) {
+
+      if (childIsInterface) {
+        // An @objc protocol cannot inherit from non-@objc protocol.
+        errorMessageFormat = INVALID_OBJC_PROTOCOL_INHERITANCE_MESSAGE;
+      } else if (!parentIsInterface) {
+        // An @objc class cannot inherit from non-@objc class.
+        errorMessageFormat = INVALID_OBJC_CLASS_INHERITANCE_MESSAGE;
+      }
+    }
+
+    if (errorMessageFormat != null) {
+      return String.format(
+          errorMessageFormat,
+          FrancaTypeHelper.getFullName(francaInterface),
+          FrancaTypeHelper.getFullName(parentInterface));
+    } else {
+      return null;
+    }
   }
 }
