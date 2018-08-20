24cf0bf6edb71509f1484dad5c6d8c38d137c482
Daniel Kamkha <daniel.kamkha@here.com>
2018-08-20 16:01:11 +0200

    Add smoke tests for "External Interfaces"
    Change-Id: I11e895b5bf3a6da6da10bd6968d5d71f89b95169
    Related: APIGEN-1341

--- genium/src/test/resources/smoke/interfaces/output/android/com/example/smoke/NestedInterface.java
+++ genium/src/test/resources/smoke/interfaces/output/android/com/example/smoke/NestedInterface.java
@@ -9,4 +9,7 @@ public interface NestedInterface {
     void setSameTypeInstances(final SimpleInterface interfaceOne, final SimpleInterface interfaceTwo);
     SimpleInterface getInstanceOne();
     SimpleInterface getInstanceTwo();
+    VeryExternalInterface makeMoreExternal(final ExternalInterface input);
+    VeryExternalInterface.SomeStruct makeMoreExternal(final ExternalInterface.SomeStruct input);
+    VeryExternalInterface.SomeEnum makeMoreExternal(final ExternalInterface.SomeEnum input);
 }
