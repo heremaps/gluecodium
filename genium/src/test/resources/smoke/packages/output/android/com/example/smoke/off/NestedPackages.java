4aae739616824de4140ecc10bbd9c919c8ce657c
Daniel Kamkha <daniel.kamkha@here.com>
2018-09-03 14:52:06 +0200

    Fix model name handling in CppNameResolver
    Updated CppNameResolver to treat '.' characters in FModel names as
    namespace separators.
    
    Change-Id: Ie9c1300adbac28abb84ad5077f14bfa3b33c8e9a
    Resolves: APIGEN-1377

---
+++ genium/src/test/resources/smoke/packages/output/android/com/example/smoke/off/NestedPackages.java
@@ -0,0 +1,26 @@
+/*
+ *
+ * Automatically generated. Do not modify. Your changes will be lost.
+ */
+package com.example.smoke.off;
+import com.example.NativeBase;
+public class NestedPackages extends NativeBase {
+    public static class SomeStruct {
+        public String someField;
+        public SomeStruct() {}
+        public SomeStruct(String someField) {
+            this.someField = someField;
+        }
+    }
+    /** For internal use only */
+    protected NestedPackages(final long nativeHandle) {
+        super(nativeHandle, new Disposer() {
+            @Override
+            public void disposeNative(long handle) {
+                disposeNativeHandle(handle);
+            }
+        });
+    }
+    private static native void disposeNativeHandle(long nativeHandle);
+    public static native NestedPackages.SomeStruct basicMethod(final NestedPackages.SomeStruct input);
+}
