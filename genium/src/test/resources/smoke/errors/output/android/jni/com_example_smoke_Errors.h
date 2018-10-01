ef2effa39ebf7f2de25614a98581799020a86921
Daniel Kamkha <daniel.kamkha@here.com>
2018-10-01 14:23:51 +0200

    Add a missing smoke test for Errors feature
    Added a smoke test for a method that has both an error enum and a return
    value.
    
    Change-Id: Ie17bdc0c9245a9e313e09afbf6005d5e83b583b0

--- genium/src/test/resources/smoke/errors/output/android/jni/com_example_smoke_Errors.h
+++ genium/src/test/resources/smoke/errors/output/android/jni/com_example_smoke_Errors.h
@@ -17,6 +17,9 @@ Java_com_example_smoke_Errors_methodWithErrors(JNIEnv* _jenv, jobject _jinstance
 JNIEXPORT void JNICALL
 Java_com_example_smoke_Errors_methodWithExternalErrors(JNIEnv* _jenv, jobject _jinstance);
 
+JNIEXPORT jstring JNICALL
+Java_com_example_smoke_Errors_methodWithErrorsAndReturnValue(JNIEnv* _jenv, jobject _jinstance);
+
 #ifdef __cplusplus
 }
 #endif
--- genium/src/test/resources/smoke/errors/output/cbridge/include/smoke/Errors.h
+++ genium/src/test/resources/smoke/errors/output/cbridge/include/smoke/Errors.h
@@ -7,15 +7,24 @@ extern "C" {
 #endif
 
 #include "cbridge/include/BaseHandle.h"
+#include "cbridge/include/StringHandle.h"
 #include <stdbool.h>
 #include <stdint.h>
 
 typedef uint32_t smoke_Errors_InternalError;
 typedef uint32_t smoke_Errors_ExternalErrors;
+typedef struct {
+    bool has_value;
+    union {
+        smoke_Errors_InternalError error_code;
+        _baseRef returned_value;
+    };
+} smoke_Errors_methodWithErrorsAndReturnValue_result;
 
 void smoke_Errors_release(_baseRef handle);
 smoke_Errors_InternalError smoke_Errors_methodWithErrors();
 smoke_Errors_ExternalErrors smoke_Errors_methodWithExternalErrors();
+smoke_Errors_methodWithErrorsAndReturnValue_result smoke_Errors_methodWithErrorsAndReturnValue();
 
 #ifdef __cplusplus
 }
