599a5cc401af7cd746d5be2d86dcc942180177cb
Daniel Kamkha <daniel.kamkha@here.com>
2018-10-16 14:38:24 +0200

    Rename SwiftContainerType class to SwiftStruct
    Renamed SwiftContainerType class to SwiftStruct. It was only used for
    generating "struct" declarations in Swift generated code. It was not
    used for some other generic purpose as the old name suggested.
    
    Change-Id: Ie83eea635fa6e0a566a2da0ce792166624d08634

--- genium/src/main/java/com/here/genium/model/swift/SwiftStruct.java
+++ genium/src/main/java/com/here/genium/model/swift/SwiftStruct.java
@@ -22,25 +22,25 @@ package com.here.genium.model.swift;
 import java.util.LinkedList;
 import java.util.List;
 
-public final class SwiftContainerType extends SwiftType {
+public final class SwiftStruct extends SwiftType {
 
   public final List<SwiftField> fields = new LinkedList<>();
   public final List<SwiftConstant> constants = new LinkedList<>();
-  public final SwiftContainerType parent;
+  public final SwiftStruct parent;
   public final String cPrefix;
   public final boolean isInterface;
   public final boolean isEquatable;
 
   @SuppressWarnings("ParameterNumber")
   @lombok.Builder(builderClassName = "Builder")
-  private SwiftContainerType(
+  private SwiftStruct(
       final String name,
       final SwiftVisibility visibility,
       final TypeCategory category,
       final String implementingClass,
       final String publicName,
       final boolean optional,
-      final SwiftContainerType parent,
+      final SwiftStruct parent,
       final String cPrefix,
       final boolean isEquatable) {
     super(
@@ -62,8 +62,8 @@ public final class SwiftContainerType extends SwiftType {
 
   @Override
   public SwiftType withAlias(final String aliasName) {
-    SwiftContainerType container =
-        new SwiftContainerType(
+    SwiftStruct container =
+        new SwiftStruct(
             name,
             visibility,
             category,
@@ -80,8 +80,8 @@ public final class SwiftContainerType extends SwiftType {
 
   @Override
   public SwiftType withOptional(final boolean optional) {
-    SwiftContainerType container =
-        new SwiftContainerType(
+    SwiftStruct container =
+        new SwiftStruct(
             name,
             visibility,
             category,
