b5841844ae631d7bdf8f0774f4627f04d7597d13
Daniel Kamkha <daniel.kamkha@here.com>
2018-10-25 13:30:47 +0200

    Updated smoke tests for default initialization fix for C++ structs
    Change-Id: I995b4d85d6fad5cfcb85a2d9482c571f37b83141
    Related: APIGEN-1419

--- genium/src/test/resources/smoke/instances/output/cpp/src/smoke/InstanceWithStruct.cpp
+++ genium/src/test/resources/smoke/instances/output/cpp/src/smoke/InstanceWithStruct.cpp
@@ -12,14 +12,20 @@ namespace smoke {
 
 InstanceWithStruct::~InstanceWithStruct() = default;
 
-InstanceWithStruct::InnerStruct::InnerStruct( ) = default;
+InstanceWithStruct::InnerStruct::InnerStruct( )
+    : value{ }
+{
+}
 
 InstanceWithStruct::InnerStruct::InnerStruct( const int8_t value )
     : value( value )
 {
 }
 
-InstanceWithStruct::StructWithInstance::StructWithInstance( ) = default;
+InstanceWithStruct::StructWithInstance::StructWithInstance( )
+    : instance{ }, instance_not_null{ }, instance_not_null_with_comment{ }
+{
+}
 
 InstanceWithStruct::StructWithInstance::StructWithInstance( const ::std::shared_ptr< ::smoke::SimpleInstantiableOne >& instance, const ::std::shared_ptr< ::smoke::SimpleInstantiableOne >& instance_not_null, const ::std::shared_ptr< ::smoke::SimpleInstantiableOne >& instance_not_null_with_comment )
     : instance( instance ), instance_not_null( instance_not_null ), instance_not_null_with_comment( instance_not_null_with_comment )
