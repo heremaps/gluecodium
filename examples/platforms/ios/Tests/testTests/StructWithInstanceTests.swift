9432b62ee798d53f31ec13fad9083e0c87eae698
Daniel Kamkha <daniel.kamkha@here.com>
2018-11-15 12:56:14 +0000

    Revert "Add infrastructure for catching fatalError() in Swift functional tests"
    This reverts commit 85ed58d550d687c60201bafab407587c84ae9081.
    
    The fatalError-catching infrastructure increased the execution time for
    Swift functional tests on CI from 1.5 minutes to 17 minutes. This huge
    slowdown is not worth it.
    
    Change-Id: Ia8e121fd001459020684a3a747dcb8873983dc1b
    Related: APIGEN-1427

--- examples/platforms/ios/Tests/testTests/StructWithInstanceTests.swift
+++ examples/platforms/ios/Tests/testTests/StructWithInstanceTests.swift
@@ -60,37 +60,18 @@ class StructWithInstanceTests: XCTestCase {
         XCTAssertNotNil(structNotNull.mySelf)
     }
 
-    func testNullInstanceInNotNullStruct() {
-        var fatalErrorHappened = false
-        let expectation = self.expectation(description: "expectingFatalError")
-        FatalErrorHelper.replaceFatalError { _, _, _ in
-            fatalErrorHappened = true
-            expectation.fulfill()
-            self.unreachable()
-        }
-
-        DispatchQueue.global(qos: .userInitiated).async {
-            hello.InstanceInStruct.createInEmptyNotNullStruct()
-        }
-
-        waitForExpectations(timeout: 10) { _ in
-            XCTAssertTrue(fatalErrorHappened)
-            FatalErrorHelper.restoreFatalError()
-        }
-    }
-
-    private func unreachable() -> Never {
-        repeat {
-            RunLoop.current.run()
-        } while (true)
-    }
+// TODO: APIGEN-1427 add infrastructure for catching fatalError(), rewrite this test and reenable it
+//    func testNullInstanceInNotNullStruct() {
+//        let emptyStruct = hello.InstanceInStruct.createInEmptyNotNullStruct()
+//        XCTAssertNil(emptyStruct)
+//    }
 
     static var allTests = [
         ("testAssignInstanceToStruct", testAssignInstanceToStruct),
         ("testCopyInstanceInStruct", testCopyInstanceInStruct),
         ("testCopyInstanceInStructFromMethod", testCopyInstanceInStructFromMethod),
         ("testNullInstanceInStruct", testNullInstanceInStruct),
-        ("testInstanceInNotNullStruct", testInstanceInNotNullStruct),
-        ("testNullInstanceInNotNullStruct", testNullInstanceInNotNullStruct)
+        ("testInstanceInNotNullStruct", testInstanceInNotNullStruct)/*,
+        ("testNullInstanceInNotNullStruct", testNullInstanceInNotNullStruct)*/
     ]
 }
