7999793f109764148449e10dee29ae9d88cc9231
Daniel Kamkha <daniel.kamkha@here.com>
2018-11-22 14:06:10 +0100

    Add smoke tests for "NotNull" method parameters
    Change-Id: Ibf498c1935a4b40fe2aa695d5076229486d158d5
    Related: APIGEN-1450

--- genium/src/test/resources/smoke/instances/output/android/com/example/smoke/NestedInstantiable.java
+++ genium/src/test/resources/smoke/instances/output/android/com/example/smoke/NestedInstantiable.java
@@ -21,4 +21,5 @@ public class NestedInstantiable extends NativeBase {
     private static native void disposeNativeHandle(long nativeHandle);
     public native void setSameTypeInstances(final SimpleInstantiable instanceOne, final SimpleInstantiable instanceTwo);
     public native SimpleInstantiable getInstanceOne();
+    public native SimpleInstantiable instanceNotNullMethod(final SimpleInstantiable input);
 }
