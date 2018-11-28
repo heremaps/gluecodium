5a11465940b05e4727093e6d9c36f12e2bd486d7
Daniel Kamkha <daniel.kamkha@here.com>
2018-11-28 11:13:19 +0100

    Add "NotNull" for attributes; implement it for C++ and Java
    Change-Id: Ib718183f9a1bb49e99e3aadecd6c13c3fb6bf186
    Related: APIGEN-1456

--- genium/src/test/java/com/here/genium/generator/java/JavaModelBuilderTest.java
+++ genium/src/test/java/com/here/genium/generator/java/JavaModelBuilderTest.java
@@ -679,7 +679,7 @@ public class JavaModelBuilderTest {
   }
 
   @Test
-  public void finishBuildingFrancaAttributeMarksGetterAsNotNull() {
+  public void finishBuildingFrancaAttributeGetterReadsNotNullFromType() {
     when(typeMapper.getNotNullAnnotation()).thenReturn(javaCustomType);
     contextStack.injectResult(JavaCustomType.builder("FooType").isNotNull(true).build());
     modelBuilder.finishBuilding(francaAttribute);
@@ -687,6 +687,47 @@ public class JavaModelBuilderTest {
     JavaMethod javaMethod = modelBuilder.getFinalResult(JavaMethod.class);
     assertNotNull(javaMethod);
     assertTrue(javaMethod.annotations.contains(javaCustomType));
+  }
+
+  @Test
+  public void finishBuildingFrancaAttributeSetterReadsNotNullFromType() {
+    when(typeMapper.getNotNullAnnotation()).thenReturn(javaCustomType);
+    contextStack.injectResult(JavaCustomType.builder("FooType").isNotNull(true).build());
+    modelBuilder.finishBuilding(francaAttribute);
+
+    List<JavaMethod> methods =
+        CollectionsHelper.getAllOfType(modelBuilder.getFinalResults(), JavaMethod.class);
+    assertEquals(2, methods.size());
+    JavaMethod setterMethod = methods.get(1);
+    assertEquals(1, setterMethod.parameters.size());
+    assertTrue(setterMethod.parameters.get(0).annotations.contains(javaCustomType));
+  }
+
+  @Test
+  public void finishBuildingFrancaAttributeGetterReadsNotNullFromAttribute() {
+    when(typeMapper.getNotNullAnnotation()).thenReturn(javaCustomType);
+    contextStack.injectResult(JavaCustomType.builder("FooType").build());
+    when(deploymentModel.isNotNull(any())).thenReturn(true);
+    modelBuilder.finishBuilding(francaAttribute);
+
+    JavaMethod javaMethod = modelBuilder.getFinalResult(JavaMethod.class);
+    assertNotNull(javaMethod);
+    assertTrue(javaMethod.annotations.contains(javaCustomType));
+  }
+
+  @Test
+  public void finishBuildingFrancaAttributeSetterReadsNotNullFromAttribute() {
+    when(typeMapper.getNotNullAnnotation()).thenReturn(javaCustomType);
+    contextStack.injectResult(JavaCustomType.builder("FooType").build());
+    when(deploymentModel.isNotNull(any())).thenReturn(true);
+    modelBuilder.finishBuilding(francaAttribute);
+
+    List<JavaMethod> methods =
+        CollectionsHelper.getAllOfType(modelBuilder.getFinalResults(), JavaMethod.class);
+    assertEquals(2, methods.size());
+    JavaMethod setterMethod = methods.get(1);
+    assertEquals(1, setterMethod.parameters.size());
+    assertTrue(setterMethod.parameters.get(0).annotations.contains(javaCustomType));
   }
 
   @Test
