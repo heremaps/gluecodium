a2954172c6e50d162ca13fcc18e566b567671ee7
Daniel Kamkha <daniel.kamkha@here.com>
2018-12-12 13:48:21 +0100

    Document "special" names and add smoke tests for them
    Documented "special" naming conventions for names of the FIDL entities
    and added corresponding smoke tests.
    
    Change-Id: I956cca5fba215f09520b787d7159e0f5bc299bd1
    Resolves: APIGEN-1496

---
+++ genium/src/test/resources/smoke/method_overloads/output/android/jni/com_example_smoke_SpecialNames.h
@@ -0,0 +1,22 @@
+/*
+ *
+ * Automatically generated. Do not modify. Your changes will be lost.
+ */
+#pragma once
+#include <jni.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+JNIEXPORT void JNICALL
+Java_com_example_smoke_SpecialNames_create(JNIEnv* _jenv, jobject _jinstance);
+JNIEXPORT void JNICALL
+Java_com_example_smoke_SpecialNames_release(JNIEnv* _jenv, jobject _jinstance);
+JNIEXPORT void JNICALL
+Java_com_example_smoke_SpecialNames_createProxy(JNIEnv* _jenv, jobject _jinstance);
+JNIEXPORT void JNICALL
+Java_com_example_smoke_SpecialNames_update(JNIEnv* _jenv, jobject _jinstance);
+JNIEXPORT void JNICALL
+Java_com_example_smoke_SpecialNames_Uppercase(JNIEnv* _jenv, jobject _jinstance);
+#ifdef __cplusplus
+}
+#endif
