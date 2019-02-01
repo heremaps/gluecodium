1561e3074718509f42e1eba2b7cb4a04ed8f9cd5
Daniel Kamkha <daniel.kamkha@here.com>
2019-02-01 14:01:20 +0100

    Generate documentation comment for constructors in Java and Swift
    Moved generated documentation comment for constructors in Java and Swift
    from the private static method (where it is not visible to the user of
    the API) to the constructor itself.
    
    Change-Id: Ibed526e23a0dfdc105a946cab9cde40ec084fe9e
    Resolves: APIGEN-1530

---
+++ genium/src/test/resources/smoke/comments/output/android/com/example/smoke/CommentsInstantiable.java
@@ -0,0 +1,27 @@
+/*
+ *
+ * Automatically generated. Do not modify. Your changes will be lost.
+ */
+package com.example.smoke;
+import com.example.NativeBase;
+public class CommentsInstantiable extends NativeBase {
+    /**
+     * This is some very useful constructor.
+     * @param input Very useful input parameter
+     * @return Very useful result
+     */
+    public CommentsInstantiable(final String input) {
+        this(create(input));
+    }
+    /** For internal use only */
+    protected CommentsInstantiable(final long nativeHandle) {
+        super(nativeHandle, new Disposer() {
+            @Override
+            public void disposeNative(long handle) {
+                disposeNativeHandle(handle);
+            }
+        });
+    }
+    private static native void disposeNativeHandle(long nativeHandle);
+    private static native long create(final String input);
+}
