774bfbd7bfa9cbff1f23a0a42bb8648596c55ba6
Georg Bremer <georg.bremer@here.com>
2019-02-22 12:47:18 +0000

    Fix HelloWorld app for newer Android SDK
    Linter complains when not using latest SDK for building. Updating it and
    fixing follow up issues.
    The order for dependent repositories needs to be different, google wants
    to come first, otherwise some baseLibrary.jar cannot be found.
    
    Change-Id: I96f2614bc762dd593d25ae97c0d909feee1b7adc

--- examples/platforms/android/app/src/test/java/com/here/android/test/CppProxyWithComplexListenersTest.java
+++ examples/platforms/android/app/src/test/java/com/here/android/test/CppProxyWithComplexListenersTest.java
@@ -21,7 +21,8 @@ package com.here.android.test;
 import static junit.framework.Assert.assertTrue;
 
 import android.os.Build;
-import android.support.compat.BuildConfig;
+
+import com.example.here.hello.BuildConfig;
 import com.here.android.RobolectricApplication;
 import java.util.ArrayList;
 import java.util.List;
