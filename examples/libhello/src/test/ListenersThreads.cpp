a2866310c7465daf5572453322054358cc2cf365
Georg Bremer <georg.bremer@here.com>
2019-02-27 15:15:59 +0000

    Add instrumented test for non-Java thread callbacks
    Calling back into Java from non-Java created thread is problematic
    because the thread does not have a context class loader. Tests confirm
    this but could not reproduce any issue arising from it.
    
    Change-Id: I3643a9059e6cf6d99fb9688600ebdc78aa7e1faf
    Resolves: APIGEN-1548

---
+++ examples/libhello/src/test/ListenersThreads.cpp
@@ -0,0 +1,75 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (C) 2016-2019 HERE Europe B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+// License-Filename: LICENSE
+//
+// -------------------------------------------------------------------------------------------------
+
+#include "test/ThreadedNotifier.h"
+
+#include <memory>
+#include <string>
+#include <thread>
+
+namespace test
+{
+namespace
+{
+void
+notify_impl( const std::shared_ptr< ThreadedListener >& listener,
+             const std::string& message,
+             int64_t& response )
+{
+    listener->unloaded( UnloadedClass::create( ) );
+    response = listener->on_event( message );
+}
+}
+
+class ThreadedNotifierImpl : public ThreadedNotifier
+{
+public:
+    int64_t
+    notify( const std::shared_ptr< ThreadedListener >& listener,
+            const std::string& message ) override
+    {
+        int64_t response;
+        std::thread notification_thread( notify_impl, listener, message, std::ref( response ) );
+        notification_thread.join( );
+        return response;
+    }
+};
+
+class UnloadedClassImpl : public UnloadedClass
+{
+public:
+    virtual ~UnloadedClassImpl( ) = default;
+    uint8_t increment( uint8_t in ) {
+        return ++in;
+    }
+};
+
+std::shared_ptr< UnloadedClass >
+UnloadedClass::create( )
+{
+    return std::make_shared< UnloadedClassImpl >( );
+}
+
+std::shared_ptr< ThreadedNotifier >
+ThreadedNotifier::create_on_new_thread( )
+{
+    return std::make_shared< ThreadedNotifierImpl >( );
+}
+}
---
+++ examples/platforms/android/app/src/androidTest/java/com/here/android/test/ClassLoaderNonJavaThreadTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2016-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+package com.here.android.test;
+
+import android.support.annotation.NonNull;
+
+import com.here.android.hello.HelloWorld;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+/**
+ * These tests need to be run on the device since they will always pass on emulator.
+ */
+public class ClassLoaderNonJavaThreadTest {
+    class ClassLoaderListener implements ThreadedListener {
+
+        @Override
+        public long onEvent(String message) {
+            assertNull(Thread.currentThread().getContextClassLoader());
+            return Thread.currentThread().getId();
+        }
+
+        @Override
+        public void unloaded(UnloadedClass unloaded) {
+            assertNotNull(unloaded);
+            assertEquals(1, unloaded.increment((short)0));
+        }
+    }
+
+    @Test
+    public void testMultipleRoundTripsWithClassLoading() {
+        ThreadedNotifier notifier = new ThreadedNotifier();
+
+        ClassLoaderListener listener = new ClassLoaderListener();
+
+        long response = notifier.notify(listener, "foo");
+
+        assertFalse(Thread.currentThread().getId() == response);
+    }
+}
+
