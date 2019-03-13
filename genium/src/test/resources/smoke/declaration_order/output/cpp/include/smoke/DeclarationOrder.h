19bff75ebc88d1a95eabd32a572225e17e8b0d4a
Daniel Kamkha <daniel.kamkha@here.com>
2019-03-13 09:25:48 +0100

    Improve DeclarationOrder smoke test
    Reordered type declarations in DeclarationOrder.fidl so that they are
    guaranteed to be in the wrong order in C++ generated code if
    TopologicalSort algorithm is fully or partially unapplied.
    
    Also updated the Map type definition in DeclarationOrder.fidl to have an
    Array value type so that the mutual ordering of those two is tested as
    well.
    
    Change-Id: I8e552ba3ad3eeb350f323ca995e8f736f2b82d02

--- genium/src/test/resources/smoke/declaration_order/output/cpp/include/smoke/DeclarationOrder.h
+++ genium/src/test/resources/smoke/declaration_order/output/cpp/include/smoke/DeclarationOrder.h
@@ -6,32 +6,25 @@
 // Automatically generated. Do not modify. Your changes will be lost.
 //
 // -------------------------------------------------------------------------------------------------
-
 #pragma once
-
 #include "Export.h"
 #include <cstdint>
 #include <string>
 #include <unordered_map>
 #include <vector>
-
 namespace smoke {
-
 enum class SomeEnum {
     FOO,
     BAR = 7
 };
-
+using SomeTypeDef = int32_t;
 struct _GENIUM_CPP_EXPORT NestedStruct {
     ::std::string some_field;
     NestedStruct( );
     NestedStruct( const ::std::string& some_field );
 };
-
-using SomeTypeDef = int32_t;
 using NestedStructArray = ::std::vector< ::smoke::NestedStruct >;
-using ErrorCodeToMessageMap = ::std::unordered_map< int32_t, ::std::string >;
-
+using ErrorCodeToMessageMap = ::std::unordered_map< int32_t, ::smoke::NestedStructArray >;
 struct _GENIUM_CPP_EXPORT MainStruct {
     ::smoke::NestedStruct struct_field;
     ::smoke::SomeTypeDef type_def_field;
@@ -41,5 +34,4 @@ struct _GENIUM_CPP_EXPORT MainStruct {
     MainStruct( );
     MainStruct( const ::smoke::NestedStruct& struct_field, const ::smoke::SomeTypeDef type_def_field, const ::smoke::NestedStructArray& struct_array_field, const ::smoke::ErrorCodeToMessageMap& map_field, const ::smoke::SomeEnum enum_field );
 };
-
 }
