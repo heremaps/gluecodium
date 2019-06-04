47b4b926bd7186ec04f651c98a57c41203a1a1ae
Daniel Kamkha <daniel.kamkha@here.com>
2019-06-04 09:05:10 +0200

    Kotlinize CppTemplateTypeRefTest
    Change-Id: Id41b1972c0196d678a11978aa1b994d020534095
    Related: APIGEN-1399

--- genium/src/test/java/com/here/genium/model/cpp/CppTemplateTypeRefTest.kt
+++ genium/src/test/java/com/here/genium/model/cpp/CppTemplateTypeRefTest.kt
@@ -17,128 +17,118 @@
  * License-Filename: LICENSE
  */
 
-package com.here.genium.model.cpp;
+package com.here.genium.model.cpp
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import com.here.genium.generator.cpp.CppLibraryIncludes
+import com.here.genium.model.common.Include
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-import com.here.genium.generator.cpp.CppLibraryIncludes;
-import com.here.genium.model.common.Include;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+@RunWith(JUnit4::class)
+class CppTemplateTypeRefTest {
+    @Test
+    fun sharedPointerOfPrimitiveType() {
+        val templateTypeRef = CppTemplateTypeRef.create(
+            CppTemplateTypeRef.TemplateClass.SHARED_POINTER, INT_TYPE_REF
+        )
 
-@RunWith(JUnit4.class)
-public final class CppTemplateTypeRefTest {
+        assertEquals(
+            CppTemplateTypeRef.TemplateClass.SHARED_POINTER, templateTypeRef.templateClass
+        )
+        assertEquals(1, templateTypeRef.templateParameters.size)
+        assertEquals(INT_TYPE_REF, templateTypeRef.templateParameters[0])
+        assertEquals(2, templateTypeRef.includes.size)
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.MEMORY))
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.INT_TYPES))
+    }
 
-  private static final List<Include> INCLUDES =
-      Arrays.asList(
-          Include.Companion.createInternalInclude("a"),
-          Include.Companion.createInternalInclude("b"));
+    @Test
+    fun sharedPointerOfComplexType() {
+        val templateTypeRef = CppTemplateTypeRef.create(
+            CppTemplateTypeRef.TemplateClass.SHARED_POINTER, COMPLEX_TYPE_REF
+        )
 
-  private static final CppPrimitiveTypeRef INT_TYPE_REF = CppPrimitiveTypeRef.Companion.getINT32();
+        assertEquals(
+            CppTemplateTypeRef.TemplateClass.SHARED_POINTER, templateTypeRef.templateClass
+        )
+        assertEquals(1, templateTypeRef.templateParameters.size)
+        assertEquals(COMPLEX_TYPE_REF, templateTypeRef.templateParameters[0])
+        assertEquals(3, templateTypeRef.includes.size)
+        assertTrue(templateTypeRef.includes.containsAll(INCLUDES))
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.MEMORY))
+    }
 
-  private static final CppComplexTypeRef COMPLEX_TYPE_REF =
-      new CppComplexTypeRef("Custom", INCLUDES, null, false, false);
+    @Test
+    fun map() {
+        val templateTypeRef = CppTemplateTypeRef.create(
+            CppTemplateTypeRef.TemplateClass.MAP, INT_TYPE_REF, COMPLEX_TYPE_REF
+        )
 
-  private static final List<Include> ERROR_INCLUDES =
-      Collections.singletonList(Include.Companion.createInternalInclude("E"));
+        assertEquals(CppTemplateTypeRef.TemplateClass.MAP, templateTypeRef.templateClass)
+        assertEquals(2, templateTypeRef.templateParameters.size)
+        assertEquals(INT_TYPE_REF, templateTypeRef.templateParameters[0])
+        assertEquals(COMPLEX_TYPE_REF, templateTypeRef.templateParameters[1])
+        assertEquals(4, templateTypeRef.includes.size)
+        assertTrue(templateTypeRef.includes.containsAll(INCLUDES))
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.INT_TYPES))
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.MAP))
+    }
 
-  private static final CppComplexTypeRef ERROR_TYPE_REF =
-      new CppComplexTypeRef("CustomError", ERROR_INCLUDES, null, false, false);
+    @Test
+    fun vectorOfPrimitiveType() {
+        val templateTypeRef =
+            CppTemplateTypeRef.create(CppTemplateTypeRef.TemplateClass.VECTOR, INT_TYPE_REF)
 
-  @Test
-  public void sharedPointerOfPrimitiveType() {
-    CppTemplateTypeRef templateTypeRef =
-        CppTemplateTypeRef.Companion.create(
-            CppTemplateTypeRef.TemplateClass.SHARED_POINTER, INT_TYPE_REF);
+        assertEquals(CppTemplateTypeRef.TemplateClass.VECTOR, templateTypeRef.templateClass)
+        assertEquals(1, templateTypeRef.templateParameters.size)
+        assertEquals(INT_TYPE_REF, templateTypeRef.templateParameters[0])
 
-    assertEquals(
-        CppTemplateTypeRef.TemplateClass.SHARED_POINTER, templateTypeRef.getTemplateClass());
-    assertEquals(1, templateTypeRef.getTemplateParameters().size());
-    assertEquals(INT_TYPE_REF, templateTypeRef.getTemplateParameters().get(0));
-    assertEquals(2, templateTypeRef.includes.size());
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.MEMORY));
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.INT_TYPES));
-  }
+        assertEquals(2, templateTypeRef.includes.size)
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.VECTOR))
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.INT_TYPES))
+    }
 
-  @Test
-  public void sharedPointerOfComplexType() {
-    CppTemplateTypeRef templateTypeRef =
-        CppTemplateTypeRef.Companion.create(
-            CppTemplateTypeRef.TemplateClass.SHARED_POINTER, COMPLEX_TYPE_REF);
+    @Test
+    fun vectorOfComplexType() {
+        val templateTypeRef =
+            CppTemplateTypeRef.create(CppTemplateTypeRef.TemplateClass.VECTOR, COMPLEX_TYPE_REF)
 
-    assertEquals(
-        CppTemplateTypeRef.TemplateClass.SHARED_POINTER, templateTypeRef.getTemplateClass());
-    assertEquals(1, templateTypeRef.getTemplateParameters().size());
-    assertEquals(COMPLEX_TYPE_REF, templateTypeRef.getTemplateParameters().get(0));
-    assertEquals(3, templateTypeRef.includes.size());
-    assertTrue(templateTypeRef.includes.containsAll(INCLUDES));
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.MEMORY));
-  }
+        assertEquals(CppTemplateTypeRef.TemplateClass.VECTOR, templateTypeRef.templateClass)
+        assertEquals(1, templateTypeRef.templateParameters.size)
+        assertEquals(COMPLEX_TYPE_REF, templateTypeRef.templateParameters[0])
 
-  @Test
-  public void map() {
-    CppTemplateTypeRef templateTypeRef =
-        CppTemplateTypeRef.Companion.create(
-            CppTemplateTypeRef.TemplateClass.MAP, INT_TYPE_REF, COMPLEX_TYPE_REF);
+        assertEquals(3, templateTypeRef.includes.size)
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.VECTOR))
+        assertTrue(templateTypeRef.includes.containsAll(INCLUDES))
+    }
 
-    assertEquals(CppTemplateTypeRef.TemplateClass.MAP, templateTypeRef.getTemplateClass());
-    assertEquals(2, templateTypeRef.getTemplateParameters().size());
-    assertEquals(INT_TYPE_REF, templateTypeRef.getTemplateParameters().get(0));
-    assertEquals(COMPLEX_TYPE_REF, templateTypeRef.getTemplateParameters().get(1));
-    assertEquals(4, templateTypeRef.includes.size());
-    assertTrue(templateTypeRef.includes.containsAll(INCLUDES));
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.INT_TYPES));
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.MAP));
-  }
+    @Test
+    fun expected() {
+        val templateTypeRef = CppTemplateTypeRef.create(
+            "", CppTemplateTypeRef.TemplateClass.RETURN, COMPLEX_TYPE_REF, ERROR_TYPE_REF
+        )
 
-  @Test
-  public void vectorOfPrimitiveType() {
-    CppTemplateTypeRef templateTypeRef =
-        CppTemplateTypeRef.Companion.create(CppTemplateTypeRef.TemplateClass.VECTOR, INT_TYPE_REF);
+        assertEquals(CppTemplateTypeRef.TemplateClass.RETURN, templateTypeRef.templateClass)
+        assertEquals(2, templateTypeRef.templateParameters.size)
+        assertEquals(COMPLEX_TYPE_REF, templateTypeRef.templateParameters[0])
+        assertEquals(ERROR_TYPE_REF, templateTypeRef.templateParameters[1])
 
-    assertEquals(CppTemplateTypeRef.TemplateClass.VECTOR, templateTypeRef.getTemplateClass());
-    assertEquals(1, templateTypeRef.getTemplateParameters().size());
-    assertEquals(INT_TYPE_REF, templateTypeRef.getTemplateParameters().get(0));
+        assertEquals(4, templateTypeRef.includes.size)
+        assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.RETURN))
+        assertTrue(templateTypeRef.includes.containsAll(INCLUDES))
+        assertTrue(templateTypeRef.includes.containsAll(ERROR_INCLUDES))
+    }
 
-    assertEquals(2, templateTypeRef.includes.size());
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.VECTOR));
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.INT_TYPES));
-  }
-
-  @Test
-  public void vectorOfComplexType() {
-    CppTemplateTypeRef templateTypeRef =
-        CppTemplateTypeRef.Companion.create(
-            CppTemplateTypeRef.TemplateClass.VECTOR, COMPLEX_TYPE_REF);
-
-    assertEquals(CppTemplateTypeRef.TemplateClass.VECTOR, templateTypeRef.getTemplateClass());
-    assertEquals(1, templateTypeRef.getTemplateParameters().size());
-    assertEquals(COMPLEX_TYPE_REF, templateTypeRef.getTemplateParameters().get(0));
-
-    assertEquals(3, templateTypeRef.includes.size());
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.VECTOR));
-    assertTrue(templateTypeRef.includes.containsAll(INCLUDES));
-  }
-
-  @Test
-  public void expected() {
-    CppTemplateTypeRef templateTypeRef =
-        CppTemplateTypeRef.Companion.create(
-            "", CppTemplateTypeRef.TemplateClass.RETURN, COMPLEX_TYPE_REF, ERROR_TYPE_REF);
-
-    assertEquals(CppTemplateTypeRef.TemplateClass.RETURN, templateTypeRef.getTemplateClass());
-    assertEquals(2, templateTypeRef.getTemplateParameters().size());
-    assertEquals(COMPLEX_TYPE_REF, templateTypeRef.getTemplateParameters().get(0));
-    assertEquals(ERROR_TYPE_REF, templateTypeRef.getTemplateParameters().get(1));
-
-    assertEquals(4, templateTypeRef.includes.size());
-    assertTrue(templateTypeRef.includes.contains(CppLibraryIncludes.RETURN));
-    assertTrue(templateTypeRef.includes.containsAll(INCLUDES));
-    assertTrue(templateTypeRef.includes.containsAll(ERROR_INCLUDES));
-  }
+    companion object {
+        private val INCLUDES =
+            listOf(Include.createInternalInclude("a"), Include.createInternalInclude("b"))
+        private val INT_TYPE_REF = CppPrimitiveTypeRef.INT32
+        private val COMPLEX_TYPE_REF = CppComplexTypeRef("Custom", INCLUDES, null, false, false)
+        private val ERROR_INCLUDES = listOf(Include.createInternalInclude("E"))
+        private val ERROR_TYPE_REF =
+            CppComplexTypeRef("CustomError", ERROR_INCLUDES, null, false, false)
+    }
 }
