724e15cadf1c0fc3faf356b9b3ae13e55ddf9363
Daniel Kamkha <daniel.kamkha@here.com>
2020-02-17 10:59:04 +0100

    Add error handling to calling Dart callbacks from C++
    Updated Dart and FFI templates to propagate exceptions thrown in Dart
    code to C++ as error code, when calling Dart callbacks from C++.
    
    Added smoke and functional tests as appropriate.
    
    Resolves: 137
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

---
+++ gluecodium/src/main/java/com/here/gluecodium/generator/ffi/FfiCppReturnTypeNameResolver.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.gluecodium.generator.ffi
+
+import com.here.gluecodium.cli.GluecodiumExecutionException
+import com.here.gluecodium.generator.common.NameResolver
+import com.here.gluecodium.model.lime.LimeBasicType
+import com.here.gluecodium.model.lime.LimeBasicType.TypeId
+import com.here.gluecodium.model.lime.LimeEnumeration
+import com.here.gluecodium.model.lime.LimeFunction
+
+internal class FfiCppReturnTypeNameResolver(
+    private val internalNamespace: List<String>,
+    private val ffiCppNameResolver: FfiCppNameResolver
+) : NameResolver {
+
+    override fun resolveName(element: Any): String =
+        when (element) {
+            is LimeFunction -> resolveReturnTypeName(element)
+            else -> throw GluecodiumExecutionException("Unsupported element type ${element.javaClass.name}")
+        }
+
+    private fun resolveReturnTypeName(limeFunction: LimeFunction): String {
+        val returnTypeName = ffiCppNameResolver.resolveName(limeFunction.returnType.typeRef)
+        val exceptionPayload = limeFunction.exception?.errorType?.type ?: return returnTypeName
+
+        val returnPrefix = (internalNamespace + "Return").joinToString("::")
+        if (exceptionPayload.actualType !is LimeEnumeration) {
+            return "$returnPrefix<$returnTypeName, ${ffiCppNameResolver.resolveName(exceptionPayload)}>"
+        }
+
+        val actualReturnType = limeFunction.returnType.typeRef.type.actualType
+        return if ((actualReturnType as? LimeBasicType)?.typeId == TypeId.VOID) {
+            STD_ERROR_CODE
+        } else {
+            "$returnPrefix<$returnTypeName, $STD_ERROR_CODE>"
+        }
+    }
+
+    companion object {
+        private const val STD_ERROR_CODE = "std::error_code"
+    }
+}
