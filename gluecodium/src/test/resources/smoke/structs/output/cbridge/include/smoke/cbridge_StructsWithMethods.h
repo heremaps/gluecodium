45d9055216e0949cd31a2b9adbcf2ccb2d660b2e
Daniel Kamkha <daniel.kamkha@here.com>
2020-11-03 10:24:13 +0100

    Add CBridge header reference files to "structs" smoke test
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

---
+++ gluecodium/src/test/resources/smoke/structs/output/cbridge/include/smoke/cbridge_StructsWithMethods.h
@@ -0,0 +1,33 @@
+//
+//
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "cbridge/include/BaseHandle.h"
+#include "cbridge/include/Export.h"
+#include "cbridge/include/smoke/cbridge_ValidationUtils.h"
+#include <stdbool.h>
+#include <stdint.h>
+_GLUECODIUM_C_EXPORT _baseRef smoke_StructsWithMethods_Vector_create_handle(double x, double y);
+_GLUECODIUM_C_EXPORT void smoke_StructsWithMethods_Vector_release_handle(_baseRef handle);
+_GLUECODIUM_C_EXPORT _baseRef smoke_StructsWithMethods_Vector_create_optional_handle(double x, double y);
+_GLUECODIUM_C_EXPORT _baseRef smoke_StructsWithMethods_Vector_unwrap_optional_handle(_baseRef handle);
+_GLUECODIUM_C_EXPORT void smoke_StructsWithMethods_Vector_release_optional_handle(_baseRef handle);
+_GLUECODIUM_C_EXPORT double smoke_StructsWithMethods_Vector_x_get(_baseRef handle);
+_GLUECODIUM_C_EXPORT double smoke_StructsWithMethods_Vector_y_get(_baseRef handle);
+typedef struct {
+    bool has_value;
+    union {
+        smoke_ValidationUtils_ValidationErrorCode error_value;
+        _baseRef returned_value;
+    };
+} smoke_StructsWithMethods_Vector_create_Vector_result;
+_GLUECODIUM_C_EXPORT double smoke_StructsWithMethods_Vector_distanceTo(_baseRef _instance, _baseRef other);
+_GLUECODIUM_C_EXPORT _baseRef smoke_StructsWithMethods_Vector_add(_baseRef _instance, _baseRef other);
+_GLUECODIUM_C_EXPORT bool smoke_StructsWithMethods_Vector_validate(double x, double y);
+_GLUECODIUM_C_EXPORT _baseRef smoke_StructsWithMethods_Vector_create_Double_Double(double x, double y);
+_GLUECODIUM_C_EXPORT smoke_StructsWithMethods_Vector_create_Vector_result smoke_StructsWithMethods_Vector_create_Vector(_baseRef other);
+#ifdef __cplusplus
+}
+#endif
