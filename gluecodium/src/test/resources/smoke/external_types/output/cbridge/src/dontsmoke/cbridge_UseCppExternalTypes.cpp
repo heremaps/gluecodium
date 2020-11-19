0f29238a91ffee56eab418f52f36adc30025193f
Daniel Kamkha <daniel.kamkha@here.com>
2020-11-19 14:27:55 +0100

    Add more CBridge tests for external types usage (#613)
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

---
+++ gluecodium/src/test/resources/smoke/external_types/output/cbridge/src/dontsmoke/cbridge_UseCppExternalTypes.cpp
@@ -0,0 +1,45 @@
+//
+//
+#include "cbridge/include/dontsmoke/cbridge_UseCppExternalTypes.h"
+#include "cbridge_internal/include/BaseHandleImpl.h"
+#include "cbridge_internal/include/TypeInitRepository.h"
+#include "cbridge_internal/include/WrapperCache.h"
+#include "dontsmoke/UseCppExternalTypes.h"
+#include "foo/Bar.h"
+#include "foo/Bazz.h"
+#include "gluecodium/Optional.h"
+#include "gluecodium/TypeRepository.h"
+#include "include/ExternalTypes.h"
+#include "non/Sense.h"
+#include <memory>
+#include <new>
+void dontsmoke_UseCppExternalTypes_release_handle(_baseRef handle) {
+    delete get_pointer<::std::shared_ptr< ::dontsmoke::UseCppExternalTypes >>(handle);
+}
+_baseRef dontsmoke_UseCppExternalTypes_copy_handle(_baseRef handle) {
+    return handle
+        ? reinterpret_cast<_baseRef>(checked_pointer_copy(*get_pointer<::std::shared_ptr< ::dontsmoke::UseCppExternalTypes >>(handle)))
+        : 0;
+}
+const void* dontsmoke_UseCppExternalTypes_get_swift_object_from_wrapper_cache(_baseRef handle) {
+    return handle
+        ? ::gluecodium::get_wrapper_cache().get_cached_wrapper(get_pointer<::std::shared_ptr< ::dontsmoke::UseCppExternalTypes >>(handle)->get())
+        : nullptr;
+}
+void dontsmoke_UseCppExternalTypes_cache_swift_object_wrapper(_baseRef handle, const void* swift_pointer) {
+    if (!handle) return;
+    ::gluecodium::get_wrapper_cache().cache_wrapper(get_pointer<::std::shared_ptr< ::dontsmoke::UseCppExternalTypes >>(handle)->get(), swift_pointer);
+}
+void dontsmoke_UseCppExternalTypes_remove_swift_object_from_wrapper_cache(_baseRef handle) {
+    if (!::gluecodium::WrapperCache::is_alive) return;
+    ::gluecodium::get_wrapper_cache().remove_cached_wrapper(get_pointer<::std::shared_ptr< ::dontsmoke::UseCppExternalTypes >>(handle)->get());
+}
+void dontsmoke_UseCppExternalTypes_useStruct(_baseRef input) {
+    return ::dontsmoke::UseCppExternalTypes::use_struct(Conversion<::fire::SomeVeryExternalStruct>::toCpp(input));
+}
+void dontsmoke_UseCppExternalTypes_useEnum(smoke_Enums_ExternalEnum input) {
+    return ::dontsmoke::UseCppExternalTypes::use_enum(static_cast<::smoke::Enums::External_Enum>(input));
+}
+void dontsmoke_UseCppExternalTypes_useClass(_baseRef input) {
+    return ::dontsmoke::UseCppExternalTypes::use_class(Conversion<::std::shared_ptr< ::smoke::ClassWithOverloads >>::toCpp(input));
+}
---
+++ gluecodium/src/test/resources/smoke/external_types/output/cbridge/src/smoke/cbridge_ExternalTypeInTypesCollection.cpp
@@ -0,0 +1,39 @@
+//
+//
+#include "cbridge/include/smoke/cbridge_ExternalTypeInTypesCollection.h"
+#include "cbridge_internal/include/BaseHandleImpl.h"
+#include "gluecodium/Optional.h"
+#include "include/ExternalTypeInTypesCollection.h"
+#include <memory>
+#include <new>
+_baseRef
+smoke_ExternalTypeInTypesCollection_IntStruct_create_handle( int32_t intField )
+{
+    ::external::IntStruct* _struct = new ( ::std::nothrow ) ::external::IntStruct();
+    _struct->int_field = intField;
+    return reinterpret_cast<_baseRef>( _struct );
+}
+void
+smoke_ExternalTypeInTypesCollection_IntStruct_release_handle( _baseRef handle )
+{
+    delete get_pointer<::external::IntStruct>( handle );
+}
+_baseRef
+smoke_ExternalTypeInTypesCollection_IntStruct_create_optional_handle(int32_t intField)
+{
+    auto _struct = new ( ::std::nothrow ) ::gluecodium::optional<::external::IntStruct>( ::external::IntStruct( ) );
+    (*_struct)->int_field = intField;
+    return reinterpret_cast<_baseRef>( _struct );
+}
+_baseRef
+smoke_ExternalTypeInTypesCollection_IntStruct_unwrap_optional_handle( _baseRef handle )
+{
+    return reinterpret_cast<_baseRef>( &**reinterpret_cast<::gluecodium::optional<::external::IntStruct>*>( handle ) );
+}
+void smoke_ExternalTypeInTypesCollection_IntStruct_release_optional_handle(_baseRef handle) {
+    delete reinterpret_cast<::gluecodium::optional<::external::IntStruct>*>( handle );
+}
+int32_t smoke_ExternalTypeInTypesCollection_IntStruct_intField_get(_baseRef handle) {
+    auto struct_pointer = get_pointer<const ::external::IntStruct>(handle);
+    return struct_pointer->int_field;
+}
