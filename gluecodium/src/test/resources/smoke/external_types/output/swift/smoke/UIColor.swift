afa68486658d8567072d4d9406ccbdc22a1afcd4
Daniel Kamkha <daniel.kamkha@here.com>
2020-12-07 11:00:31 +0100

    Update Swift "external type" smoke test (#640)
    Updated Swift "external type" smoke test with a more realistic scenario (platform-renamed type, non-default framework).
    
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

---
+++ gluecodium/src/test/resources/smoke/external_types/output/swift/smoke/UIColor.swift
@@ -0,0 +1,69 @@
+//
+//
+import Foundation
+import UIKit
+internal struct UIColor_internal {
+    public var red: Float
+    public var green: Float
+    public var blue: Float
+    public var alpha: Float
+    internal init(red: Float, green: Float, blue: Float, alpha: Float) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
+    internal init(cHandle: _baseRef) {
+        red = moveFromCType(smoke_UIColor_red_get(cHandle))
+        green = moveFromCType(smoke_UIColor_green_get(cHandle))
+        blue = moveFromCType(smoke_UIColor_blue_get(cHandle))
+        alpha = moveFromCType(smoke_UIColor_alpha_get(cHandle))
+    }
+}
+internal func copyFromCType(_ handle: _baseRef) -> UIColor {
+    return ColorConverter.convertFromInternal(UIColor_internal(cHandle: handle))
+}
+internal func moveFromCType(_ handle: _baseRef) -> UIColor {
+    defer {
+        smoke_UIColor_release_handle(handle)
+    }
+    return copyFromCType(handle)
+}
+internal func copyToCType(_ swiftType_ext: UIColor) -> RefHolder {
+    let swiftType = ColorConverter.convertToInternal(swiftType_ext)
+    let c_red = moveToCType(swiftType.red)
+    let c_green = moveToCType(swiftType.green)
+    let c_blue = moveToCType(swiftType.blue)
+    let c_alpha = moveToCType(swiftType.alpha)
+    return RefHolder(smoke_UIColor_create_handle(c_red.ref, c_green.ref, c_blue.ref, c_alpha.ref))
+}
+internal func moveToCType(_ swiftType: UIColor) -> RefHolder {
+    return RefHolder(ref: copyToCType(swiftType).ref, release: smoke_UIColor_release_handle)
+}
+internal func copyFromCType(_ handle: _baseRef) -> UIColor? {
+    guard handle != 0 else {
+        return nil
+    }
+    let unwrappedHandle = smoke_UIColor_unwrap_optional_handle(handle)
+    return ColorConverter.convertFromInternal(UIColor_internal(cHandle: unwrappedHandle))
+}
+internal func moveFromCType(_ handle: _baseRef) -> UIColor? {
+    defer {
+        smoke_UIColor_release_optional_handle(handle)
+    }
+    return copyFromCType(handle)
+}
+internal func copyToCType(_ swiftType_ext: UIColor?) -> RefHolder {
+    guard let swiftType_ext = swiftType_ext else {
+        return RefHolder(0)
+    }
+    let swiftType = ColorConverter.convertToInternal(swiftType_ext)
+    let c_red = moveToCType(swiftType.red)
+    let c_green = moveToCType(swiftType.green)
+    let c_blue = moveToCType(swiftType.blue)
+    let c_alpha = moveToCType(swiftType.alpha)
+    return RefHolder(smoke_UIColor_create_optional_handle(c_red.ref, c_green.ref, c_blue.ref, c_alpha.ref))
+}
+internal func moveToCType(_ swiftType: UIColor?) -> RefHolder {
+    return RefHolder(ref: copyToCType(swiftType).ref, release: smoke_UIColor_release_optional_handle)
+}
