ac4c1947d2331f2a5271383a6652a9fcfd10e18b
Daniel Kamkha <daniel.kamkha@here.com>
2021-08-26 08:22:51 +0200

    Fix extraneous exports/imports in Dart
    Fixed extraneous exports for type collections in Dart.
    
    Fixed extraneous imports for inherited functions/properties in classes in Dart.
    
    Resolves: #1061
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

--- gluecodium/src/test/resources/smoke/visibility/output/dart/lib/smoke.dart
+++ gluecodium/src/test/resources/smoke/visibility/output/dart/lib/smoke.dart
@@ -1,4 +1,4 @@
+export 'src/smoke/dont_export_in_dart.dart' show DoExportInDart;
 export 'src/smoke/public_class.dart' show PublicClass, PublicClass_PublicStruct, PublicClass_PublicStructWithInternalDefaults;
 export 'src/smoke/public_interface.dart' show PublicInterface;
 export 'src/smoke/public_struct_with_non_default_internal_field.dart' show PublicStructWithNonDefaultInternalField;
-export 'src/smoke/public_type_collection.dart' show PublicTypeCollection;
