5a758cabb777d3701c9ac21ccf2080c00befc906
Daniel Kamkha <daniel.kamkha@here.com>
2021-10-27 13:38:33 +0200

    Fix function/property skipping in interface in Swift (#1137)
    Updated FunctionTable.mustache template to properly skip functions and
    properties if they are marked with `@Skip(Swift)` or some other applicable skip
    attribute. This fixes a compilation issue when such skipped element refers to an
    also-skipped enum type.
    
    Added smoke and functional tests.
    
    Resolves: #1135
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

--- gluecodium/src/test/resources/smoke/skip/output/cbridge/src/smoke/cbridge_SkipProxy.cpp
+++ gluecodium/src/test/resources/smoke/skip/output/cbridge/src/smoke/cbridge_SkipProxy.cpp
@@ -111,6 +111,11 @@ public:
     }
     void set_skipped_everywhere(const ::smoke::SkippedEverywhere& value) override {
     }
+    ::smoke::SkippedEverywhereEnum get_skipped_everywhere_too() const override {
+        return {};
+    }
+    void set_skipped_everywhere_too(const ::smoke::SkippedEverywhereEnum value) override {
+    }
 private:
     smoke_SkipProxy_FunctionTable mFunctions;
 };
