5b5f39a8eb0e5c2ebbfd04f1958ae4e1f782d67e
Daniel Kamkha <daniel.kamkha@here.com>
2021-12-16 10:34:22 +0100

    Fix "internal" inheritance issue in Java
    Updated Java templates to force methods inherited by a class from an interface
    to be always public. This fixes a compilation issue when both the class and its
    interface parent are `internal`. Java treats all interface methods as "public",
    even if the interface itself is not, so overriding interface methods with
    lower-visibility class methods is a compilation error.
    
    Added smoke and functional tests.
    
    Resolves: #1222
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

---
+++ gluecodium/src/test/resources/smoke/visibility/output/android/com/example/smoke/InternalClassInherits.java
@@ -0,0 +1,24 @@
+/*
+ *
+ */
+package com.example.smoke;
+import android.support.annotation.NonNull;
+import com.example.NativeBase;
+class InternalClassInherits extends NativeBase implements InternalInterfaceParent {
+    protected InternalClassInherits(final long nativeHandle, final Object dummy) {
+        super(nativeHandle, new Disposer() {
+            @Override
+            public void disposeNative(long handle) {
+                disposeNativeHandle(handle);
+            }
+        });
+    }
+    private static native void disposeNativeHandle(long nativeHandle);
+    @Override
+    public native void fooBar();
+    @NonNull
+    @Override
+    public native String getProp();
+    @Override
+    public native void setProp(@NonNull final String value);
+}
