86da2495d1fdf3fd3d846e42e69530b7d45aa20a
Daniel Kamkha <daniel.kamkha@here.com>
2022-08-24 15:14:44 +0200

    Fix `@Java(Internal)` on properties
    Signed-off-by: Daniel Kamkha <daniel.kamkha@here.com>

---
+++ gluecodium/src/test/resources/smoke/visibility_platform/output/android/com/example/smoke/JavaInternalProperty.java
@@ -0,0 +1,24 @@
+/*
+ *
+ */
+package com.example.smoke;
+import android.support.annotation.Nullable;
+import com.example.NativeBase;
+public final class JavaInternalProperty extends NativeBase {
+    /**
+     * For internal use only.
+     * @hidden
+     */
+    protected JavaInternalProperty(final long nativeHandle, final Object dummy) {
+        super(nativeHandle, new Disposer() {
+            @Override
+            public void disposeNative(long handle) {
+                disposeNativeHandle(handle);
+            }
+        });
+    }
+    private static native void disposeNativeHandle(long nativeHandle);
+    @Nullable
+    native String getAppContext();
+    native void setAppContext(@Nullable final String value);
+}
