2513cddb08202a1a9ef502d55abdf4087ec46984
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2024-11-05 09:56:31 +0100

    Extend smoke tests to show invalid behavior of attributes converter
    In the case of internal attribute platforms can be specified
    inside the annotation. However, when quotation marks are not
    present for each listed platform, then the behavior of converter
    is invalid.
    
    This change adds omitted files for smoke tests.
    Please note, that the visibility for mentioned types is as follows:
     - JavaInternalClassRev -- shall be internal only for Java, but
                               it is internal also in Swift and Dart.
     - SwiftInternalClassRev -- shall be internal only for Swift, but
                                it is internal also in Java and Dart.
     - DartInternalClassRev -- shall be internal only for Dart, but
                               it is internal also in Swift and Java.
    
    In next patches the logic will be fixed. The mentioned files will
    be used to show that the behavior has been adjusted.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/visibility_platform/output/android/com/example/smoke/SwiftInternalClassRev.java
@@ -0,0 +1,33 @@
+/*
+
+ *
+ */
+
+package com.example.smoke;
+
+import com.example.NativeBase;
+
+final class SwiftInternalClassRev extends NativeBase {
+
+    /**
+     * For internal use only.
+     * @hidden
+     * @param nativeHandle The SDK nativeHandle instance.
+     * @param dummy The SDK dummy instance.
+     */
+    protected SwiftInternalClassRev(final long nativeHandle, final Object dummy) {
+        super(nativeHandle, new Disposer() {
+            @Override
+            public void disposeNative(long handle) {
+                disposeNativeHandle(handle);
+            }
+        });
+    }
+
+    private static native void disposeNativeHandle(long nativeHandle);
+
+
+
+
+}
+
---
+++ gluecodium/src/test/resources/smoke/visibility_platform/output/swift/smoke/JavaInternalClassRev.swift
@@ -0,0 +1,106 @@
+//
+
+//
+
+import Foundation
+
+internal class JavaInternalClassRev {
+
+
+    let c_instance : _baseRef
+
+    init(cJavaInternalClassRev: _baseRef) {
+        guard cJavaInternalClassRev != 0 else {
+            fatalError("Nullptr value is not supported for initializers")
+        }
+        c_instance = cJavaInternalClassRev
+    }
+
+    deinit {
+        smoke_JavaInternalClassRev_remove_swift_object_from_wrapper_cache(c_instance)
+        smoke_JavaInternalClassRev_release_handle(c_instance)
+    }
+
+
+}
+
+
+
+internal func getRef(_ ref: JavaInternalClassRev?, owning: Bool = true) -> RefHolder {
+    guard let c_handle = ref?.c_instance else {
+        return RefHolder(0)
+    }
+    let handle_copy = smoke_JavaInternalClassRev_copy_handle(c_handle)
+    return owning
+        ? RefHolder(ref: handle_copy, release: smoke_JavaInternalClassRev_release_handle)
+        : RefHolder(handle_copy)
+}
+
+extension JavaInternalClassRev: NativeBase {
+    /// :nodoc:
+    var c_handle: _baseRef { return c_instance }
+}
+extension JavaInternalClassRev: Hashable {
+    /// :nodoc:
+    public static func == (lhs: JavaInternalClassRev, rhs: JavaInternalClassRev) -> Bool {
+        return lhs.c_handle == rhs.c_handle
+    }
+
+    /// :nodoc:
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(c_handle)
+    }
+}
+
+internal func JavaInternalClassRev_copyFromCType(_ handle: _baseRef) -> JavaInternalClassRev {
+    if let swift_pointer = smoke_JavaInternalClassRev_get_swift_object_from_wrapper_cache(handle),
+        let re_constructed = Unmanaged<AnyObject>.fromOpaque(swift_pointer).takeUnretainedValue() as? JavaInternalClassRev {
+        return re_constructed
+    }
+    let result = JavaInternalClassRev(cJavaInternalClassRev: smoke_JavaInternalClassRev_copy_handle(handle))
+    smoke_JavaInternalClassRev_cache_swift_object_wrapper(handle, Unmanaged<AnyObject>.passUnretained(result).toOpaque())
+    return result
+}
+
+internal func JavaInternalClassRev_moveFromCType(_ handle: _baseRef) -> JavaInternalClassRev {
+    if let swift_pointer = smoke_JavaInternalClassRev_get_swift_object_from_wrapper_cache(handle),
+        let re_constructed = Unmanaged<AnyObject>.fromOpaque(swift_pointer).takeUnretainedValue() as? JavaInternalClassRev {
+        smoke_JavaInternalClassRev_release_handle(handle)
+        return re_constructed
+    }
+    let result = JavaInternalClassRev(cJavaInternalClassRev: handle)
+    smoke_JavaInternalClassRev_cache_swift_object_wrapper(handle, Unmanaged<AnyObject>.passUnretained(result).toOpaque())
+    return result
+}
+
+internal func JavaInternalClassRev_copyFromCType(_ handle: _baseRef) -> JavaInternalClassRev? {
+    guard handle != 0 else {
+        return nil
+    }
+    return JavaInternalClassRev_moveFromCType(handle) as JavaInternalClassRev
+}
+internal func JavaInternalClassRev_moveFromCType(_ handle: _baseRef) -> JavaInternalClassRev? {
+    guard handle != 0 else {
+        return nil
+    }
+    return JavaInternalClassRev_moveFromCType(handle) as JavaInternalClassRev
+}
+
+internal func copyToCType(_ swiftClass: JavaInternalClassRev) -> RefHolder {
+    return getRef(swiftClass, owning: false)
+}
+
+internal func moveToCType(_ swiftClass: JavaInternalClassRev) -> RefHolder {
+    return getRef(swiftClass, owning: true)
+}
+
+internal func copyToCType(_ swiftClass: JavaInternalClassRev?) -> RefHolder {
+    return getRef(swiftClass, owning: false)
+}
+
+internal func moveToCType(_ swiftClass: JavaInternalClassRev?) -> RefHolder {
+    return getRef(swiftClass, owning: true)
+}
+
+
+
