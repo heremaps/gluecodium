804a156d839d67d50baed07fe12913cb0cdd4d40
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2024-11-27 09:46:03 +0100

    Warn when documentation of accessors parameters is missing
    This change extends LimePropertiesValidator class to
    warn when parameters or return types used by accessors
    of properties are missing.
    
    The logic works as follows:
     - all properties need to be documented
     - return value of getter needs to be documented
     - parameter of setter needs to be documented
     - the validation is not applied for internal properties
       or properties nested in internal types (or types nested
       in internal types)
    
    The user may enable treating the warning as error via
    werror flag.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/main/java/com/here/gluecodium/validator/LimePropertiesValidator.kt
+++ gluecodium/src/main/java/com/here/gluecodium/validator/LimePropertiesValidator.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2020 HERE Europe B.V.
+ * Copyright (C) 2016-2024 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,28 +20,39 @@
 package com.here.gluecodium.validator
 
 import com.here.gluecodium.common.LimeLogger
+import com.here.gluecodium.generator.common.GeneratorOptions
 import com.here.gluecodium.model.lime.LimeAttributeType.CACHED
 import com.here.gluecodium.model.lime.LimeAttributeType.CPP
 import com.here.gluecodium.model.lime.LimeAttributeValueType.REF
 import com.here.gluecodium.model.lime.LimeContainerWithInheritance
+import com.here.gluecodium.model.lime.LimeElement
 import com.here.gluecodium.model.lime.LimeInterface
 import com.here.gluecodium.model.lime.LimeModel
+import com.here.gluecodium.model.lime.LimeNamedElement
 import com.here.gluecodium.model.lime.LimeProperty
+import com.here.gluecodium.validator.LimeValidatorUtils.needsDocumentationComment
 
-internal class LimePropertiesValidator(private val logger: LimeLogger) {
+internal class LimePropertiesValidator(private val logger: LimeLogger, generatorOptions: GeneratorOptions = GeneratorOptions()) {
+    private val werrorPropertiesDocs = generatorOptions.werror.contains(GeneratorOptions.WARNING_LIME_PROPERTIES_DOCS)
+    private val maybeError: LimeLogger.(LimeNamedElement, String) -> Unit =
+        if (werrorPropertiesDocs) LimeLogger::error else LimeLogger::warning
+
     fun validate(limeModel: LimeModel): Boolean {
         val validationResults =
             limeModel.referenceMap.values
                 .filterIsInstance<LimeContainerWithInheritance>()
-                .map { validateProperties(it) }
+                .map { validateProperties(it, limeModel.referenceMap) }
 
         return !validationResults.contains(false)
     }
 
-    private fun validateProperties(limeContainer: LimeContainerWithInheritance): Boolean {
+    private fun validateProperties(
+        limeContainer: LimeContainerWithInheritance,
+        referenceMap: Map<String, LimeElement>,
+    ): Boolean {
         val allPropertyNames = (limeContainer.properties + limeContainer.inheritedProperties).map { it.name }
         return !limeContainer.properties
-            .map { validateProperty(it, allPropertyNames, limeContainer) }
+            .map { validateProperty(it, allPropertyNames, limeContainer, referenceMap) }
             .contains(false)
     }
 
@@ -49,6 +60,7 @@ internal class LimePropertiesValidator(private val logger: LimeLogger) {
         limeProperty: LimeProperty,
         allPropertyNames: List<String>,
         parentElement: LimeContainerWithInheritance,
+        referenceMap: Map<String, LimeElement>,
     ): Boolean {
         var result = true
         if (allPropertyNames.filter { it == limeProperty.name }.size > 1) {
@@ -63,6 +75,36 @@ internal class LimePropertiesValidator(private val logger: LimeLogger) {
             logger.error(limeProperty, "property in an interface cannot be marked with @Cpp(Ref)")
             result = false
         }
+        if (needsDocumentationComment(limeProperty, referenceMap) && !validateComments(limeProperty)) {
+            if (werrorPropertiesDocs) {
+                result = false
+            }
+        }
+        return result
+    }
+
+    private fun validateComments(limeProperty: LimeProperty): Boolean {
+        val propertiesDocsErrorMessage = "property must be documented; check 'docs/lime_markdown.md'"
+        var result = true
+
+        if (limeProperty.comment.isEmpty()) {
+            logger.maybeError(limeProperty, "main comment is empty; $propertiesDocsErrorMessage")
+            result = false
+        }
+
+        if (limeProperty.getter.returnType.comment.isEmpty()) {
+            logger.maybeError(limeProperty, "return of getter is not documented; $propertiesDocsErrorMessage")
+            result = false
+        }
+
+        if (limeProperty.setter != null) {
+            val isSetterCommentEmpty = limeProperty.setter?.parameters?.firstOrNull()?.comment?.isEmpty()
+            if (isSetterCommentEmpty == null || isSetterCommentEmpty) {
+                logger.maybeError(limeProperty, "parameter of setter is not documented; $propertiesDocsErrorMessage")
+                result = false
+            }
+        }
+
         return result
     }
 }
