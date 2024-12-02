cb2e6fde41087d0bbd58740cd68fc234c0b63c35
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2024-12-02 09:07:58 +0100

    Dart: confirm invalid behavior for BLOB type
    The constructors of 'Uint8List' type are not const.
    This type is used when BLOB is specified in LIME
    file.
    
    Therefore, the usage of BLOB type for a field of
    struct that is marked as 'PositionalDefaults' and
    specifying a default value of this field leads to
    compilation error.
    
    This change is used to document the invalid behavior.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/test/resources/smoke/defaults/output/dart/lib/src/smoke/pos_default_struct_with_custom_structs_fields.dart
+++ gluecodium/src/test/resources/smoke/defaults/output/dart/lib/src/smoke/pos_default_struct_with_custom_structs_fields.dart
@@ -1,7 +1,9 @@
 
 
 import 'dart:ffi';
+import 'dart:typed_data';
 import 'package:library/src/_library_context.dart' as __lib;
+import 'package:library/src/builtin_types__conversion.dart';
 import 'package:library/src/generic_types__conversion.dart';
 import 'package:library/src/smoke/immutable_struct_with_defaults.dart';
 import 'package:library/src/smoke/pos_default_struct_with_field_using_immutable_struct.dart';
@@ -37,16 +39,22 @@ class PosDefaultStructWithCustomStructsFields {
 
   StructWithAllDefaults? nonConstCtorField4;
 
-  PosDefaultStructWithCustomStructsFields([ImmutableStructWithDefaults constCtorField0 = const ImmutableStructWithDefaults.withDefaults(), ImmutableStructWithDefaults? constCtorField1 = const ImmutableStructWithDefaults.withDefaults(), List<String> constCtorField2 = const ["abc", "def", "ghi"], Map<String, String>? constCtorField3 = null, int constCtorField4 = 77, double constCtorField5 = 77.77, ImmutableStructWithDefaults? constCtorField6 = const ImmutableStructWithDefaults.withDefaults(), ImmutableStructWithDefaults? constCtorField7 = null, StructWithAllDefaults? nonConstCtorField0 = null, PosDefaultStructWithFieldUsingImmutableStruct? nonConstCtorField1 = null, SomeMutableCustomStructWithDefaults? nonConstCtorField2 = null, StructWithNullableCollectionDefaults? nonConstCtorField3 = null, StructWithAllDefaults? nonConstCtorField4 = null])
-    : constCtorField0 = constCtorField0, constCtorField1 = constCtorField1, constCtorField2 = constCtorField2, constCtorField3 = constCtorField3, constCtorField4 = constCtorField4, constCtorField5 = constCtorField5, constCtorField6 = constCtorField6, constCtorField7 = constCtorField7, nonConstCtorField0 = nonConstCtorField0 ?? StructWithAllDefaults(), nonConstCtorField1 = nonConstCtorField1 ?? PosDefaultStructWithFieldUsingImmutableStruct(), nonConstCtorField2 = nonConstCtorField2 ?? SomeMutableCustomStructWithDefaults(), nonConstCtorField3 = nonConstCtorField3 ?? StructWithNullableCollectionDefaults(), nonConstCtorField4 = nonConstCtorField4 ?? null;
+  Uint8List nonConstCtorField5;
+
+  Uint8List nonConstCtorField6;
+
+  Uint8List? nonConstCtorField7;
+
+  PosDefaultStructWithCustomStructsFields([ImmutableStructWithDefaults constCtorField0 = const ImmutableStructWithDefaults.withDefaults(), ImmutableStructWithDefaults? constCtorField1 = const ImmutableStructWithDefaults.withDefaults(), List<String> constCtorField2 = const ["abc", "def", "ghi"], Map<String, String>? constCtorField3 = null, int constCtorField4 = 77, double constCtorField5 = 77.77, ImmutableStructWithDefaults? constCtorField6 = const ImmutableStructWithDefaults.withDefaults(), ImmutableStructWithDefaults? constCtorField7 = null, StructWithAllDefaults? nonConstCtorField0 = null, PosDefaultStructWithFieldUsingImmutableStruct? nonConstCtorField1 = null, SomeMutableCustomStructWithDefaults? nonConstCtorField2 = null, StructWithNullableCollectionDefaults? nonConstCtorField3 = null, StructWithAllDefaults? nonConstCtorField4 = null, Uint8List nonConstCtorField5 = Uint8List.fromList([]), Uint8List nonConstCtorField6 = Uint8List.fromList([222, 173, 190, 239]), Uint8List? nonConstCtorField7 = null])
+    : constCtorField0 = constCtorField0, constCtorField1 = constCtorField1, constCtorField2 = constCtorField2, constCtorField3 = constCtorField3, constCtorField4 = constCtorField4, constCtorField5 = constCtorField5, constCtorField6 = constCtorField6, constCtorField7 = constCtorField7, nonConstCtorField0 = nonConstCtorField0 ?? StructWithAllDefaults(), nonConstCtorField1 = nonConstCtorField1 ?? PosDefaultStructWithFieldUsingImmutableStruct(), nonConstCtorField2 = nonConstCtorField2 ?? SomeMutableCustomStructWithDefaults(), nonConstCtorField3 = nonConstCtorField3 ?? StructWithNullableCollectionDefaults(), nonConstCtorField4 = nonConstCtorField4 ?? null, nonConstCtorField5 = nonConstCtorField5, nonConstCtorField6 = nonConstCtorField6, nonConstCtorField7 = nonConstCtorField7;
 }
 
 
 // PosDefaultStructWithCustomStructsFields "private" section, not exported.
 
 final _smokePosdefaultstructwithcustomstructsfieldsCreateHandle = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
-    Pointer<Void> Function(Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Int32, Double, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>),
-    Pointer<Void> Function(Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, int, double, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>)
+    Pointer<Void> Function(Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Int32, Double, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>),
+    Pointer<Void> Function(Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, int, double, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>, Pointer<Void>)
   >('library_smoke_PosDefaultStructWithCustomStructsFields_create_handle'));
 final _smokePosdefaultstructwithcustomstructsfieldsReleaseHandle = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
     Void Function(Pointer<Void>),
@@ -104,6 +112,18 @@ final _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField4 = 
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
   >('library_smoke_PosDefaultStructWithCustomStructsFields_get_field_nonConstCtorField4'));
+final _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField5 = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
+    Pointer<Void> Function(Pointer<Void>),
+    Pointer<Void> Function(Pointer<Void>)
+  >('library_smoke_PosDefaultStructWithCustomStructsFields_get_field_nonConstCtorField5'));
+final _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField6 = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
+    Pointer<Void> Function(Pointer<Void>),
+    Pointer<Void> Function(Pointer<Void>)
+  >('library_smoke_PosDefaultStructWithCustomStructsFields_get_field_nonConstCtorField6'));
+final _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField7 = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
+    Pointer<Void> Function(Pointer<Void>),
+    Pointer<Void> Function(Pointer<Void>)
+  >('library_smoke_PosDefaultStructWithCustomStructsFields_get_field_nonConstCtorField7'));
 
 
 
@@ -121,7 +141,10 @@ Pointer<Void> smokePosdefaultstructwithcustomstructsfieldsToFfi(PosDefaultStruct
   final _nonConstCtorField2Handle = smokeSomemutablecustomstructwithdefaultsToFfi(value.nonConstCtorField2);
   final _nonConstCtorField3Handle = smokeStructwithnullablecollectiondefaultsToFfi(value.nonConstCtorField3);
   final _nonConstCtorField4Handle = smokeStructwithalldefaultsToFfiNullable(value.nonConstCtorField4);
-  final _result = _smokePosdefaultstructwithcustomstructsfieldsCreateHandle(_constCtorField0Handle, _constCtorField1Handle, _constCtorField2Handle, _constCtorField3Handle, _constCtorField4Handle, _constCtorField5Handle, _constCtorField6Handle, _constCtorField7Handle, _nonConstCtorField0Handle, _nonConstCtorField1Handle, _nonConstCtorField2Handle, _nonConstCtorField3Handle, _nonConstCtorField4Handle);
+  final _nonConstCtorField5Handle = blobToFfi(value.nonConstCtorField5);
+  final _nonConstCtorField6Handle = blobToFfi(value.nonConstCtorField6);
+  final _nonConstCtorField7Handle = blobToFfiNullable(value.nonConstCtorField7);
+  final _result = _smokePosdefaultstructwithcustomstructsfieldsCreateHandle(_constCtorField0Handle, _constCtorField1Handle, _constCtorField2Handle, _constCtorField3Handle, _constCtorField4Handle, _constCtorField5Handle, _constCtorField6Handle, _constCtorField7Handle, _nonConstCtorField0Handle, _nonConstCtorField1Handle, _nonConstCtorField2Handle, _nonConstCtorField3Handle, _nonConstCtorField4Handle, _nonConstCtorField5Handle, _nonConstCtorField6Handle, _nonConstCtorField7Handle);
   smokeImmutablestructwithdefaultsReleaseFfiHandle(_constCtorField0Handle);
   smokeImmutablestructwithdefaultsReleaseFfiHandleNullable(_constCtorField1Handle);
   foobarListofStringReleaseFfiHandle(_constCtorField2Handle);
@@ -135,6 +158,9 @@ Pointer<Void> smokePosdefaultstructwithcustomstructsfieldsToFfi(PosDefaultStruct
   smokeSomemutablecustomstructwithdefaultsReleaseFfiHandle(_nonConstCtorField2Handle);
   smokeStructwithnullablecollectiondefaultsReleaseFfiHandle(_nonConstCtorField3Handle);
   smokeStructwithalldefaultsReleaseFfiHandleNullable(_nonConstCtorField4Handle);
+  blobReleaseFfiHandle(_nonConstCtorField5Handle);
+  blobReleaseFfiHandle(_nonConstCtorField6Handle);
+  blobReleaseFfiHandleNullable(_nonConstCtorField7Handle);
   return _result;
 }
 
@@ -152,6 +178,9 @@ PosDefaultStructWithCustomStructsFields smokePosdefaultstructwithcustomstructsfi
   final _nonConstCtorField2Handle = _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField2(handle);
   final _nonConstCtorField3Handle = _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField3(handle);
   final _nonConstCtorField4Handle = _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField4(handle);
+  final _nonConstCtorField5Handle = _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField5(handle);
+  final _nonConstCtorField6Handle = _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField6(handle);
+  final _nonConstCtorField7Handle = _smokePosdefaultstructwithcustomstructsfieldsGetFieldnonConstCtorField7(handle);
   try {
     return PosDefaultStructWithCustomStructsFields(
       smokeImmutablestructwithdefaultsFromFfi(_constCtorField0Handle), 
@@ -166,7 +195,10 @@ PosDefaultStructWithCustomStructsFields smokePosdefaultstructwithcustomstructsfi
       smokePosdefaultstructwithfieldusingimmutablestructFromFfi(_nonConstCtorField1Handle), 
       smokeSomemutablecustomstructwithdefaultsFromFfi(_nonConstCtorField2Handle), 
       smokeStructwithnullablecollectiondefaultsFromFfi(_nonConstCtorField3Handle), 
-      smokeStructwithalldefaultsFromFfiNullable(_nonConstCtorField4Handle)
+      smokeStructwithalldefaultsFromFfiNullable(_nonConstCtorField4Handle), 
+      blobFromFfi(_nonConstCtorField5Handle), 
+      blobFromFfi(_nonConstCtorField6Handle), 
+      blobFromFfiNullable(_nonConstCtorField7Handle)
     );
   } finally {
     smokeImmutablestructwithdefaultsReleaseFfiHandle(_constCtorField0Handle);
@@ -182,6 +214,9 @@ PosDefaultStructWithCustomStructsFields smokePosdefaultstructwithcustomstructsfi
     smokeSomemutablecustomstructwithdefaultsReleaseFfiHandle(_nonConstCtorField2Handle);
     smokeStructwithnullablecollectiondefaultsReleaseFfiHandle(_nonConstCtorField3Handle);
     smokeStructwithalldefaultsReleaseFfiHandleNullable(_nonConstCtorField4Handle);
+    blobReleaseFfiHandle(_nonConstCtorField5Handle);
+    blobReleaseFfiHandle(_nonConstCtorField6Handle);
+    blobReleaseFfiHandleNullable(_nonConstCtorField7Handle);
   }
 }
 
