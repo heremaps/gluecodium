054eac6a807a07dba2b4ac1a313ae234d9f465f7
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-02-12 12:58:40 +0100

    Kotlin: support static members in interfaces
    This change:
     - adds support for constants and static properties
     - adjusts old smoke tests and adds new ones
     - adds a new functional test to confirm that the new
       functionality works as expected
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/listeners/output/android-kotlin/com/example/smoke/InterfaceWithStaticImpl.kt
@@ -0,0 +1,34 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+import com.example.NativeBase
+
+class InterfaceWithStaticImpl : NativeBase, InterfaceWithStatic {
+    /*
+     * For internal use only.
+     * @hidden
+     * @param nativeHandle The handle to resources on C++ side.
+     * @param tag Tag used by callers to avoid overload resolution problems.
+     */
+    protected constructor(nativeHandle: Long, tag: Any?)
+        : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+    override external fun regularFunction() : String
+
+    override var regularProperty: String
+        external get
+        external set
+
+
+    companion object {
+        @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+        @JvmStatic external fun staticFunction() : String
+        @JvmStatic var staticProperty: String
+            external get
+            external set
+    }
+}
