5d2d11af89782f9187f2d7e637fef76dca33c741
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-02-12 08:37:35 +0100

    Adjust smoke tests
    This commit is used to showcase the new behavior.
    Certain types defined in 'Async.lime' were generated
    only for C++ and Dart. In such case the redundant
    function was added to C++ classes.
    
    Note: 'AsyncWithSkips' class, which is also defined
    in 'Async.lime' file does not contain 'Skip(Java, Swift)'
    and therefore its smoke tests were not modified by this
    commit.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/test/resources/smoke/async/output/cpp/include/smoke/AsyncStruct.h
+++ gluecodium/src/test/resources/smoke/async/output/cpp/include/smoke/AsyncStruct.h
@@ -1,27 +1,36 @@
 // -------------------------------------------------------------------------------------------------
 //
+
 //
 // -------------------------------------------------------------------------------------------------
+
 #pragma once
+
 #include "gluecodium/ExportGluecodiumCpp.h"
 #include "gluecodium/Return.h"
 #include <cstdint>
 #include <functional>
 #include <string>
+
 namespace smoke {
 struct _GLUECODIUM_CPP_EXPORT AsyncStruct {
     ::std::string string_field;
+
     AsyncStruct( );
     explicit AsyncStruct( ::std::string string_field );
+
     void async_void( std::function<void()> _result_callback, const bool input ) const;
-    void async_void( const bool input ) const;
+
     void async_void_throws( std::function<void()> _result_callback, std::function<void(::std::string)> _error_callback, const bool input ) const;
-    ::gluecodium::Return< void, ::std::string > async_void_throws( const bool input ) const;
+
     void async_int( std::function<void(int32_t)> _result_callback, const bool input ) const;
-    int32_t async_int( const bool input ) const;
+
     void async_int_throws( std::function<void(int32_t)> _result_callback, std::function<void(::std::string)> _error_callback, const bool input ) const;
-    ::gluecodium::Return< int32_t, ::std::string > async_int_throws( const bool input ) const;
+
     static void async_static( std::function<void()> _result_callback, const bool input );
-    static void async_static( const bool input );
+
+
 };
+
+
 }
