e3573662c96a5f5baf2dcec78ccd4621595f1e6c
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-02-19 10:01:45 +0100

    smoke tests: add tests of 'EnableIf' for Kotlin generator
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/skip/output/android-kotlin/com/example/smoke/EnableIfEnabled.kt
@@ -0,0 +1,37 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+import com.example.NativeBase
+
+class EnableIfEnabled : NativeBase {
+
+
+
+    /*
+     * For internal use only.
+     * @hidden
+     * @param nativeHandle The handle to resources on C++ side.
+     * @param tag Tag used by callers to avoid overload resolution problems.
+     */
+    protected constructor(nativeHandle: Long, tag: Any?)
+        : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+
+
+
+
+    companion object {
+        @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+        @JvmStatic external fun enableIfUnquoted() : Unit
+        @JvmStatic external fun enableIfUnquotedList() : Unit
+        @JvmStatic external fun enableIfQuoted() : Unit
+        @JvmStatic external fun enableIfQuotedList() : Unit
+        @JvmStatic external fun enableIfTagged() : Unit
+        @JvmStatic external fun enableIfTaggedList() : Unit
+        @JvmStatic external fun enableIfMixedList() : Unit
+    }
+}
