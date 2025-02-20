baf2196a9f0eca390208fe21a42267691b99c761
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-02-20 08:14:37 +0100

    Adjust smoke tests after rebase (2)
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/test/resources/smoke/platform_names/output/android-kotlin/com/example/smoke/dodoTypes.kt
+++ gluecodium/src/test/resources/smoke/platform_names/output/android-kotlin/com/example/smoke/dodoTypes.kt
@@ -7,25 +7,34 @@ package com.example.smoke
 
 import com.example.smoke.dodoTypes
 
-class dodoTypes() {
+class dodoTypes {
 
     enum class dodoEnum(private val value: Int) {
         DODO_ITEM(0);
     }
-    class dodoStruct(var DODO_FIELD: String) {
+    class dodoStruct {
+        var DODO_FIELD: String
+
+
+        constructor(DodoParameter: String) {
+            val _other = DodoCreate(DodoParameter)
+            this.DODO_FIELD = _other.DODO_FIELD
+        }
+
+
 
 
 
         companion object {
-            @JvmStatic external fun DodoCreate(DodoParameter: String) : Long
+            @JvmStatic external fun DodoCreate(DodoParameter: String) : dodoTypes.dodoStruct
         }
-
     }
+
+
 
 
 
 
 
 }
-
 
