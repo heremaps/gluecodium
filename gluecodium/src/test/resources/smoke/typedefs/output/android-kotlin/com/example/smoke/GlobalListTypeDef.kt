679f5d502daee1ddbd996a6db69264721d543191
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-02-24 13:09:53 +0100

    Kotlin: validate and fix usage of 'typealias' (#1663)
    The way how names were resolved for standalone global type alias
    usage in the case of Kotlin generator was invalid. It also caused problems
    when building the LIME input of functional tests for external types.
    
    This series introduces smoke tests and fixes the problem in name resolver.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/typedefs/output/android-kotlin/com/example/smoke/GlobalListTypeDef.kt
@@ -0,0 +1,9 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+
+typealias GlobalListTypeDef = MutableList<Float>
---
+++ gluecodium/src/test/resources/smoke/typedefs/output/android-kotlin/com/example/smoke/GlobalMapTypeDef.kt
@@ -0,0 +1,9 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+
+typealias GlobalMapTypeDef = MutableMap<Int, String>
