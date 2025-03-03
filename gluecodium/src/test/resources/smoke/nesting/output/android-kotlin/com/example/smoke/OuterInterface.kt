b01a81b854c5d009e2094a9445bc2eeaf8989217
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-03-03 11:49:00 +0100

    Kotlin: add smoke tests of 'nesting' (#1671)
    This change introduces smoke tests for 'nesting'
    test suite. The output files were compared with
    their Java counterparts to ensure the same level
    of support.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/nesting/output/android-kotlin/com/example/smoke/OuterInterface.kt
@@ -0,0 +1,64 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+import com.example.NativeBase
+
+interface OuterInterface {
+    class InnerClass : NativeBase {
+
+
+
+        /*
+         * For internal use only.
+         * @hidden
+         * @param nativeHandle The handle to resources on C++ side.
+         * @param tag Tag used by callers to avoid overload resolution problems.
+         */
+        protected constructor(nativeHandle: Long, tag: Any?)
+            : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+
+
+        external fun foo(input: String) : String
+
+
+
+        companion object {
+            @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+        }
+    }
+    interface InnerInterface {
+
+        fun foo(input: String) : String
+
+
+    }
+
+    class InnerInterfaceImpl : NativeBase, InnerInterface {
+        /*
+         * For internal use only.
+         * @hidden
+         * @param nativeHandle The handle to resources on C++ side.
+         * @param tag Tag used by callers to avoid overload resolution problems.
+         */
+        protected constructor(nativeHandle: Long, tag: Any?)
+            : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+        override external fun foo(input: String) : String
+
+
+
+        companion object {
+            @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+        }
+    }
+
+    fun foo(input: String) : String
+
+
+}
+
