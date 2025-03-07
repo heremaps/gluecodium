07392514f23103bf601ba278833fc7a2afdf588f
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-03-07 13:56:33 +0100

    smoke tests: add 'locale' test suite for Kotlin (#1686)
    This change introduces the expected output
    for smoke tests, which is aligned with the
    same test suite for Java generator to ensure
    that the level of support in Kotlin is the same.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/locales/output/android-kotlin/com/example/smoke/Locales.kt
@@ -0,0 +1,51 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+import com.example.NativeBase
+import java.util.Locale
+
+class Locales : NativeBase {
+
+    class LocaleStruct {
+        var localeField: Locale
+
+
+
+        constructor(localeField: Locale) {
+            this.localeField = localeField
+        }
+
+
+
+
+    }
+
+
+
+    /*
+     * For internal use only.
+     * @hidden
+     * @param nativeHandle The handle to resources on C++ side.
+     * @param tag Tag used by callers to avoid overload resolution problems.
+     */
+    protected constructor(nativeHandle: Long, tag: Any?)
+        : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+
+
+    external fun localeMethod(input: Locale) : Locale
+
+    var localeProperty: Locale
+        external get
+        external set
+
+
+
+    companion object {
+        @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+    }
+}
