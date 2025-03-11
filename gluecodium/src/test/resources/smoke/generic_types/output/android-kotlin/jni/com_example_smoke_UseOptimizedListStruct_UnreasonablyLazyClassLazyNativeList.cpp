094c358d30a1fdd5cd19b7cbf8ff7bf4362f48c1
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-03-11 12:10:01 +0100

    smoke tests: add 'optimized lists' test suite for Kotlin
    This change introduces the expected output
    for smoke tests, which is aligned with the
    same test suite for Java generator to ensure
    that the level of support in Kotlin is the same.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/generic_types/output/android-kotlin/jni/com_example_smoke_UseOptimizedListStruct_UnreasonablyLazyClassLazyNativeList.cpp
@@ -0,0 +1,45 @@
+/*
+
+ *
+ */
+
+#include "com_example_smoke_UnreasonablyLazyClass__Conversion.h"
+#include "com_example_smoke_UseOptimizedListStruct_UnreasonablyLazyClassLazyNativeList.h"
+#include "JniReference.h"
+#include "JniThrowNewException.h"
+#include "JniNativeHandle.h"
+#include <memory>
+#include <vector>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL
+Java_com_example_smoke_UseOptimizedListStruct_00024UnreasonablyLazyClassLazyNativeList_disposeNativeHandle(JNIEnv* _jenv, jobject _jinstance, jlong _jpointerRef) {
+    delete reinterpret_cast<std::shared_ptr<::std::vector< ::std::shared_ptr< ::smoke::UnreasonablyLazyClass > >>*>(_jpointerRef);
+}
+
+JNIEXPORT jint JNICALL
+Java_com_example_smoke_UseOptimizedListStruct_00024UnreasonablyLazyClassLazyNativeList_obtainSize(JNIEnv* _jenv, jobject _jinstance) {
+    auto handle = ::gluecodium::jni::get_class_native_handle(_jenv, _jinstance);
+    auto& vector = *reinterpret_cast<std::shared_ptr<::std::vector< ::std::shared_ptr< ::smoke::UnreasonablyLazyClass > >>*>(handle);
+    return vector->size();
+}
+
+JNIEXPORT jobject JNICALL
+Java_com_example_smoke_UseOptimizedListStruct_00024UnreasonablyLazyClassLazyNativeList_get(JNIEnv* _jenv, jobject _jinstance, jint index) {
+    auto handle = ::gluecodium::jni::get_class_native_handle(_jenv, _jinstance);
+    auto& vector = *reinterpret_cast<std::shared_ptr<::std::vector< ::std::shared_ptr< ::smoke::UnreasonablyLazyClass > >>*>(handle);
+    if (index < 0 || index >= vector->size()) {
+        ::gluecodium::jni::throw_new_index_out_of_bounds_exception(_jenv);
+        return {};
+    } else {
+        auto& result = vector->operator[](index);
+        return ::gluecodium::jni::convert_to_jni(_jenv, result).release();
+    }
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ gluecodium/src/test/resources/smoke/generic_types/output/android-kotlin/jni/com_example_smoke_UseOptimizedList_VeryBigStructLazyNativeList.cpp
@@ -0,0 +1,45 @@
+/*
+
+ *
+ */
+
+#include "com_example_smoke_UseOptimizedList_VeryBigStructLazyNativeList.h"
+#include "com_example_smoke_VeryBigStruct__Conversion.h"
+#include "JniReference.h"
+#include "JniThrowNewException.h"
+#include "JniNativeHandle.h"
+#include <memory>
+#include <vector>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL
+Java_com_example_smoke_UseOptimizedList_00024VeryBigStructLazyNativeList_disposeNativeHandle(JNIEnv* _jenv, jobject _jinstance, jlong _jpointerRef) {
+    delete reinterpret_cast<std::shared_ptr<::std::vector< ::std::shared_ptr< ::smoke::VeryBigStruct > >>*>(_jpointerRef);
+}
+
+JNIEXPORT jint JNICALL
+Java_com_example_smoke_UseOptimizedList_00024VeryBigStructLazyNativeList_obtainSize(JNIEnv* _jenv, jobject _jinstance) {
+    auto handle = ::gluecodium::jni::get_class_native_handle(_jenv, _jinstance);
+    auto& vector = *reinterpret_cast<std::shared_ptr<::std::vector< ::std::shared_ptr< ::smoke::VeryBigStruct > >>*>(handle);
+    return vector->size();
+}
+
+JNIEXPORT jobject JNICALL
+Java_com_example_smoke_UseOptimizedList_00024VeryBigStructLazyNativeList_get(JNIEnv* _jenv, jobject _jinstance, jint index) {
+    auto handle = ::gluecodium::jni::get_class_native_handle(_jenv, _jinstance);
+    auto& vector = *reinterpret_cast<std::shared_ptr<::std::vector< ::std::shared_ptr< ::smoke::VeryBigStruct > >>*>(handle);
+    if (index < 0 || index >= vector->size()) {
+        ::gluecodium::jni::throw_new_index_out_of_bounds_exception(_jenv);
+        return {};
+    } else {
+        auto& result = *vector->operator[](index);
+        return ::gluecodium::jni::convert_to_jni(_jenv, result).release();
+    }
+}
+
+#ifdef __cplusplus
+}
+#endif
