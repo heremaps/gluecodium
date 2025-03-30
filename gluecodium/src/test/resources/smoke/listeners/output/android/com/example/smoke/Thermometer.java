df6977b45895cebe6f8323a5011741daf644b82c
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-03-30 21:42:56 +0200

    Handle throwing function used in `AfterConstruction` attribute (#1710)
    The mentioned case was not tested. It did not compile for Swift.
    
    This commit:
    - introduces LimeLazyFunctionCall to LimeModel to be able to get all the information about
       called function including the original parameter names and exceptions
    - adjusts the code generators to correctly propagate the exception even when the constructor
       is not explicitly marked as throwing, but after-constructed function throws
    - removes the requirements related to the same parameters naming for Swift -- since now
       constructor and static function may use different parameter names and 'this' object does not need to be named 'self'
    - ensures that the generated documentation comment for constructor contains information
       about exceptions from AfterConstructed annotation
    - adds new smoke tests
    - adds new functional tests
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/test/resources/smoke/listeners/output/android/com/example/smoke/Thermometer.java
+++ gluecodium/src/test/resources/smoke/listeners/output/android/com/example/smoke/Thermometer.java
@@ -15,19 +15,115 @@ import java.util.List;
  * &quot;Subject&quot; in observer design pattern.
  */
 public final class Thermometer extends NativeBase {
+    /**
+     * <p>Some error code for thermometer.
+     */
+    public enum SomeThermometerErrorCode {
+        ERROR_NONE(0),
+        ERROR_FATAL(1);
+        /**
+         * @hidden
+         */
+        public final int value;
 
+        SomeThermometerErrorCode(final int value) {
+            this.value = value;
+        }
+    }
+    /**
+     * <p>This error indicates problems with notification of observers.
+     * May be thrown if observers cannot be notified.
+     */
+    public static final class NotificationException extends Exception {
+        /**
+         * @hidden
+         * @param error The error.
+         */
+        public NotificationException(final String error) {
+            super(error.toString());
+            this.error = error;
+        }
+
+        /**
+         * @hidden
+         */
+        public final String error;
+    }
+    /**
+     * <p>This error indicates other problems with notification of observers.
+     */
+    public static final class AnotherNotificationException extends Exception {
+        /**
+         * @hidden
+         * @param error The error.
+         */
+        public AnotherNotificationException(final Thermometer.SomeThermometerErrorCode error) {
+            super(error.toString());
+            this.error = error;
+        }
+
+        /**
+         * @hidden
+         */
+        public final Thermometer.SomeThermometerErrorCode error;
+    }
+    /**
+     * <p>A constructor, which makes the thermometer with readout interval.
+     * @param interval <p>readout interval
+     * @param observers <p>observers of temperature changes
+     */
 
     public Thermometer(@NonNull final Duration interval, @NonNull final List<TemperatureObserver> observers) {
         this(makeWithDuration(interval, observers), (Object)null);
         cacheThisInstance();
         notifyObservers(this, observers);
     }
-
+    /**
+     * <p>A constructor, which makes the thermometer with default readout interval (1 second).
+     * @param observers <p>observers of temperature changes
+     */
 
     public Thermometer(@NonNull final List<TemperatureObserver> observers) {
         this(makeWithoutDuration(observers), (Object)null);
         cacheThisInstance();
         notifyObservers(this, observers);
+    }
+    /**
+     * <p>A throwing constructor, which makes the thermometer with default readout interval (1 second).
+     * @param id <p>identification of this thermometer
+     * @param observers <p>observers of temperature changes
+     * @throws Thermometer.NotificationException <p>if identification number is invalid
+     */
+
+    public Thermometer(final int id, @NonNull final List<TemperatureObserver> observers) throws Thermometer.NotificationException {
+        this(throwingMake(id, observers), (Object)null);
+        cacheThisInstance();
+        throwingNotifyObservers(this, observers);
+    }
+    /**
+     * <p>A non-throwing constructor, which makes the thermometer with default readout interval (1 second).
+     * @param label <p>some identification label
+     * @param niceObservers <p>observers of temperature changes
+     * @throws Thermometer.NotificationException <p>if notification of observers failed
+     */
+
+    public Thermometer(@NonNull final String label, @NonNull final List<TemperatureObserver> niceObservers) throws Thermometer.NotificationException {
+        this(nothrowMake(label, niceObservers), (Object)null);
+        cacheThisInstance();
+        throwingNotifyObservers(this, niceObservers);
+    }
+    /**
+     * <p>A throwing constructor, which makes the thermometer with default readout interval (1 second).
+     * @param dummy <p>some dummy boolean flag
+     * @param observers <p>observers of temperature changes
+     * @throws Thermometer.AnotherNotificationException <p>if some problem occurs
+     * @throws Thermometer.NotificationException <p>if notification of observers failed
+     */
+
+    public Thermometer(final boolean dummy, @NonNull final List<TemperatureObserver> observers) throws Thermometer.AnotherNotificationException, Thermometer.NotificationException {
+        this(anotherThrowingMake(dummy, observers), (Object)null);
+        cacheThisInstance();
+        throwingNotifyObservers(this, observers);
     }
 
     /**
@@ -53,8 +149,22 @@ public final class Thermometer extends NativeBase {
 
     private static native long makeWithoutDuration(@NonNull final List<TemperatureObserver> observers);
 
+    private static native long throwingMake(final int id, @NonNull final List<TemperatureObserver> observers) throws Thermometer.NotificationException;
 
-    public static native void notifyObservers(@NonNull final Thermometer self, @NonNull final List<TemperatureObserver> observers);
+    private static native long nothrowMake(@NonNull final String label, @NonNull final List<TemperatureObserver> niceObservers);
+
+    private static native long anotherThrowingMake(final boolean dummy, @NonNull final List<TemperatureObserver> observers) throws Thermometer.AnotherNotificationException;
+
+
+    public static native void notifyObservers(@NonNull final Thermometer thermometer, @NonNull final List<TemperatureObserver> someObservers);
+
+    /**
+     * <p>Function used to notify observers.
+     * @param thermometer <p>subject that has changed state
+     * @param someObservers <p>observers to be notified
+     * @throws Thermometer.NotificationException <p>if notification of observers failed
+     */
+    public static native void throwingNotifyObservers(@NonNull final Thermometer thermometer, @NonNull final List<TemperatureObserver> someObservers) throws Thermometer.NotificationException;
 
 
     public native void forceUpdate();
--- gluecodium/src/test/resources/smoke/listeners/output/cpp/include/smoke/Thermometer.h
+++ gluecodium/src/test/resources/smoke/listeners/output/cpp/include/smoke/Thermometer.h
@@ -8,9 +8,13 @@
 
 #include "gluecodium/DurationHash.h"
 #include "gluecodium/ExportGluecodiumCpp.h"
+#include "gluecodium/Return.h"
 #include "gluecodium/VectorHash.h"
 #include <chrono>
+#include <cstdint>
 #include <memory>
+#include <string>
+#include <system_error>
 #include <vector>
 
 
@@ -35,24 +39,65 @@ public:
 
 public:
     /**
-     *
-     * \param[in] interval
-     * \param[in] observers
+     * Some error code for thermometer.
+
+     */
+    enum class SomeThermometerErrorCode {
+        ERROR_NONE,
+        ERROR_FATAL
+    };
+
+
+public:
+    /**
+     * A constructor, which makes the thermometer with readout interval.
+     * \param[in] interval readout interval
+     * \param[in] observers observers of temperature changes
      * \return @NotNull
      */
     static ::std::shared_ptr< ::smoke::Thermometer > make_with_duration( const ::std::chrono::seconds interval, const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& observers );
     /**
-     *
-     * \param[in] observers
+     * A constructor, which makes the thermometer with default readout interval (1 second).
+     * \param[in] observers observers of temperature changes
      * \return @NotNull
      */
     static ::std::shared_ptr< ::smoke::Thermometer > make_without_duration( const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& observers );
     /**
-     *
-     * \param[in] self @NotNull
-     * \param[in] observers
+     * A throwing constructor, which makes the thermometer with default readout interval (1 second).
+     * \param[in] id identification of this thermometer
+     * \param[in] observers observers of temperature changes
+     * \return @NotNull
+     * \retval ::::String if identification number is invalid
      */
-    static void notify_observers( const ::std::shared_ptr< ::smoke::Thermometer >& self, const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& observers );
+    static ::gluecodium::Return< ::std::shared_ptr< ::smoke::Thermometer >, ::std::string > throwing_make( const int32_t id, const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& observers );
+    /**
+     * A non-throwing constructor, which makes the thermometer with default readout interval (1 second).
+     * \param[in] label some identification label
+     * \param[in] nice_observers observers of temperature changes
+     * \return @NotNull
+     */
+    static ::std::shared_ptr< ::smoke::Thermometer > nothrow_make( const ::std::string& label, const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& nice_observers );
+    /**
+     * A throwing constructor, which makes the thermometer with default readout interval (1 second).
+     * \param[in] dummy some dummy boolean flag
+     * \param[in] observers observers of temperature changes
+     * \return @NotNull
+     * \retval ::smoke::Thermometer::SomeThermometerErrorCode if some problem occurs
+     */
+    static ::gluecodium::Return< ::std::shared_ptr< ::smoke::Thermometer >, ::std::error_code > another_throwing_make( const bool dummy, const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& observers );
+    /**
+     *
+     * \param[in] thermometer @NotNull
+     * \param[in] some_observers
+     */
+    static void notify_observers( const ::std::shared_ptr< ::smoke::Thermometer >& thermometer, const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& some_observers );
+    /**
+     * Function used to notify observers.
+     * \param[in] thermometer @NotNull subject that has changed state
+     * \param[in] some_observers observers to be notified
+     * \retval ::::String if notification of observers failed
+     */
+    static ::gluecodium::Return< void, ::std::string > throwing_notify_observers( const ::std::shared_ptr< ::smoke::Thermometer >& thermometer, const ::std::vector< ::std::shared_ptr< ::smoke::TemperatureObserver > >& some_observers );
     virtual void force_update(  ) = 0;
     virtual double get_celsius(  ) = 0;
     virtual double get_kelvin(  ) = 0;
@@ -60,4 +105,11 @@ public:
 };
 
 
+_GLUECODIUM_CPP_EXPORT ::std::error_code make_error_code( ::smoke::Thermometer::SomeThermometerErrorCode value ) noexcept;
+}
+
+namespace std
+{
+template <>
+struct is_error_code_enum< ::smoke::Thermometer::SomeThermometerErrorCode > : public std::true_type { };
 }
