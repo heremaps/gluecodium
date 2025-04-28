9bb1fc89c0137b7207c85817bff08da89a91286f
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-04-28 09:56:51 +0200

    smoke tests: showcase invalid behavior for 'duplicated names'
    Kotlin generator imports two classes with the same name.
    Such code does not compile.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/duplicate_names/output/android-kotlin/com/example/smoke/LearnToRead.kt
@@ -0,0 +1,26 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+import com.example.smoke.foo.Alphabet
+
+class LearnToRead {
+    var fieldA: Alphabet
+    var fieldB: Alphabet
+
+
+
+    constructor() {
+        this.fieldA = Alphabet.A
+        this.fieldB = Alphabet.BETA
+    }
+
+
+
+
+
+}
+
---
+++ gluecodium/src/test/resources/smoke/duplicate_names/output/android-kotlin/com/example/smoke/LearnToReadAgain.kt
@@ -0,0 +1,27 @@
+/*
+
+ *
+ */
+
+package com.example.smoke
+
+import com.example.smoke.bar.Alphabet
+import com.example.smoke.foo.Alphabet
+
+class LearnToReadAgain {
+    var fieldB: Alphabet
+    var fieldC: Alphabet
+
+
+
+    constructor() {
+        this.fieldB = Alphabet.BETA
+        this.fieldC = Alphabet.GIMEL
+    }
+
+
+
+
+
+}
+
