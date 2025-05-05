d13d9b624e13463450f58cc9f0b94f78a62b88bd
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-05-05 09:26:02 +0200

    smoke tests: add 'method overloads' test suite for Kotlin
    This change introduces the expected output
    for smoke tests, which is aligned with the
    same test suite for Java generator to ensure
    that the level of support in Kotlin is the same.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/method_overloads/output/android-kotlin/com/example/smoke/SpecialNames.kt
@@ -0,0 +1,55 @@
+/*
+
+ *
+ */
+
+@file:JvmName("SpecialNames")
+
+package com.example.smoke
+
+import com.example.NativeBase
+
+class SpecialNames : NativeBase {
+
+
+
+
+    constructor(result: String) : this(make(result), null as Any?) {
+        cacheThisInstance();
+    }
+
+    /**
+     * For internal use only.
+     * @suppress
+     * @param nativeHandle The handle to resources on C++ side.
+     * @param tag Tag used by callers to avoid overload resolution problems.
+     */
+    protected constructor(nativeHandle: Long, @Suppress("UNUSED_PARAMETER") tag: Any?)
+        : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+    private external fun cacheThisInstance()
+
+
+
+
+    external fun create() : Unit
+
+
+    external fun release() : Unit
+
+
+    external fun createProxy() : Unit
+
+
+    external fun Uppercase() : Unit
+
+
+
+
+    companion object {
+        @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+
+        @JvmStatic external fun make(result: String) : Long
+    }
+}
+
