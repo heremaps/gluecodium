5bc900c9ad9a6e812f60261b78bb46462942e072
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-05-08 13:08:35 +0200

    smoke tests: add 'dates' test suite for Kotlin (#1722)
    This change introduces the expected output
    for smoke tests, which is aligned with the
    same test suite for Java generator to ensure
    that the level of support in Kotlin is the same.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/dates/output/android-kotlin/jni/com_example_smoke_Dates.cpp
@@ -0,0 +1,171 @@
+/*
+
+ *
+ */
+
+#include "com_example_smoke_Dates.h"
+#include "com_example_smoke_Dates__Conversion.h"
+#include "ArrayConversionUtils.h"
+#include "JniClassCache.h"
+#include "JniNativeHandle.h"
+#include "JniReference.h"
+#include "JniThrowNewException.h"
+#include "JniWrapperCache.h"
+
+extern "C" {
+
+jobject
+Java_com_example_smoke_Dates_dateMethod(JNIEnv* _jenv, jobject _jinstance, jobject jinput)
+
+{
+
+
+
+    ::std::chrono::system_clock::time_point input = ::gluecodium::jni::convert_from_jni(_jenv,
+            ::gluecodium::jni::make_non_releasing_ref(jinput),
+            ::gluecodium::jni::TypeId<::std::chrono::system_clock::time_point>{});
+
+
+
+    auto pInstanceSharedPointer = reinterpret_cast<std::shared_ptr<::smoke::Dates>*> (
+
+        ::gluecodium::jni::get_class_native_handle(_jenv,_jinstance));
+
+
+
+
+    auto _result = (*pInstanceSharedPointer)->date_method(input);
+
+    return ::gluecodium::jni::convert_to_jni(_jenv, _result).release();
+}
+
+jobject
+Java_com_example_smoke_Dates_nullableDateMethod(JNIEnv* _jenv, jobject _jinstance, jobject jinput)
+
+{
+
+
+
+    std::optional< ::std::chrono::system_clock::time_point > input = ::gluecodium::jni::convert_from_jni(_jenv,
+            ::gluecodium::jni::make_non_releasing_ref(jinput),
+            ::gluecodium::jni::TypeId<std::optional< ::std::chrono::system_clock::time_point >>{});
+
+
+
+    auto pInstanceSharedPointer = reinterpret_cast<std::shared_ptr<::smoke::Dates>*> (
+
+        ::gluecodium::jni::get_class_native_handle(_jenv,_jinstance));
+
+
+
+
+    auto _result = (*pInstanceSharedPointer)->nullable_date_method(input);
+
+    return ::gluecodium::jni::convert_to_jni(_jenv, _result).release();
+}
+
+
+jobject
+Java_com_example_smoke_Dates_getDateProperty(JNIEnv* _jenv, jobject _jinstance)
+
+{
+
+
+
+    auto pInstanceSharedPointer = reinterpret_cast<std::shared_ptr<::smoke::Dates>*> (
+
+        ::gluecodium::jni::get_class_native_handle(_jenv,_jinstance));
+
+
+
+
+    auto _result = (*pInstanceSharedPointer)->get_date_property();
+
+    return ::gluecodium::jni::convert_to_jni(_jenv, _result).release();
+}
+
+
+
+void
+Java_com_example_smoke_Dates_setDateProperty(JNIEnv* _jenv, jobject _jinstance, jobject jvalue)
+
+{
+
+
+
+    ::std::chrono::system_clock::time_point value = ::gluecodium::jni::convert_from_jni(_jenv,
+            ::gluecodium::jni::make_non_releasing_ref(jvalue),
+            ::gluecodium::jni::TypeId<::std::chrono::system_clock::time_point>{});
+
+
+
+    auto pInstanceSharedPointer = reinterpret_cast<std::shared_ptr<::smoke::Dates>*> (
+
+        ::gluecodium::jni::get_class_native_handle(_jenv,_jinstance));
+
+
+
+
+    (*pInstanceSharedPointer)->set_date_property(value);
+
+}
+
+
+
+jobject
+Java_com_example_smoke_Dates_getDateSet(JNIEnv* _jenv, jobject _jinstance)
+
+{
+
+
+
+    auto pInstanceSharedPointer = reinterpret_cast<std::shared_ptr<::smoke::Dates>*> (
+
+        ::gluecodium::jni::get_class_native_handle(_jenv,_jinstance));
+
+
+
+
+    auto _result = (*pInstanceSharedPointer)->get_date_set();
+
+    return ::gluecodium::jni::convert_to_jni(_jenv, _result).release();
+}
+
+
+
+void
+Java_com_example_smoke_Dates_setDateSet(JNIEnv* _jenv, jobject _jinstance, jobject jvalue)
+
+{
+
+
+
+    ::std::unordered_set< ::std::chrono::system_clock::time_point, ::gluecodium::hash< ::std::chrono::system_clock::time_point > > value = ::gluecodium::jni::convert_from_jni(_jenv,
+            ::gluecodium::jni::make_non_releasing_ref(jvalue),
+            ::gluecodium::jni::TypeId<::std::unordered_set< ::std::chrono::system_clock::time_point, ::gluecodium::hash< ::std::chrono::system_clock::time_point > >>{});
+
+
+
+    auto pInstanceSharedPointer = reinterpret_cast<std::shared_ptr<::smoke::Dates>*> (
+
+        ::gluecodium::jni::get_class_native_handle(_jenv,_jinstance));
+
+
+
+
+    (*pInstanceSharedPointer)->set_date_set(value);
+
+}
+
+
+
+
+JNIEXPORT void JNICALL
+Java_com_example_smoke_Dates_disposeNativeHandle(JNIEnv* _jenv, jobject _jinstance, jlong _jpointerRef)
+{
+    auto p_nobj = reinterpret_cast<std::shared_ptr<::smoke::Dates>*>(_jpointerRef);
+    ::gluecodium::jni::JniWrapperCache::remove_cached_wrapper(_jenv, *p_nobj);
+    delete p_nobj;
+}
+
+}
