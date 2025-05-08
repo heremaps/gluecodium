637a023dc6b18c7cf380fba65ac2ea8e00b55c43
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-05-08 13:48:13 +0200

    smoke tests: add 'lambdas' test suite for Kotlin (#1709)
    This change introduces the expected output
    for smoke tests, which is aligned with the
    same test suite for Java generator to ensure
    that the level of support in Kotlin is the same.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/lambdas/output/android-kotlin/jni/com_example_smoke_Lambdas_IndexerImplCppProxy.cpp
@@ -0,0 +1,37 @@
+/*
+
+ *
+ */
+
+#include "com_example_smoke_Lambdas_IndexerImplCppProxy.h"
+#include "com_example_smoke_Lambdas_Indexer__Conversion.h"
+#include "ArrayConversionUtils.h"
+#include "FieldAccessMethods.h"
+
+namespace gluecodium
+{
+namespace jni
+{
+
+com_example_smoke_Lambdas_00024Indexer_CppProxy::com_example_smoke_Lambdas_00024Indexer_CppProxy(JniReference<jobject> globalRef, jint _jHashCode) noexcept
+    : CppProxyBase(std::move(globalRef), _jHashCode, "com_example_smoke_Lambdas_00024Indexer") {
+}
+
+int32_t
+com_example_smoke_Lambdas_00024Indexer_CppProxy::operator()( const ::std::string& np0, const float nidx ) {
+
+    JNIEnv* jniEnv = getJniEnvironment( );
+    auto jp0 = convert_to_jni( jniEnv, np0 );
+    jfloat jidx = nidx;
+    auto _result = callJavaMethod<jint>( "apply", "(Ljava/lang/String;F)I", jniEnv , jp0, jidx);
+
+    checkExceptionAndReportIfAny(jniEnv);
+
+    return _result;
+
+
+
+}
+
+}
+}
