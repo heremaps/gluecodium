ed8ad039015eb53d927ffbc32502a95eeab3ee39
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-05-13 11:36:15 +0200

    Kotlin: correctly handle types with duplicated names (#1720)
    This change enables 'CrossPackageNameClash' functional test
    suite for Kotlin generator tests.
    
    Moreover, this change adds handling of types with duplicated names.
    It ensurers that 'cross-package-name-clash' test suite passes without problems.
    
    Without this change Kotlin generator was creating multiple
    imports of type with the same name. With this commit it does
    not generate imports if there are types with duplicated names
    and references them with full path.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/duplicate_names/output/android-kotlin/com/example/smoke/LearnToReadAgain.kt
@@ -0,0 +1,27 @@
+/*
+
+ *
+ */
+
+@file:JvmName("LearnToReadAgain")
+
+package com.example.smoke
+
+
+class LearnToReadAgain {
+    @JvmField var fieldB: com.example.smoke.foo.Alphabet
+    @JvmField var fieldC: com.example.smoke.bar.Alphabet
+
+
+
+    constructor() {
+        this.fieldB = com.example.smoke.foo.Alphabet.BETA
+        this.fieldC = com.example.smoke.bar.Alphabet.GIMEL
+    }
+
+
+
+
+
+}
+
