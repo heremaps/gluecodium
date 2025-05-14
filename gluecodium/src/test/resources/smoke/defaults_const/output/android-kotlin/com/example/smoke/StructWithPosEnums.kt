067f1ccd045c6bcfdb565a637970c0a1f19baa9d
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-05-14 06:50:44 +0200

    smoke tests: add 'defaults const' test suite for Kotlin (#1738)
    This change introduces the expected output
    for smoke tests, which is aligned with the
    same test suite for Java generator to ensure
    that the level of support in Kotlin is the same.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/defaults_const/output/android-kotlin/com/example/smoke/StructWithPosEnums.kt
@@ -0,0 +1,32 @@
+/*
+
+ *
+ */
+
+@file:JvmName("StructWithPosEnums")
+
+package com.example.smoke
+
+
+class StructWithPosEnums {
+    @JvmField var firstField: SomethingEnum
+    @JvmField var explicitField: SomethingEnum
+    @JvmField var lastField: SomethingEnum
+
+
+
+    constructor() {
+        this.firstField = SomethingEnum.REALLY_FIRST
+        this.explicitField = SomethingEnum.EXPLICIT
+        this.lastField = SomethingEnum.LAST
+    }
+
+
+
+
+
+    companion object {
+        @JvmField final val FIRST_CONSTANT: SomethingEnum = SomethingEnum.REALLY_FIRST
+    }
+}
+
