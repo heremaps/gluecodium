471da812c9391ff791dd8fc39ab0229a83778bbd
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-05-26 09:55:58 +0200

    smoke tests: show invalid behavior of Kotlin generator
    The generated 'Impl' class for nested lambda
    contains also properties of the outer class.
    
    This should not happen -- it causes compilation
    error.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/lambdas/output/android-kotlin/com/example/smoke/OuterClassWithLambdaAndProperty.kt
@@ -0,0 +1,66 @@
+/*
+
+ *
+ */
+
+@file:JvmName("OuterClassWithLambdaAndProperty")
+
+package com.example.smoke
+
+import com.example.NativeBase
+
+class OuterClassWithLambdaAndProperty : NativeBase {
+
+    fun interface SomeInternalLambda {
+
+
+        fun apply(p0: Int) : Int
+    }
+
+    /**
+     * @suppress
+     */
+    class SomeInternalLambdaImpl : NativeBase, SomeInternalLambda {
+        protected constructor(nativeHandle: Long, @Suppress("UNUSED_PARAMETER") tag: Any?)
+            : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+
+
+        override external fun apply(p0: Int) : Int
+
+        override var someInteger: Int
+            external get
+            external set
+
+
+
+        companion object {
+            @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+        }
+    }
+
+
+    /**
+     * For internal use only.
+     * @suppress
+     * @param nativeHandle The handle to resources on C++ side.
+     * @param tag Tag used by callers to avoid overload resolution problems.
+     */
+    protected constructor(nativeHandle: Long, @Suppress("UNUSED_PARAMETER") tag: Any?)
+        : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+
+
+
+    var someInteger: Int
+        external get
+        external set
+
+
+
+
+    companion object {
+        @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+    }
+}
+
