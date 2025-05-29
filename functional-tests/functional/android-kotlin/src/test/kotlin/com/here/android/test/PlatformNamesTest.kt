786bcbcbdc1f5266544e654678e8524743a32076
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-05-29 16:03:26 +0200

    functional: implement accessor test for 'android-kotlin'
    This commit adds Kotlin test for accessors
    with custom platform names to showcase that
    functionality does not work for Kotlin.
    
    In order to enable custom name for getter/setter
    the JvmName() annotation needs to be used.
    
    It will be implemented in the next commits.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ functional-tests/functional/android-kotlin/src/test/kotlin/com/here/android/test/PlatformNamesTest.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016-2025 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+package com.here.android.test
+
+import com.here.android.RobolectricApplication
+import org.junit.Assert.assertEquals
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(application = RobolectricApplication::class)
+class PlatformNamesTest {
+
+    @org.junit.Test
+    fun useAccessorsWithCustomName() {
+        val bar: barInterface = barInterface("some_string")
+        bar.BAR_ATTRIBUTE = 77
+        assertEquals(77, bar.BAR_ATTRIBUTE)
+    }
+
+}
