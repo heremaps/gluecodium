87e51ae69c39d430ca26443eb3b8349dd8ff1e20
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2025-06-02 10:07:24 +0200

    smoke tests: add 'method overloads' test suite for Kotlin (#1723)
    This change introduces the expected output
    for smoke tests, which is aligned with the
    same test suite for Java generator to ensure
    that the level of support in Kotlin is the same.
    
    This change adjusts the LIME file used as input for the
    functional tests to match the one used in smoke tests.
    This is done to ensure that the generated code compiles
    and runs without problems.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/method_overloads/output/android-kotlin/com/example/smoke/KotlinMethodOverloads.kt
@@ -0,0 +1,41 @@
+/*
+
+ *
+ */
+
+@file:JvmName("KotlinMethodOverloads")
+
+package com.example.smoke
+
+import com.example.NativeBase
+
+class KotlinMethodOverloads : NativeBase {
+
+
+
+    /**
+     * For internal use only.
+     * @suppress
+     * @param nativeHandle The handle to resources on C++ side.
+     * @param tag Tag used by callers to avoid overload resolution problems.
+     */
+    protected constructor(nativeHandle: Long, @Suppress("UNUSED_PARAMETER") tag: Any?)
+        : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+
+
+
+
+    external fun three(input: String) : Unit
+
+
+    external fun three(input: MutableList<String>) : Unit
+
+
+
+
+    companion object {
+        @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+    }
+}
+
