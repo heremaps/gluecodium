2a0d9d7e5a03c6dd302d44544ec7ea57b704c588
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-06-11 14:42:17 +0200

    smoke tests: add global typealias test case for Dart
    This change introduces new expected output files
    for Dart to showcase that global typedefs work as expected.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/typedefs/output/dart/lib/src/smoke/global_list_type_def.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:library/src/generic_types__conversion.dart';
+
+/// This is some standalone list typedef.
+typedef GlobalListTypeDef = List<double>;
+
+
---
+++ gluecodium/src/test/resources/smoke/typedefs/output/dart/lib/src/smoke/global_map_type_def.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:library/src/generic_types__conversion.dart';
+
+/// This is some standalone map typedef.
+typedef GlobalMapTypeDef = Map<int, String>;
+
+
