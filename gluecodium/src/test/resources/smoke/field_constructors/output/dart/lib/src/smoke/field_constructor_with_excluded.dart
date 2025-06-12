8409db1e141d389d4260337aa0f73ccb421a4c66
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-06-12 13:39:18 +0200

    Dart: list field ctor params using bullet points
    DartDoc uses markdown. The bullet points should be used
    for describing parameters of constructors and functions.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/test/resources/smoke/field_constructors/output/dart/lib/src/smoke/field_constructor_with_excluded.dart
+++ gluecodium/src/test/resources/smoke/field_constructors/output/dart/lib/src/smoke/field_constructor_with_excluded.dart
@@ -1,15 +1,23 @@
+
+
 import 'dart:ffi';
 import 'package:library/src/_library_context.dart' as __lib;
 import 'package:library/src/builtin_types__conversion.dart';
+
+
 class FieldConstructorWithExcluded {
   /// Some field
   String stringField;
+
   /// Some field constructor
-  /// [stringField] Some field
+  /// - [stringField] Some field
   /// @nodoc
   FieldConstructorWithExcluded(this.stringField);
 }
+
+
 // FieldConstructorWithExcluded "private" section, not exported.
+
 final _smokeFieldconstructorwithexcludedCreateHandle = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
@@ -22,12 +30,16 @@ final _smokeFieldconstructorwithexcludedGetFieldstringField = __lib.catchArgumen
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
   >('library_smoke_FieldConstructorWithExcluded_get_field_stringField'));
+
+
+
 Pointer<Void> smokeFieldconstructorwithexcludedToFfi(FieldConstructorWithExcluded value) {
   final _stringFieldHandle = stringToFfi(value.stringField);
   final _result = _smokeFieldconstructorwithexcludedCreateHandle(_stringFieldHandle);
   stringReleaseFfiHandle(_stringFieldHandle);
   return _result;
 }
+
 FieldConstructorWithExcluded smokeFieldconstructorwithexcludedFromFfi(Pointer<Void> handle) {
   final _stringFieldHandle = _smokeFieldconstructorwithexcludedGetFieldstringField(handle);
   try {
@@ -38,8 +50,11 @@ FieldConstructorWithExcluded smokeFieldconstructorwithexcludedFromFfi(Pointer<Vo
     stringReleaseFfiHandle(_stringFieldHandle);
   }
 }
+
 void smokeFieldconstructorwithexcludedReleaseFfiHandle(Pointer<Void> handle) => _smokeFieldconstructorwithexcludedReleaseHandle(handle);
+
 // Nullable FieldConstructorWithExcluded
+
 final _smokeFieldconstructorwithexcludedCreateHandleNullable = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
@@ -52,6 +67,7 @@ final _smokeFieldconstructorwithexcludedGetValueNullable = __lib.catchArgumentEr
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
   >('library_smoke_FieldConstructorWithExcluded_get_value_nullable'));
+
 Pointer<Void> smokeFieldconstructorwithexcludedToFfiNullable(FieldConstructorWithExcluded? value) {
   if (value == null) return Pointer<Void>.fromAddress(0);
   final _handle = smokeFieldconstructorwithexcludedToFfi(value);
@@ -59,6 +75,7 @@ Pointer<Void> smokeFieldconstructorwithexcludedToFfiNullable(FieldConstructorWit
   smokeFieldconstructorwithexcludedReleaseFfiHandle(_handle);
   return result;
 }
+
 FieldConstructorWithExcluded? smokeFieldconstructorwithexcludedFromFfiNullable(Pointer<Void> handle) {
   if (handle.address == 0) return null;
   final _handle = _smokeFieldconstructorwithexcludedGetValueNullable(handle);
@@ -66,6 +83,10 @@ FieldConstructorWithExcluded? smokeFieldconstructorwithexcludedFromFfiNullable(P
   smokeFieldconstructorwithexcludedReleaseFfiHandle(_handle);
   return result;
 }
+
 void smokeFieldconstructorwithexcludedReleaseFfiHandleNullable(Pointer<Void> handle) =>
   _smokeFieldconstructorwithexcludedReleaseHandleNullable(handle);
+
 // End of FieldConstructorWithExcluded "private" section.
+
+
--- gluecodium/src/test/resources/smoke/field_constructors/output/dart/lib/src/smoke/field_constructor_with_parent_comment.dart
+++ gluecodium/src/test/resources/smoke/field_constructors/output/dart/lib/src/smoke/field_constructor_with_parent_comment.dart
@@ -1,14 +1,22 @@
+
+
 import 'dart:ffi';
 import 'package:library/src/_library_context.dart' as __lib;
 import 'package:library/src/builtin_types__conversion.dart';
+
 /// SomeStruct
+
 class FieldConstructorWithParentComment {
   String stringField;
+
   /// There are constructors
-  /// [stringField]
+  /// - [stringField] 
   FieldConstructorWithParentComment(this.stringField);
 }
+
+
 // FieldConstructorWithParentComment "private" section, not exported.
+
 final _smokeFieldconstructorwithparentcommentCreateHandle = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
@@ -21,12 +29,16 @@ final _smokeFieldconstructorwithparentcommentGetFieldstringField = __lib.catchAr
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
   >('library_smoke_FieldConstructorWithParentComment_get_field_stringField'));
+
+
+
 Pointer<Void> smokeFieldconstructorwithparentcommentToFfi(FieldConstructorWithParentComment value) {
   final _stringFieldHandle = stringToFfi(value.stringField);
   final _result = _smokeFieldconstructorwithparentcommentCreateHandle(_stringFieldHandle);
   stringReleaseFfiHandle(_stringFieldHandle);
   return _result;
 }
+
 FieldConstructorWithParentComment smokeFieldconstructorwithparentcommentFromFfi(Pointer<Void> handle) {
   final _stringFieldHandle = _smokeFieldconstructorwithparentcommentGetFieldstringField(handle);
   try {
@@ -37,8 +49,11 @@ FieldConstructorWithParentComment smokeFieldconstructorwithparentcommentFromFfi(
     stringReleaseFfiHandle(_stringFieldHandle);
   }
 }
+
 void smokeFieldconstructorwithparentcommentReleaseFfiHandle(Pointer<Void> handle) => _smokeFieldconstructorwithparentcommentReleaseHandle(handle);
+
 // Nullable FieldConstructorWithParentComment
+
 final _smokeFieldconstructorwithparentcommentCreateHandleNullable = __lib.catchArgumentError(() => __lib.nativeLibrary.lookupFunction<
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
@@ -51,6 +66,7 @@ final _smokeFieldconstructorwithparentcommentGetValueNullable = __lib.catchArgum
     Pointer<Void> Function(Pointer<Void>),
     Pointer<Void> Function(Pointer<Void>)
   >('library_smoke_FieldConstructorWithParentComment_get_value_nullable'));
+
 Pointer<Void> smokeFieldconstructorwithparentcommentToFfiNullable(FieldConstructorWithParentComment? value) {
   if (value == null) return Pointer<Void>.fromAddress(0);
   final _handle = smokeFieldconstructorwithparentcommentToFfi(value);
@@ -58,6 +74,7 @@ Pointer<Void> smokeFieldconstructorwithparentcommentToFfiNullable(FieldConstruct
   smokeFieldconstructorwithparentcommentReleaseFfiHandle(_handle);
   return result;
 }
+
 FieldConstructorWithParentComment? smokeFieldconstructorwithparentcommentFromFfiNullable(Pointer<Void> handle) {
   if (handle.address == 0) return null;
   final _handle = _smokeFieldconstructorwithparentcommentGetValueNullable(handle);
@@ -65,6 +82,10 @@ FieldConstructorWithParentComment? smokeFieldconstructorwithparentcommentFromFfi
   smokeFieldconstructorwithparentcommentReleaseFfiHandle(_handle);
   return result;
 }
+
 void smokeFieldconstructorwithparentcommentReleaseFfiHandleNullable(Pointer<Void> handle) =>
   _smokeFieldconstructorwithparentcommentReleaseHandleNullable(handle);
+
 // End of FieldConstructorWithParentComment "private" section.
+
+
