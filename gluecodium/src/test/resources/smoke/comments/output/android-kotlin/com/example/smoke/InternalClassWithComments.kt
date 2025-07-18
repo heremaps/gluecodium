a472d0aee3f6f5c138ab61039e332732c4f4fe3e
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2025-07-18 13:15:02 +0200

    Kotlin: add more smoke tests on internal keyword
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

---
+++ gluecodium/src/test/resources/smoke/comments/output/android-kotlin/com/example/smoke/InternalClassWithComments.kt
@@ -0,0 +1,43 @@
+/*
+
+ *
+ */
+
+@file:JvmName("InternalClassWithComments")
+
+package com.example.smoke
+
+import com.example.NativeBase
+
+/**
+ * This looks internal
+ */
+internal class InternalClassWithComments : NativeBase {
+
+
+
+    /**
+     * For internal use only.
+     * @suppress
+     * @param nativeHandle The handle to resources on C++ side.
+     * @param tag Tag used by callers to avoid overload resolution problems.
+     */
+    protected constructor(nativeHandle: Long, @Suppress("UNUSED_PARAMETER") tag: Any?)
+        : super(nativeHandle, { disposeNativeHandle(it) }) {}
+
+
+
+    /**
+     * This is definitely internal
+     */
+
+    external fun doNothing() : Unit
+
+
+
+
+    companion object {
+        @JvmStatic private external fun disposeNativeHandle(nativeHandle: Long)
+    }
+}
+
---
+++ gluecodium/src/test/resources/smoke/field_constructors/output/android-kotlin/com/example/smoke/FieldConstructorsInternalFields.kt
@@ -0,0 +1,45 @@
+/*
+
+ *
+ */
+
+@file:JvmName("FieldConstructorsInternalFields")
+
+package com.example.smoke
+
+
+class FieldConstructorsInternalFields {
+    @JvmField var stringField: String
+    @JvmField var intField: Int
+    @JvmField internal var boolField: Boolean
+
+
+
+
+
+    constructor() {
+        this.stringField = "nonsense"
+        this.intField = 42
+        this.boolField = true
+    }
+
+
+    constructor(intField: Int, stringField: String) {
+        this.intField = intField
+        this.stringField = stringField
+        this.boolField = true
+    }
+
+
+    constructor(boolField: Boolean, intField: Int, stringField: String) {
+        this.boolField = boolField
+        this.intField = intField
+        this.stringField = stringField
+    }
+
+
+
+
+
+}
+
