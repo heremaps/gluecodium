c57c2a9e490d3560c43207ef5cb07cfbc8662c66
Patryk Wróbel <183546751+pwrobeldev@users.noreply.github.com>
2026-03-03 09:01:53 +0100

    C++: do not generate duplicated using statements for overloads in derived classes (#1803)
    ---------- Motivation ----------
    When base and derived classes declare method with the same name,
    then in the derived class we need to add 'using' statement to avoid
    compiler warning/error related to shadowing.
    
    It seems that when multiple function overloads were declared in the
    derived class, then Gluecodium generated redundant using statements
    which look like a bug.
    
    Moreover, usage of multiple using statements causes compilation error.
    
    ---------- Solution ----------
    Implemented filtering of prepared using statements to ensure that they
    are unique (via distinct() function call on list).
    
    This commit:
    - brings smoke tests that reproduce the problem
    - provides fix for the bug
    - showcases that the bug is solved in smoke tests
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/test/resources/smoke/method_overloads/output/android/jni/com_example_smoke_ChildClassFromInterfaceOverloads.h
+++ gluecodium/src/test/resources/smoke/method_overloads/output/android/jni/com_example_smoke_ChildClassFromInterfaceOverloads.h
@@ -1,15 +1,25 @@
 /*
+
  *
  */
+
 #pragma once
+
 #include <jni.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 JNIEXPORT void JNICALL
 Java_com_example_smoke_ChildClassFromInterfaceOverloads_foo__Ljava_lang_String_2(JNIEnv* _jenv, jobject _jinstance, jstring jinput);
 JNIEXPORT void JNICALL
+Java_com_example_smoke_ChildClassFromInterfaceOverloads_foo__D(JNIEnv* _jenv, jobject _jinstance, jdouble jinput);
+JNIEXPORT void JNICALL
 Java_com_example_smoke_ChildClassFromInterfaceOverloads_bar__Ljava_lang_String_2(JNIEnv* _jenv, jobject _jinstance, jstring jinput);
+JNIEXPORT void JNICALL
+Java_com_example_smoke_ChildClassFromInterfaceOverloads_bar__D(JNIEnv* _jenv, jobject _jinstance, jdouble jinput);
+
 JNIEXPORT void JNICALL
 Java_com_example_smoke_ChildClassFromInterfaceOverloads_foo__(JNIEnv* _jenv, jobject _jinstance);
 JNIEXPORT void JNICALL
@@ -18,6 +28,8 @@ JNIEXPORT void JNICALL
 Java_com_example_smoke_ChildClassFromInterfaceOverloads_bar(JNIEnv* _jenv, jobject _jinstance);
 JNIEXPORT void JNICALL
 Java_com_example_smoke_ChildClassFromInterfaceOverloads_baz(JNIEnv* _jenv, jobject _jinstance);
+
+
 #ifdef __cplusplus
 }
 #endif
--- gluecodium/src/test/resources/smoke/method_overloads/output/cbridge/include/smoke/cbridge_ChildClassFromInterfaceOverloads.h
+++ gluecodium/src/test/resources/smoke/method_overloads/output/cbridge/include/smoke/cbridge_ChildClassFromInterfaceOverloads.h
@@ -1,24 +1,47 @@
 //
+
 //
+
 #pragma once
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 #include "cbridge/include/BaseHandle.h"
 #include "cbridge/include/Export.h"
 #include <stdint.h>
+
+
+
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_release_handle(_baseRef handle);
 _GLUECODIUM_C_EXPORT _baseRef smoke_ChildClassFromInterfaceOverloads_copy_handle(_baseRef handle);
 _GLUECODIUM_C_EXPORT const void* smoke_ChildClassFromInterfaceOverloads_get_swift_object_from_wrapper_cache(_baseRef handle);
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_cache_swift_object_wrapper(_baseRef handle, const void* swift_pointer);
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_remove_swift_object_from_wrapper_cache(_baseRef handle);
 _GLUECODIUM_C_EXPORT void* smoke_ChildClassFromInterfaceOverloads_get_typed(_baseRef handle);
+
+
+
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_foo_String(_baseRef _instance, _baseRef input);
+
+_GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_foo_Double(_baseRef _instance, double input);
+
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_bar_String(_baseRef _instance, _baseRef input);
+
+_GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_bar_Double(_baseRef _instance, double input);
+
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_foo_(_baseRef _instance);
+
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_foo_Int(_baseRef _instance, int32_t input);
+
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_bar(_baseRef _instance);
+
 _GLUECODIUM_C_EXPORT void smoke_ChildClassFromInterfaceOverloads_baz(_baseRef _instance);
+
+
+
+
 #ifdef __cplusplus
 }
 #endif
