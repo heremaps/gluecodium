cfa06a565ff0281b82893c3e9e7c58783bd635c3
Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>
2026-07-21 13:51:47 +0200

    smoke-test: invalid behavior for dart for all-args-ctor
    Scenario: a structure that does not define any explicit constructors and
              does not have initialized fields. It has one internal field.
    
    Expectation: the generated all-args ctor is internal.
    
    Actual behavior: Java/Kotlin/Swift generates correct code.
                     Dart generates invalid code -- public ctor with internal field.
    
    Signed-off-by: Patryk Wrobel <183546751+pwrobeldev@users.noreply.github.com>

--- gluecodium/src/test/resources/smoke/visibility_attribute/output/dart/lib/smoke.dart
+++ gluecodium/src/test/resources/smoke/visibility_attribute/output/dart/lib/smoke.dart
@@ -1,6 +1,7 @@
 
 
 export 'src/smoke/internal_property_only.dart' show InternalPropertyOnly;
+export 'src/smoke/no_ctors_uninitialized_fields_one_internal_field.dart' show NoCtorsUninitializedFieldsOneInternalField;
 export 'src/smoke/public_class.dart' show PublicClass, PublicClass_PublicStruct, PublicClass_PublicStructWithInternalDefaults;
 export 'src/smoke/public_interface.dart' show PublicInterface;
 export 'src/smoke/public_struct_with_internal_constructors.dart' show PublicStructWithInternalConstructors;
